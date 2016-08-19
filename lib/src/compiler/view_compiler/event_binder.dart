e795e135e06c007bf91dce78e3ee9c9d60ee4750
ferhat <ferhat@google.com>
2016-08-19 13:41:14 -0700

    refactor(CompileEventListener): Added comments, minor style cleanup, reduceGC/speedup.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=140520745

--- lib/src/compiler/view_compiler/event_binder.dart
+++ lib/src/compiler/view_compiler/event_binder.dart
@@ -7,6 +7,13 @@ import 'compile_method.dart' show CompileMethod;
 import 'constants.dart' show EventHandlerVars;
 import 'expression_converter.dart' show convertCdStatementToIr;
 
+/// Generates code to listen to a single eventName on a [CompileElement].
+///
+/// Since multiple directives on an element could potentially listen to the
+/// same event, this class collects the individual handlers as actions and
+/// creates a single member method on the component that calls each of the
+/// handlers and then applies logical AND to determine resulting
+/// prevent default value.
 class CompileEventListener {
   CompileElement compileElement;
   String eventName;
@@ -14,39 +21,44 @@ class CompileEventListener {
   bool _hasComponentHostListener = false;
   String _methodName;
   o.FnParam _eventParam;
-  List<o.Expression> _actionResultExprs = [];
+  List<o.Expression> _actionResultExprs = <o.Expression>[];
+
+  /// Helper function to search for an event in [targetEventListeners] list and
+  /// add a new one if it doesn't exist yet.
+  /// TODO: try using Map, this code is quadratic and assumes typical event
+  /// lists are very small.
   static CompileEventListener getOrCreate(CompileElement compileElement,
       String eventName, List<CompileEventListener> targetEventListeners) {
-    var listener = targetEventListeners.firstWhere(
-        (listener) => listener.eventName == eventName,
-        orElse: () => null);
-    if (listener == null) {
-      listener = new CompileEventListener(
-          compileElement, eventName, targetEventListeners.length);
-      targetEventListeners.add(listener);
+    for (int i = 0, len = targetEventListeners.length; i < len; i++) {
+      var existingListener = targetEventListeners[i];
+      if (existingListener.eventName == eventName) return existingListener;
     }
+    var listener = new CompileEventListener(
+        compileElement, eventName, targetEventListeners.length);
+    targetEventListeners.add(listener);
     return listener;
   }
 
-  CompileEventListener(this.compileElement, this.eventName, num listenerIndex) {
-    this._method = new CompileMethod(compileElement.view);
-    this._methodName =
+  CompileEventListener(this.compileElement, this.eventName, int listenerIndex) {
+    _method = new CompileMethod(compileElement.view);
+    _methodName =
         '_handle_${sanitizeEventName(eventName)}_${compileElement.nodeIndex}_'
-        '${ listenerIndex}';
+        '$listenerIndex';
     // TODO: type event param as Identifiers.HTML_EVENT if non-custom event or
     // stream.
-    this._eventParam =
+    _eventParam =
         new o.FnParam(EventHandlerVars.event.name, o.importType(null));
   }
+
   void addAction(BoundEventAst hostEvent, CompileDirectiveMetadata directive,
       o.Expression directiveInstance) {
     if (directive != null && directive.isComponent) {
-      this._hasComponentHostListener = true;
+      _hasComponentHostListener = true;
     }
-    this._method.resetDebugInfo(this.compileElement.nodeIndex, hostEvent);
+    _method.resetDebugInfo(compileElement.nodeIndex, hostEvent);
     var context = directiveInstance ?? new o.ReadClassMemberExpr('ctx');
     var actionStmts = convertCdStatementToIr(
-        this.compileElement.view,
+        compileElement.view,
         context,
         hostEvent.handler,
         this.compileElement.view.component.template.preserveWhitespace);
@@ -54,9 +66,8 @@ class CompileEventListener {
     if (lastIndex >= 0) {
       var lastStatement = actionStmts[lastIndex];
       var returnExpr = convertStmtIntoExpression(lastStatement);
-      var preventDefaultVar =
-          o.variable('pd_${ this . _actionResultExprs . length}');
-      this._actionResultExprs.add(preventDefaultVar);
+      var preventDefaultVar = o.variable('pd_${_actionResultExprs.length}');
+      _actionResultExprs.add(preventDefaultVar);
       if (returnExpr != null) {
         // Note: We need to cast the result of the method call to dynamic,
         // as it might be a void method!
@@ -65,22 +76,24 @@ class CompileEventListener {
             .toDeclStmt(null, [o.StmtModifier.Final]);
       }
     }
-    this._method.addStmts(actionStmts);
+    _method.addStmts(actionStmts);
   }
 
   void finishMethod() {
-    var markPathToRootStart = this._hasComponentHostListener
-        ? this.compileElement.appViewContainer.prop('componentView')
+    var markPathToRootStart = _hasComponentHostListener
+        ? compileElement.appViewContainer.prop('componentView')
         : o.THIS_EXPR;
     o.Expression resultExpr = o.literal(true);
-    this._actionResultExprs.forEach((expr) {
-      resultExpr = resultExpr.and(expr);
-    });
-    List<o.Statement> stmts = (new List.from((new List.from((<o.Statement>[
+    for (var i = 0, len = _actionResultExprs.length; i < len; i++) {
+      resultExpr = resultExpr.and(_actionResultExprs[i]);
+    }
+    List<o.Statement> stmts = <o.Statement>[
       markPathToRootStart.callMethod('markPathToRootAsCheckOnce', []).toStmt()
-    ]))..addAll(this._method.finish())))
-      ..addAll([new o.ReturnStatement(resultExpr)]));
-    this.compileElement.view.eventHandlerMethods.add(new o.ClassMethod(
+    ]
+      ..addAll(_method.finish())
+      ..add(new o.ReturnStatement(resultExpr));
+
+    compileElement.view.eventHandlerMethods.add(new o.ClassMethod(
         this._methodName,
         [this._eventParam],
         stmts,
@@ -100,10 +113,7 @@ class CompileEventListener {
       eventListener
     ]);
 
-    this
-        .compileElement
-        .view
-        .createMethod
+    compileElement.view.createMethod
         .addStmt(new o.ExpressionStatement(listenExpr));
   }
 
@@ -127,43 +137,47 @@ class CompileEventListener {
 List<CompileEventListener> collectEventListeners(List<BoundEventAst> hostEvents,
     List<DirectiveAst> dirs, CompileElement compileElement) {
   List<CompileEventListener> eventListeners = [];
-  hostEvents.forEach((hostEvent) {
+  for (var hostEvent in hostEvents) {
     compileElement.view.bindings
         .add(new CompileBinding(compileElement, hostEvent));
     var listener = CompileEventListener.getOrCreate(
         compileElement, hostEvent.name, eventListeners);
     listener.addAction(hostEvent, null, null);
-  });
+  }
   var i = -1;
-  dirs.forEach((directiveAst) {
+  for (var directiveAst in dirs) {
     i++;
     var directiveInstance = compileElement.directiveInstances[i];
-    directiveAst.hostEvents.forEach((hostEvent) {
+    for (var hostEvent in directiveAst.hostEvents) {
       compileElement.view.bindings
           .add(new CompileBinding(compileElement, hostEvent));
       var listener = CompileEventListener.getOrCreate(
           compileElement, hostEvent.name, eventListeners);
       listener.addAction(hostEvent, directiveAst.directive, directiveInstance);
-    });
-  });
-  eventListeners.forEach((listener) => listener.finishMethod());
+    }
+  }
+  for (int i = 0, len = eventListeners.length; i < len; i++) {
+    eventListeners[i].finishMethod();
+  }
   return eventListeners;
 }
 
 void bindDirectiveOutputs(DirectiveAst directiveAst,
     o.Expression directiveInstance, List<CompileEventListener> eventListeners) {
   directiveAst.directive.outputs.forEach((observablePropName, eventName) {
-    eventListeners
-        .where((listener) => listener.eventName == eventName)
-        .toList()
-        .forEach((listener) {
+    for (int i = 0, len = eventListeners.length; i < len; i++) {
+      CompileEventListener listener = eventListeners[i];
+      if (listener.eventName != eventName) continue;
       listener.listenToDirective(directiveInstance, observablePropName);
-    });
+    }
   });
 }
 
 void bindRenderOutputs(List<CompileEventListener> eventListeners) {
-  eventListeners.forEach((listener) => listener.listenToRenderer());
+  for (int i = 0, len = eventListeners.length; i < len; i++) {
+    CompileEventListener listener = eventListeners[i];
+    listener.listenToRenderer();
+  }
 }
 
 o.Expression convertStmtIntoExpression(o.Statement stmt) {
@@ -175,6 +189,8 @@ o.Expression convertStmtIntoExpression(o.Statement stmt) {
   return null;
 }
 
+final RegExp _eventNameRegExp = new RegExp(r'[^a-zA-Z_]');
+
 String sanitizeEventName(String name) {
-  return name.replaceAll(new RegExp(r'[^a-zA-Z_]'), '_');
+  return name.replaceAll(_eventNameRegExp, '_');
 }
