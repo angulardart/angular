93498da038937f5e9450999d0d438112f655b935
Luis Vargas <luisbigvargas@gmail.com>
2016-08-20 06:53:36 -0400

    Translate ng_template_outlet.ts to dart

--- lib/src/common/directives/ng_template_outlet.dart
+++ lib/src/common/directives/ng_template_outlet.dart
@@ -1,24 +1,47 @@
-import "package:angular2/core.dart"
-    show Directive, Input, ViewContainerRef, ViewRef, TemplateRef;
+import "package:angular2/core.dart";
+import 'package:angular2/src/core/linker/view_ref.dart';
 
 /// Creates and inserts an embedded view based on a prepared `TemplateRef`.
+/// You can attach a context object to the `EmbeddedViewRef` by setting `[ngOutletContext]`.
+/// `[ngOutletContext]` should be an object, the object's keys will be the local template variables
+/// available within the `TemplateRef`.
+///
+/// Note: using the key `$implicit` in the context object will set it's value as default.
 ///
 /// ### Syntax
-/// - `<template [ngTemplateOutlet]="templateRefExpression"></template>`
+///
+/// ```
+/// <template [ngTemplateOutlet]="templateRefExpression"
+///           [ngOutletContext]="objectExpression">
+/// </template>
+/// ```
+///
+/// @experimental
 @Directive(selector: "[ngTemplateOutlet]")
-class NgTemplateOutlet {
-  final ViewContainerRef _viewContainerRef;
-  ViewRef _insertedViewRef;
+class NgTemplateOutlet implements OnChanges {
+  EmbeddedViewRef _viewRef;
+  var _context;
+  TemplateRef _templateRef;
+  ViewContainerRef _viewContainerRef;
+
   NgTemplateOutlet(this._viewContainerRef);
+
   @Input()
-  set ngTemplateOutlet(TemplateRef templateRef) {
-    if (_insertedViewRef != null) {
-      _viewContainerRef
-          .remove(this._viewContainerRef.indexOf(this._insertedViewRef));
+  set ngOutletContext(context) =>
+      _context = context;
+
+  @Input()
+  set ngTemplateOutlet(TemplateRef templateRef) =>
+      _templateRef = templateRef;
+
+  @override
+  ngOnChanges(Map<String, SimpleChange> changes) {
+    if (_viewRef != null) {
+      _viewContainerRef.remove(_viewContainerRef.indexOf(_viewRef));
     }
-    if (templateRef != null) {
-      this._insertedViewRef =
-          this._viewContainerRef.createEmbeddedView(templateRef);
+
+    if (_templateRef != null) {
+      _viewRef = _viewContainerRef.createEmbeddedView(_templateRef, _context);
     }
   }
 }
