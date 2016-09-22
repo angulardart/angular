3bcf200eba8d7d12210e72d4e01a168f460a885c
ntaoo <ntaoo.g@gmail.com>
2016-09-22 19:49:25 +0900

    PipeMetadata should be pure by default.

--- lib/src/compiler/compile_metadata.dart
+++ lib/src/compiler/compile_metadata.dart
@@ -747,7 +747,7 @@ class CompilePipeMetadata implements CompileMetadataWithType {
       List<LifecycleHooks> lifecycleHooks}) {
     this.type = type;
     this.name = name;
-    this.pure = pure == true;
+    this.pure = pure ?? true;
     this.lifecycleHooks = lifecycleHooks ?? [];
   }
   CompileIdentifierMetadata get identifier {
