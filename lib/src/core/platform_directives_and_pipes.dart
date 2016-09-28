63c6f8785e8d28c60f584e7a1ee3f522016d8b93
kulshekhar <kulshekhar@users.noreply.github.com>
2016-09-28 14:28:44 +0530

    Convert examples from Typescript to Dart

--- lib/src/core/platform_directives_and_pipes.dart
+++ lib/src/core/platform_directives_and_pipes.dart
@@ -1,49 +1,58 @@
 import "package:angular2/src/core/di.dart" show OpaqueToken;
 
-/// A token that can be provided when bootstraping an application to make an array of directives
-/// available in every component of the application.
+/// A token that can be provided when bootstraping an application to make an
+/// array of directives available in every component of the application.
 ///
 /// ### Example
 ///
-/// ```typescript
-/// import {PLATFORM_DIRECTIVES} from 'angular2/core';
-/// import {OtherDirective} from './myDirectives';
+/// ```dart
+/// import 'package:angular2/core.dart' show PLATFORM_DIRECTIVES;
+/// import './myDirectives' show OtherDirective;
 ///
-/// @Component({
-///   selector: 'my-component',
-///   template: `
-///     <!-- can use other directive even though the component does not list it in `directives` -->
-///     <other-directive></other-directive>
-///   `
-/// })
-/// export class MyComponent {
+/// @Component(
+///     selector: 'my-component',
+///     template: '''
+///       <!-- can use other directive even though the component does not list it
+///            in `directives` -->
+///
+///       <other-directive></other-directive>
+///     ''')
+/// class MyComponent {
 ///   ...
 /// }
 ///
-/// bootstrap(MyComponent, [provide(PLATFORM_DIRECTIVES, {useValue: [OtherDirective], multi:true})]);
+/// void main() {
+///   bootstrap(MyComponent, [
+///     provide(PLATFORM_DIRECTIVES, useValue: [OtherDirective], multi: true)
+///   ]);
+/// }
 /// ```
+///
 const OpaqueToken PLATFORM_DIRECTIVES =
     const OpaqueToken("Platform Directives");
 
-/// A token that can be provided when bootstraping an application to make an array of pipes
-/// available in every component of the application.
+/// A token that can be provided when bootstraping an application to make an
+/// array of pipes available in every component of the application.
 ///
 /// ### Example
 ///
-/// ```typescript
-/// import {PLATFORM_PIPES} from 'angular2/core';
-/// import {OtherPipe} from './myPipe';
-///
-/// @Component({
-///   selector: 'my-component',
-///   template: `
+/// ```dart
+/// import 'angular2/core' show PLATFORM_PIPES;
+/// import './myPipe' show OtherPipe;
+/// @Component(
+///     selector: 'my-component',
+///     template: '''
 ///     {{123 | other-pipe}}
-///   `
-/// })
-/// export class MyComponent {
+///   ''')
+/// class MyComponent {
 ///   ...
 /// }
 ///
-/// bootstrap(MyComponent, [provide(PLATFORM_PIPES, {useValue: [OtherPipe], multi:true})]);
+/// void main() {
+///   bootstrap(MyComponent, [
+///     provide(PLATFORM_PIPES, useValue: [OtherPipe], multi: true)
+///   ]);
+/// }
 /// ```
+///
 const OpaqueToken PLATFORM_PIPES = const OpaqueToken("Platform Pipes");
