8d87101dfd275d55f9a45b540cbf236a67b82497
Matan Lurey <matanl@google.com>
2016-10-17 13:09:42 -0700

    Initial commit: Skeleton of the new template parser.

---
+++ lib/src/modules/compiler/template_parser/test/sample_test.dart
@@ -0,0 +1,8 @@
+import 'package:angular2_template_parser/template_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('should run a test', () {
+    expect(sample(), 'Sample');
+  });
+}
---
+++ lib/src/modules/compiler/template_parser/tool/presubmit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Make sure dartfmt is run on everything
+# This assumes you have dart_style as a dev_dependency
+echo "Checking dartfmt..."
+NEEDS_DARTFMT="$(find benchmark lib test -name "*.dart" | xargs pub run dart_style:format -n)"
+if [[ ${NEEDS_DARTFMT} != "" ]]
+then
+  echo "FAILED"
+  echo "${NEEDS_DARTFMT}"
+  exit 1
+fi
+echo "PASSED"
+
+# Make sure we pass the analyzer
+echo "Checking dartanalyzer..."
+FAILS_ANALYZER="$(find benchmark lib test -name "*.dart" | xargs dartanalyzer --options analysis_options.yaml)"
+if [[ $FAILS_ANALYZER == *"[error]"* ]]
+then
+  echo "FAILED"
+  echo "${FAILS_ANALYZER}"
+  exit 1
+fi
+echo "PASSED"
+
+# Fail on anything that fails going forward.
+set -e
+
+pub run test
