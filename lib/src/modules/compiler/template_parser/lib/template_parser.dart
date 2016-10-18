3caf94945ba040428b4425acd6dc9c6a86b3ef77
Matan Lurey <matanl@google.com>
2016-10-17 17:01:20 -0700

    Add utils and parser base.

--- lib/src/modules/compiler/template_parser/lib/template_parser.dart
+++ lib/src/modules/compiler/template_parser/lib/template_parser.dart
@@ -1,2 +1,10 @@
-/// Returns the string 'Sample'.
-String sample() => 'Sample';
+/// Placeholder interface for a recognized template AST.
+abstract class NgTemplateAst implements List<NgTemplateAst> {}
+
+/// Parses an Angular Dart template into a concrete AST.
+///
+/// See `GRAMMAR.md` for more information.
+abstract class NgTemplateParser {
+  /// Parses [template] into a series of root [NgTemplateAst]s.
+  List<NgTemplateAst> parse(String template);
+}
