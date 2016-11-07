311684ee9bbb98d2ae6776c168d9d1669c56afa2
Jonah Williams <jonahwilliams@users.noreply.github.com>
2016-11-07 07:18:11 -0800

    imported analyzer for expression parsing and implemented simple method for parsing (#183)

--- lib/src/modules/compiler/template_parser/lib/src/utils.dart
+++ lib/src/modules/compiler/template_parser/lib/src/utils.dart
@@ -1,4 +1,45 @@
 import 'package:charcode/charcode.dart';
+import 'package:analyzer/analyzer.dart';
+import 'package:analyzer/src/dart/scanner/scanner.dart';
+import 'package:analyzer/src/dart/scanner/reader.dart';
+// import 'package:analyzer/src/dart/ast/ast.dart';
+import 'package:analyzer/src/generated/parser.dart';
+import 'package:analyzer/src/string_source.dart';
+
+/// Attempts to parse a String into an unresolved AST expression.
+Expression parseAngularExpression(String contents, String name,
+    {bool suppressErrors: false, bool parseFunctionBodies: true}) {
+  var source = new StringSource(contents, name);
+  var reader = new CharSequenceReader(contents);
+  var errorCollector = new _ErrorCollector();
+  var scanner = new Scanner(source, reader, errorCollector);
+  var token = scanner.tokenize();
+  var parser = new Parser(source, errorCollector)
+    ..parseFunctionBodies = parseFunctionBodies;
+  var unit = parser.parseExpression(token);
+
+  if (errorCollector.hasErrors && !suppressErrors) throw errorCollector.group;
+
+  return unit;
+}
+
+/// Taken from package:analyzer/analyzer.dart.
+/// A simple error listener that collects errors into an [AnalyzerErrorGroup].
+class _ErrorCollector extends AnalysisErrorListener {
+  final _errors = <AnalysisError>[];
+
+  _ErrorCollector();
+
+  /// The group of errors collected.
+  AnalyzerErrorGroup get group =>
+      new AnalyzerErrorGroup.fromAnalysisErrors(_errors);
+
+  /// Whether any errors where collected.
+  bool get hasErrors => _errors.isNotEmpty;
+
+  @override
+  void onError(AnalysisError error) => _errors.add(error);
+}
 
 /// A `[` character.
 const int $openProperty = $open_bracket;
