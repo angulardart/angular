fa5e00b82f2714bf2739d55c8c8000e0d031a579
matanl <matanl@google.com>
2016-11-18 12:24:29 -0800

    Cleanup ng_template_outlet.dart.
    (Remove 'this.', inconsistent use of ' and ", etc)
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=139609258

--- lib/src/common/directives/ng_template_outlet.dart
+++ lib/src/common/directives/ng_template_outlet.dart
@@ -1,24 +1,28 @@
-import "package:angular2/core.dart"
+import 'package:angular2/core.dart'
     show Directive, Input, ViewContainerRef, ViewRef, TemplateRef;
 
 /// Creates and inserts an embedded view based on a prepared `TemplateRef`.
 ///
 /// ### Syntax
 /// - `<template [ngTemplateOutlet]="templateRefExpression"></template>`
-@Directive(selector: "[ngTemplateOutlet]")
+@Directive(selector: '[ngTemplateOutlet]')
 class NgTemplateOutlet {
   final ViewContainerRef _viewContainerRef;
+
   ViewRef _insertedViewRef;
+
   NgTemplateOutlet(this._viewContainerRef);
+
+  /// Sets the DOM to render the result of [templateRef].
+  ///
+  /// If the template is changed, the previous template is removed first.
   @Input()
   set ngTemplateOutlet(TemplateRef templateRef) {
     if (_insertedViewRef != null) {
-      _viewContainerRef
-          .remove(this._viewContainerRef.indexOf(this._insertedViewRef));
+      _viewContainerRef.remove(_viewContainerRef.indexOf(_insertedViewRef));
     }
     if (templateRef != null) {
-      this._insertedViewRef =
-          this._viewContainerRef.createEmbeddedView(templateRef);
+      _insertedViewRef = _viewContainerRef.createEmbeddedView(templateRef);
     }
   }
 }
