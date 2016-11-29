a6796b509571b634473cabd8ffdf5206f73d9914
kevmoo <kevmoo@google.com>
2016-11-29 08:58:46 -0800

    Fix import of reflection library
    ...and spelling
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=140484896

--- lib/reflection.dart
+++ lib/reflection.dart
@@ -1,4 +1,4 @@
-import 'src/core/reflection/reflector.dart';
+import 'src/core/reflection/reflection_capabilities.dart';
 import 'src/core/reflection/reflection.dart';
 
 /// Setup Angular to use runtime (`dart:mirrors`-based) reflection.
@@ -10,7 +10,7 @@ import 'src/core/reflection/reflection.dart';
 ///    code-generation-only testing model, but this is not deprecated yet.
 ///
 /// 2. **Deprecated**: You are relying on runtime reflection for your
-///    development cylce inside of Dartium, and use pub transformers at build
+///    development cycle inside of Dartium, and use pub transformers at build
 ///    time to replace them with code generation. All development should be done
 ///    using code generation or a bug should be filed.
 void allowRuntimeReflection() {
