d1b16f2cba694e69c023249d4d47f9cdfb31044d
matanl <matanl@google.com>
2016-12-06 17:06:00 -0800

    refactor(Directives): Remove ObservableListDiffFactory and NgPlural.
    It was deprecated in 2.1.0, and no longer used in public API.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=141248268

--- lib/src/common/directives.dart
+++ lib/src/common/directives.dart
@@ -4,8 +4,6 @@ export "directives/core_directives.dart" show CORE_DIRECTIVES;
 export "directives/ng_class.dart" show NgClass;
 export "directives/ng_for.dart" show NgFor;
 export "directives/ng_if.dart" show NgIf;
-export "directives/ng_plural.dart" show NgPlural, NgPluralCase, NgLocalization;
 export "directives/ng_style.dart" show NgStyle;
 export "directives/ng_switch.dart" show NgSwitch, NgSwitchWhen, NgSwitchDefault;
 export "directives/ng_template_outlet.dart" show NgTemplateOutlet;
-export "directives/observable_list_diff.dart";
