90608125f82dd8cc9ed3da50a0eb46a988e97cec
nbosch <nbosch@google.com>
2017-01-06 10:03:53 -0800

    Avoid writing a .dart.deferredCount in source_gen mode
    We allow writing arbitrary assets but this can't be used because it isn't
    declared as an output. We don't run the DeferredRewriter outside of barback so
    in source_gen mode it isn't useful to write it at all.
    
    Keeping the outputs to only those which are used by subsequent steps in the
    build will allow the codegen script to be more strict and reduce the amount of
    configuration necessary.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=143785347

--- lib/src/transform/directive_processor/transformer.dart
+++ lib/src/transform/directive_processor/transformer.dart
@@ -47,15 +47,18 @@ class DirectiveProcessor extends Transformer implements LazyTransformer {
       transform.addOutput(new Asset.fromString(
           _ngSummaryAssetId(primaryId), _encoder.convert(ngMeta.toJson())));
 
-      var deferredCount = 0;
-      if (ngMeta.ngDeps != null) {
-        deferredCount = ngMeta.ngDeps.imports.where((i) => i.isDeferred).length;
-      }
-      if (deferredCount > 0) {
-        // The existence of this file with the value != "0" signals
-        // DeferredRewriter that the associated .dart file needs attention.
-        transform.addOutput(new Asset.fromString(
-            _deferredAssetId(primaryId), deferredCount.toString()));
+      if (!options.checkDeferredImportInitialization) {
+        var deferredCount = 0;
+        if (ngMeta.ngDeps != null) {
+          deferredCount =
+              ngMeta.ngDeps.imports.where((i) => i.isDeferred).length;
+        }
+        if (deferredCount > 0) {
+          // The existence of this file with the value != "0" signals
+          // DeferredRewriter that the associated .dart file needs attention.
+          transform.addOutput(new Asset.fromString(
+              _deferredAssetId(primaryId), deferredCount.toString()));
+        }
       }
     }, log: transform.logger);
   }
