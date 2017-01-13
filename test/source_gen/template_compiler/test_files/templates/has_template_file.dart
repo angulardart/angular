88b4d5a94719e4232392aa7cce90df4b9a3c53e5
alorenzen <alorenzen@google.com>
2017-01-13 10:06:26 -0800

    SourceGen: Look up styles for components and views.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=144454659

--- test/source_gen/template_compiler/test_files/templates/has_template_file.dart
+++ test/source_gen/template_compiler/test_files/templates/has_template_file.dart
@@ -1,4 +1,7 @@
 import 'package:angular2/angular2.dart';
 
-@Component(selector: 'has-template-file', templateUrl: 'has_template_file.html')
+@Component(
+    selector: 'has-template-file',
+    templateUrl: 'has_template_file.html',
+    styleUrls: const ['has_template_file.css'])
 class HasTemplateFileComponent {}
--- test/source_gen/template_compiler/test_files/view_annotation.dart
+++ test/source_gen/template_compiler/test_files/view_annotation.dart
@@ -1,5 +1,8 @@
 import 'package:angular2/angular2.dart';
 
-@Component(selector: 'test-foo')
-@View(template: '<div *ngIf="true">Foo</div>', directives: const [NgIf])
+@Component(selector: 'test-foo', preserveWhitespace: false)
+@View(
+    template: '<div *ngIf="true">Foo</div>',
+    directives: const [NgIf],
+    styles: const ['div { font-size: 10px; }'])
 class TestFooComponent {}
