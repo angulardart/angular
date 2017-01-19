2ee362cb986e86d423b691ffb840e84ab625a492
alorenzen <alorenzen@google.com>
2017-01-19 12:36:31 -0800

    fix(Meta): Fix export clause to reflect updated type name.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=144995424

--- lib/src/meta.dart
+++ lib/src/meta.dart
@@ -2,4 +2,4 @@
 //
 // In the future we might expose this as a pkg/angular_meta for tooling.
 
-export 'package:angular2/src/meta/types.dart' show TypeSystem;
+export 'package:angular2/src/meta/types.dart' show MetadataTypes;
