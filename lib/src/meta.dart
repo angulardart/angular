de9495cd5a2d3c6407ec3e386bd0baf7c92bce1a
matanl <matanl@google.com>
2017-01-23 10:42:39 -0800

    docs(Meta): Document why we have a meta folder and meta.dart file.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=145304717

--- lib/src/meta.dart
+++ lib/src/meta.dart
@@ -1,5 +1,5 @@
 // Encapsulates the internal metadata analysis libraries.
 //
-// In the future we might expose this as a pkg/angular_meta for tooling.
+// In the future we might expose this as a pkg/angular_ast for tooling.
 
 export 'package:angular2/src/meta/types.dart' show MetadataTypes;
