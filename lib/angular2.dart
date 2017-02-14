4c499d61787c45f16b368f90381ccd74c8a45d0d
matanl <matanl@google.com>
2017-02-14 09:51:20 -0800

    refactor(API): Deprecate package:angular2/common.dart.
    This will just become package:angular2/angular2.dart.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=147484398

--- lib/angular2.dart
+++ lib/angular2.dart
@@ -6,7 +6,6 @@ library angular2;
 import 'dart:html';
 
 export 'package:angular2/core.dart';
-export 'package:angular2/common.dart';
 export 'package:angular2/src/core/angular_entrypoint.dart'
     show AngularEntrypoint;
 export 'package:angular2/src/core/application_tokens.dart'
@@ -14,6 +13,7 @@ export 'package:angular2/src/core/application_tokens.dart'
 export 'package:angular2/src/platform/dom/dom_tokens.dart';
 export 'package:angular2/src/platform/dom/events/event_manager.dart'
     show EventManagerPlugin;
+
 // Attention: Don't use angular2/src/compiler/compiler.dart
 // for re exports as this will pull in the whole compiler,
 // even if only some parts are shown.
@@ -23,6 +23,12 @@ export 'package:angular2/src/platform/dom/events/event_manager.dart'
 export 'package:angular2/src/compiler/url_resolver.dart';
 export 'package:angular2/src/compiler/directive_resolver.dart';
 export 'package:angular2/src/compiler/view_resolver.dart';
+
+// Used to be package:angular2/common.dart.
+export "src/common/common_directives.dart";
+export "src/common/directives.dart";
+export "src/common/forms.dart";
+export "src/common/pipes.dart";
 
 /// TODO(ferhat): Remove once dynamic(s) are changed in codegen and class.
 /// This prevents unused import error in dart_analyzed_library build.
--- lib/common.dart
+++ lib/common.dart
@@ -1,3 +1,4 @@
+@Deprecated('Import package:angular2/angular2.dart instead')
 library angular2.common;
 
 export "src/common/common_directives.dart";
