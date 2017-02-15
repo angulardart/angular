7209b94389a73776d90ae96229f382d452ccfd69
matanl <matanl@google.com>
2017-02-15 15:08:49 -0800

    feat(Compiler): Add more suppport to StaticTypes|AngularMetadataTypes.
    * StaticTypes.fromResolver now includes both metadata libraries for Angular.
    * Added memoization for Type --> DartType for a bit of sanity.
    * Added more test cases that use both the mirrors and static resolution.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=147650450

--- test/meta/types_test.dart
+++ test/meta/types_test.dart
@@ -19,10 +19,13 @@ void main() {
   final runFiles = Platform.environment['RUNFILES'];
   const dirPackages = const String.fromEnvironment('packages-dir');
   const metaFile = 'angular2/lib/src/core/metadata.dart';
+  const decoratorFile = 'angular2/lib/src/core/di/decorators.dart';
+
   if (runFiles == null || dirPackages == null) {
     return;
   }
 
+  String libDecorators;
   String libMetadata;
   Completer<Null> tearDownResolver;
   Completer<Resolver> waitForResolver;
@@ -34,6 +37,8 @@ void main() {
     // Read the real `metadata.dart` file to use as a fake source.
     final metaPath = p.join(runFiles, dirPackages, metaFile);
     libMetadata = new File(metaPath).readAsStringSync();
+    final decoPath = p.join(runFiles, dirPackages, decoratorFile);
+    libDecorators = new File(decoPath).readAsStringSync();
 
     // Run our builder: we'll use it just to extract the Resolver for tests.
     //
@@ -45,15 +50,46 @@ void main() {
         return tearDownResolver.future;
       })),
       {
+        'angular2|lib/src/core/di/decorators.dart': libDecorators,
         'angular2|lib/src/core/metadata.dart': libMetadata,
         'angular2|lib/angular2.dart': r'''
+          // Not really angular2.dart, but close as far as metadata goes.
+          export 'package:angular2/src/core/di/decorators.dart';
           export 'package:angular2/src/core/metadata.dart';
         ''',
         'stub|lib/stub.dart': r'''
           import 'package:angular2/angular2.dart';
 
+          // Examples of @Directive and @Component usage.
+          // -------------------------------------------------------------------
+
           @Directive()
           class StubDirective {}
+
+          @Component()
+          class StubComponent {}
+
+          @Injectable()
+          class StubInjectable {}
+
+          @Pipe()
+          class StubPipe {}
+
+          // Examples of trying to trick our type system. Not supported.
+          // -------------------------------------------------------------------
+
+          class CustomDirective implements Directive {
+            const CustomDirective();
+          }
+          class CustomComponent implements Component {
+            const CustomComponent();
+          }
+
+          @CustomDirective()
+          class StubCustomDirective {}
+
+          @CustomComponent()
+          class StubCustomComponent {}
         '''
       },
       isInput: (p) => p.startsWith('stub'),
@@ -67,12 +103,24 @@ void main() {
   group('$AngularMetadataTypes', () {
     Resolver resolver;
     LibraryElement stubLibrary;
+
     ClassElement stubDirectiveClass;
+    ClassElement stubComponentClass;
+    ClassElement stubPipeClass;
+
+    ClassElement stubCustomDirectiveClass;
+    ClassElement stubCustomComponentClass;
 
     setUpAll(() async {
       resolver = await waitForResolver.future;
       stubLibrary = resolver.getLibrary(new AssetId('stub', 'lib/stub.dart'));
+
       stubDirectiveClass = stubLibrary.getType('StubDirective');
+      stubComponentClass = stubLibrary.getType('StubComponent');
+      stubPipeClass = stubLibrary.getType('StubPipe');
+
+      stubCustomDirectiveClass = stubLibrary.getType('StubCustomDirective');
+      stubCustomComponentClass = stubLibrary.getType('StubCustomComponent');
     });
 
     test('should be able to run test cases', () {
@@ -93,12 +141,50 @@ void main() {
           types = new AngularMetadataTypes(createStaticTypeResolver());
         });
 
-        test('should detect StubDirective as an @Directive', () {
-          expect(types.isDirectiveClass(stubDirectiveClass), isTrue);
+        group('detects StubComponent as', () {
+          test('a @Directive', () {
+            expect(types.isDirectiveClass(stubComponentClass), isTrue);
+          });
+
+          test('a @Component', () {
+            expect(types.isComponentClass(stubComponentClass), isTrue);
+          });
+
+          test('a @Injectable', () {
+            expect(types.isInjectableClass(stubComponentClass), isTrue);
+          });
         });
 
-        test('should not detect StubDirective as a @Component', () {
-          expect(types.isComponentClass(stubDirectiveClass), isFalse);
+        group('detects StubDirective as', () {
+          test('a @Directive', () {
+            expect(types.isDirectiveClass(stubDirectiveClass), isTrue);
+          });
+
+          test('not a @Component', () {
+            expect(types.isComponentClass(stubDirectiveClass), isFalse);
+          });
+
+          test('a @Injectable', () {
+            expect(types.isInjectableClass(stubDirectiveClass), isTrue);
+          });
+        });
+
+        group('detects StubPipe as', () {
+          test('a @Pipe', () {
+            expect(types.isPipeClass(stubPipeClass), isTrue);
+          });
+
+          test('a @Injectable', () {
+            expect(types.isInjectableClass(stubPipeClass), isTrue);
+          });
+        });
+
+        test('should not allow sub-typing @Directive', () {
+          expect(types.isDirectiveClass(stubCustomDirectiveClass), isFalse);
+        });
+
+        test('should not allow sub-typing @Component', () {
+          expect(types.isComponentClass(stubCustomComponentClass), isFalse);
         });
       });
     });
