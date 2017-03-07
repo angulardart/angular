49cc1e9f8ee7174125fe46007933acef877a0190
leonsenft <leonsenft@google.com>
2017-03-07 14:23:52 -0800

    Automated g4 rollback of changelist 147739393.
    *** Reason for rollback ***
    
    This wasn't the performance regression culprit.
    
    *** Original change description ***
    
    Scopes components on demand.
    
    Before changes:
    If a component had any styles, its host element and content elements would be
    scoped with a host and content class respectively.
    
    After changes:
    Host element and content elements are scoped only if styles reference the host
    or content respectively.
    
    Note that this cha...
    
    ***
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=149466349

--- lib/src/core/render/api.dart
+++ lib/src/core/render/api.dart
@@ -28,11 +28,9 @@ class RenderComponentType {
   final ViewEncapsulation encapsulation;
   List<dynamic /* String | List < dynamic > */ > templateStyles;
 
-  static const COMPONENT_VARIABLE = '%COMP%';
   static const HOST_STYLE_PREFIX = '_nghost-';
   static const CONTENT_STYLE_PREFIX = '_ngcontent-';
-  static const HOST_ATTR = '${HOST_STYLE_PREFIX}$COMPONENT_VARIABLE';
-  static const CONTENT_ATTR = '${CONTENT_STYLE_PREFIX}$COMPONENT_VARIABLE';
+
   // Content attribute value assigned to elements in template or null if
   // no assignment is required.
   String _contentAttr;
@@ -51,8 +49,18 @@ class RenderComponentType {
       stylesHost.addStyles(this._styles);
     }
     if (encapsulation == ViewEncapsulation.Emulated) {
-      _contentAttr = _shimContentAttribute(id);
-      _hostAttr = _shimHostAttribute(id);
+      for (var i = _styles.length - 1; i >= 0; i--) {
+        if (_styles[i].contains(CONTENT_STYLE_PREFIX)) {
+          _contentAttr = CONTENT_STYLE_PREFIX + id;
+          break;
+        }
+      }
+      for (var i = styles.length - 1; i >= 0; i--) {
+        if (_styles[i].contains(HOST_STYLE_PREFIX)) {
+          _hostAttr = HOST_STYLE_PREFIX + id;
+          break;
+        }
+      }
     }
   }
 
@@ -61,12 +69,6 @@ class RenderComponentType {
   String get hostAttr => _hostAttr;
 
   List<String> get styles => _styles;
-
-  String _shimContentAttribute(String componentShortId) =>
-      CONTENT_ATTR.replaceAll(COMPONENT_REGEX, componentShortId);
-
-  String _shimHostAttribute(String componentShortId) =>
-      HOST_ATTR.replaceAll(COMPONENT_REGEX, componentShortId);
 
   List<String> _flattenStyles(
       String compId,
