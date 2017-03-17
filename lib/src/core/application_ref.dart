1b26f8862f49bbc35cb6c00f5c8e5680269ab404
srawlins <srawlins@google.com>
2017-03-17 13:32:45 -0700

    Correctly (now that we have FutureOr) type ApplicationRef's run().
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=150477657

--- lib/src/core/application_ref.dart
+++ lib/src/core/application_ref.dart
@@ -171,7 +171,7 @@ abstract class ApplicationRef {
   /// Runs the given [callback] in the zone and returns the result of the call.
   ///
   /// Exceptions will be forwarded to the ExceptionHandler and rethrown.
-  run<R>(R callback());
+  run<R>(FutureOr<R> callback());
 
   /// Bootstrap a new component at the root level of the application.
   ///
@@ -299,7 +299,7 @@ class ApplicationRefImpl extends ApplicationRef {
   // When FutureOr<T> lands, this can be expressed as FutureOr<R>.  For now
   // leave it as dynamic, but pass a generic type so that the returned
   // Future (if any) has the correct reified type.
-  run<R>(R callback()) {
+  run<R>(FutureOr<R> callback()) {
     // TODO(matanl): Remove support for futures inside of appRef.run.
     var zone = injector.get(NgZone);
     var result;
