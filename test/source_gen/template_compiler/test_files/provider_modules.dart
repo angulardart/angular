e993155e95ae21027cada6315749adc8169310ea
matanl <matanl@google.com>
2017-03-17 09:53:03 -0700

    Highlight issues around analysis summaries for resolving providers.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=150450110

---
+++ test/source_gen/template_compiler/test_files/provider_modules.dart
@@ -0,0 +1,31 @@
+import 'package:angular2/angular2.dart';
+
+@Injectable()
+class FooService {}
+
+const fooProviders = const [
+  FooService,
+];
+
+const fooToken = const OpaqueToken('fooToken');
+const barToken = const OpaqueToken('barToken');
+const bazToken = const OpaqueToken('bazToken');
+
+const List<String> someValues = const ['a', 'b', 'c'];
+
+@Injectable()
+List<String> getSomeValues() => someValues;
+
+const barProviders = const [
+  fooProviders,
+  const Provider(fooToken, useValue: const Duration(seconds: 500)),
+  const Provider(barToken, useValue: someValues),
+  const Provider(bazToken, useFactory: getSomeValues),
+];
+
+@Component(
+  selector: 'provider-modules',
+  providers: const [barProviders],
+  template: '',
+)
+class ProviderModulesComponent {}
