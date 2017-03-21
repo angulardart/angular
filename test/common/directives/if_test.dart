8a1e76cf813699c083a4aaac5a0b17cd5f9194b5
matanl <matanl@google.com>
2017-03-21 15:04:02 -0700

    fix(NgIf): Restore use of `checkBinding` for dev-mode.
    This ensures that the checked value does not change during change detection, but it only occurs in dev-mode (assertions enabled, etc).
    
    * Added a regression test to if_test.dart
    * Made if_test.dart more consistent and debuggable
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=150807999

--- test/common/directives/if_test.dart
+++ test/common/directives/if_test.dart
@@ -12,23 +12,23 @@ void main() {
   group('ngIf', () {
     tearDown(() => disposeAnyRunningTest());
 
-    test("should work in a template attribute", () async {
+    test('should work in a template attribute', () async {
       var testBed = new NgTestBed<NgIfInTemplateAttributeComponent>();
       var testFixture = await testBed.create();
       Element element = testFixture.rootElement;
-      expect(element.querySelectorAll("copy-me"), hasLength(1));
-      expect(element.innerHtml, contains('hello'));
+      expect(element.querySelectorAll('copy-me'), hasLength(1));
+      expect(element.innerHtml, contains('Hello'));
     });
 
-    test("should work in a template element", () async {
+    test('should work in a template element', () async {
       var testBed = new NgTestBed<NgIfInTemplateComponent>();
       var testFixture = await testBed.create();
       Element element = testFixture.rootElement;
-      expect(element.querySelectorAll("copy-me"), hasLength(1));
-      expect(element.innerHtml, contains('hello2'));
+      expect(element.querySelectorAll('copy-me'), hasLength(1));
+      expect(element.innerHtml, contains('Hello2'));
     });
 
-    test("should toggle node when condition changes", () async {
+    test('should toggle node when condition changes', () async {
       var testBed = new NgTestBed<NgIfToggleTestComponent>();
       NgTestFixture<NgIfToggleTestComponent> testFixture =
           await testBed.create();
@@ -37,20 +37,20 @@ void main() {
       await testFixture.update((NgIfToggleTestComponent component) {
         component.booleanCondition = false;
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(0));
+      expect(element.querySelectorAll('copy-me'), hasLength(0));
 
       await testFixture.update((NgIfToggleTestComponent component) {
         component.booleanCondition = true;
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(1));
+      expect(element.querySelectorAll('copy-me'), hasLength(1));
 
       await testFixture.update((NgIfToggleTestComponent component) {
         component.booleanCondition = false;
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(0));
+      expect(element.querySelectorAll('copy-me'), hasLength(0));
     });
 
-    test("should handle nested if correctly", () async {
+    test('should handle nested if correctly', () async {
       var testBed = new NgTestBed<NgIfNestedTestComponent>();
       NgTestFixture<NgIfNestedTestComponent> testFixture =
           await testBed.create();
@@ -59,112 +59,167 @@ void main() {
       await testFixture.update((NgIfNestedTestComponent component) {
         component.booleanCondition = false;
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(0));
-      expect(element.innerHtml.contains('hello'), isFalse);
+      expect(element.querySelectorAll('copy-me'), hasLength(0));
+      expect(element.innerHtml, isNot(contains('Hello')));
 
       await testFixture.update((NgIfNestedTestComponent component) {
         component.booleanCondition = true;
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(1));
-      expect(element.innerHtml.contains('hello'), isTrue);
+      expect(element.querySelectorAll('copy-me'), hasLength(1));
+      expect(element.innerHtml, contains('Hello'));
 
       await testFixture.update((NgIfNestedTestComponent component) {
         component.nestedBooleanCondition = false;
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(0));
-      expect(element.innerHtml.contains('hello'), isFalse);
+      expect(element.querySelectorAll('copy-me'), hasLength(0));
+      expect(element.innerHtml, isNot(contains('Hello')));
 
       await testFixture.update((NgIfNestedTestComponent component) {
         component.nestedBooleanCondition = true;
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(1));
-      expect(element.innerHtml.contains('hello'), isTrue);
+      expect(element.querySelectorAll('copy-me'), hasLength(1));
+      expect(element.innerHtml, contains('Hello'));
 
       await testFixture.update((NgIfNestedTestComponent component) {
         component.booleanCondition = false;
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(0));
-      expect(element.innerHtml.contains('hello'), isFalse);
+      expect(element.querySelectorAll('copy-me'), hasLength(0));
+      expect(element.innerHtml, isNot(contains('Hello')));
     });
 
-    test("should work in a template attribute", () async {
+    test('should work in a template attribute', () async {
       var testBed = new NgTestBed<NgIfMultiUpdateTestComponent>();
       NgTestFixture<NgIfMultiUpdateTestComponent> testFixture =
           await testBed.create();
       Element element = testFixture.rootElement;
       // Check startup.
-      expect(element.querySelectorAll("copy-me"), hasLength(3));
-      expect(element.text, "helloNumberhelloStringhelloFunction");
+      expect(element.querySelectorAll('copy-me'), hasLength(3));
+      expect(element.text, 'helloNumberhelloStringhelloFunction');
 
       await testFixture.update((NgIfMultiUpdateTestComponent component) {
         component.numberCondition = 0;
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(1));
-      expect(element.text, "helloString");
+      expect(element.querySelectorAll('copy-me'), hasLength(1));
+      expect(element.text, 'helloString');
 
       await testFixture.update((NgIfMultiUpdateTestComponent component) {
         component.numberCondition = 1;
         component.stringCondition = 'bar';
       });
-      expect(element.querySelectorAll("copy-me"), hasLength(1));
-      expect(element.text, "helloNumber");
+      expect(element.querySelectorAll('copy-me'), hasLength(1));
+      expect(element.text, 'helloNumber');
       await testFixture.update((NgIfMultiUpdateTestComponent component) {
         component.booleanCondition = false;
       });
     });
+
+    test('should throw during change detection if getter changes', () async {
+      var testBed = new NgTestBed<NgIfThrowsDuringChangeDetection>();
+      var fixture = await testBed.create();
+      expect(
+        fixture.update((c) => c.startFailing = true),
+        throwsInAngular(isExpressionChanged),
+      );
+    });
   });
 }
 
+const isExpressionChanged =
+    const isInstanceOf<ExpressionChangedAfterItHasBeenCheckedException>();
+
 @Component(
-    selector: 'ngif-intemplate-attr-test',
-    template: '<div><copy-me template="ngIf booleanCondition">'
-        'hello</copy-me></div>',
-    directives: const [NgIf])
+  selector: 'ngif-intemplate-attr-test',
+  template: r'''
+      <div>
+        <copy-me template="ngIf booleanCondition">Hello</copy-me>
+      </div>
+    ''',
+  directives: const [NgIf],
+)
 class NgIfInTemplateAttributeComponent {
   bool booleanCondition = true;
 }
 
 @Component(
-    selector: 'ngif-intemplate-test',
-    template: '<div><template [ngIf]="booleanCondition">'
-        '<copy-me>hello2</copy-me></template></div>',
-    directives: const [NgIf])
+  selector: 'ngif-intemplate-test',
+  template: r'''
+      <div>
+        <template [ngIf]="booleanCondition">
+          <copy-me>Hello2</copy-me>
+        </template>
+      </div>
+    ''',
+  directives: const [NgIf],
+)
 class NgIfInTemplateComponent {
   bool booleanCondition = true;
 }
 
 @Component(
-    selector: 'ngif-toggle-test',
-    template: '<div><copy-me template="ngIf booleanCondition">hello</copy-me>'
-        '</div>',
-    directives: const [NgIf])
+  selector: 'ngif-toggle-test',
+  template: r'''
+      <div>
+        <copy-me template="ngIf booleanCondition">
+          Hello
+        </copy-me>
+      </div>
+    ''',
+  directives: const [NgIf],
+)
 class NgIfToggleTestComponent {
   bool booleanCondition = true;
 }
 
 @Component(
-    selector: 'ngif-nested-test',
-    template: '<div><template [ngIf]="booleanCondition">'
-        '<copy-me *ngIf="nestedBooleanCondition">hello</copy-me>'
-        '</template></div>',
-    directives: const [NgIf])
+  selector: 'ngif-nested-test',
+  template: r'''
+      <div>
+        <template [ngIf]="booleanCondition">
+          <copy-me *ngIf="nestedBooleanCondition">Hello</copy-me>
+        </template>
+      </div>
+    ''',
+  directives: const [NgIf],
+)
 class NgIfNestedTestComponent {
   bool booleanCondition = true;
   bool nestedBooleanCondition = true;
 }
 
 @Component(
-    selector: 'ngif-multiupdate-test',
-    template: '<div>'
-        '<copy-me template="ngIf numberCondition + 1 >= 2">helloNumber</copy-me>'
-        '<copy-me template="ngIf stringCondition == \'foo\'">helloString</copy-me>'
-        '<copy-me template="ngIf functionCondition(stringCondition, numberCondition)">helloFunction</copy-me>'
-        '</div>',
-    directives: const [NgIf])
+  selector: 'ngif-multiupdate-test',
+  template: r'''
+      <div>
+        <copy-me template="ngIf numberCondition + 1 >= 2">helloNumber</copy-me>
+        <copy-me template="ngIf stringCondition == 'foo'">helloString</copy-me>
+        <copy-me template="ngIf functionCondition(stringCondition, numberCondition)">helloFunction</copy-me>
+      </div>''',
+  directives: const [NgIf],
+  // Important! We are checking the *exact* innerText for this component.
+  preserveWhitespace: false,
+)
 class NgIfMultiUpdateTestComponent {
   bool booleanCondition = true;
   bool nestedBooleanCondition = true;
   num numberCondition = 1;
   String stringCondition = 'foo';
-  bool functionCondition(s, n) => s == "foo" && n == 1;
+  bool functionCondition(s, n) => s == 'foo' && n == 1;
+}
+
+@Component(
+  selector: 'ngif-checkbinding-test',
+  template: r'''
+    <template [ngIf]="startFailing">
+      <div *ngIf="value">Hello</div>
+    </template>
+  ''',
+  directives: const [NgIf],
+)
+class NgIfThrowsDuringChangeDetection {
+  bool _value = false;
+
+  // This is considered illegal and should throw in dev-mode.
+  bool get value => _value = !_value;
+
+  bool startFailing = false;
 }
