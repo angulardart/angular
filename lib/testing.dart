07215d7e432d8f3e15785b1c88445f03e7e39e69
matanl <matanl@google.com>
2017-03-27 10:20:40 -0700

    feat(Testing): Add a `isDebugMode` helper to angular2/testing.dart.
    Users internally have requested an API to spot check what the generated code of
    a component/application was created with - i.e. debug or release mode. For
    example, verifying that all benchmarks are run in RELEASE mode only.
    
    # All fails in smoketest are content-shell timeouts.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=151336851

--- lib/testing.dart
+++ lib/testing.dart
@@ -4,6 +4,11 @@
 /// test module explicitly.
 library angular2.testing;
 
+import 'package:angular2/src/core/change_detection.dart';
+import 'package:angular2/src/core/linker/view_ref.dart';
+import 'package:angular2/src/debug/debug_app_view.dart';
+import 'package:meta/meta.dart';
+
 export "src/compiler/xhr_mock.dart" show MockXHR;
 export "src/debug/debug_node.dart" show DebugElement, By;
 export "src/mock/directive_resolver_mock.dart" show MockDirectiveResolver;
@@ -14,3 +19,39 @@ export "src/testing/fake_async.dart";
 export "src/testing/test_component_builder.dart"
     show ComponentFixture, TestComponentBuilder;
 export "src/testing/test_injector.dart";
+
+/// Returns whether [ChangeDetectorRef] was generated in debug mode.
+///
+/// When running automated performance benchmarks you may want to ensure that
+/// the application is running as release:
+/// ```
+/// import 'package:angular2/testing.dart';
+///
+/// @Component(...)
+/// class RootComponent {
+///   RootComponent(ChangeDetectorRef changeDetectorRef) {
+///     if (isDebugMode(changeDetectorRef)) {
+///       throw 'Performance benchmarks should be run in release mode.';
+///     }
+///   }
+/// }
+/// ```
+@visibleForTesting
+bool isDebugMode(ChangeDetectorRef changeDetectorRef) {
+  // DO NOT COPY AND PASTE. THIS IS NOT PART OF THE PUBLIC API.
+  return (changeDetectorRef as ViewRefImpl).appView is DebugAppView;
+}
+
+@visibleForTesting
+void assertReleaseMode(ChangeDetectorRef changeDetectorRef) {
+  if (isDebugMode(changeDetectorRef)) {
+    throw 'Expected RELEASE mode, got DEBUG mode. Check your codegen options';
+  }
+}
+
+@visibleForTesting
+void assertDebugMode(ChangeDetectorRef changeDetectorRef) {
+  if (!isDebugMode(changeDetectorRef)) {
+    throw 'Expected DEBUG mode, got RELEASE mode. Check your codegen options';
+  }
+}
