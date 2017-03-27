198d261c1ee71f6929b78f16acff7168ae98a1a0
ferhat <ferhat@google.com>
2017-03-27 16:14:37 -0700

    Add initial test harness for !deferred loaded component.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=151386244

---
+++ test/core/linker/deferred_component_test.dart
@@ -0,0 +1,31 @@
+@Tags(const ['codegen'])
+@TestOn('browser')
+library angular2.test.core.linker.deferred_component_test;
+
+import 'dart:html';
+
+import 'package:angular2/angular2.dart';
+import 'package:angular_test/angular_test.dart';
+import 'package:test/test.dart';
+import 'deferred_view.dart';
+
+void main() {
+  group('Property access', () {
+    tearDown(() => disposeAnyRunningTest());
+    test('should load deferred component', () async {
+      var testBed = new NgTestBed<SimpleContainerTest>();
+      var testFixture = await testBed.create();
+      Element element = testFixture.rootElement;
+      Element deferredView = element.querySelector('my-deferred-view');
+      expect(deferredView, isNotNull);
+    });
+  });
+}
+
+@Component(
+    selector: 'simple-container',
+    template: '<section>'
+        '<my-deferred-view !deferred></my-deferred-view>'
+        '</section>',
+    directives: const [DeferredViewComponent])
+class SimpleContainerTest {}
---
+++ test/core/linker/deferred_view.dart
@@ -0,0 +1,27 @@
+import 'package:angular2/angular2.dart';
+
+/// Sample component used to test generating code for deferred loading of
+/// components.
+@Component(
+    selector: 'my-deferred-view',
+    template: '<div id="titleDiv" (click)="doClick()" '
+        '[attr.selected]="selected">'
+        '{{title}}</div>')
+class DeferredViewComponent extends SomeBaseClass {
+  bool selected = false;
+
+  @Input()
+  set title(String value) {
+    titleBase = value;
+  }
+
+  String get title => titleBase;
+
+  void doClick() {
+    selected = true;
+  }
+}
+
+class SomeBaseClass {
+  String titleBase;
+}
