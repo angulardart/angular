be17143450b4c190411f90d55c2911a7de623342
matanl <matanl@google.com>
2017-03-28 15:27:52 -0700

    refactor(Test): Cleanup `host_events_test.dart` and add more test cases.
    I did this while investigating https://github.com/dart-lang/angular2/issues/309
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=151507268

--- test/core/linker/host_events_test.dart
+++ test/core/linker/host_events_test.dart
@@ -1,7 +1,6 @@
 @Tags(const ['codegen'])
 @TestOn('browser && !js')
-library angular2.test.core.linker.host_events_test;
-
+import 'dart:async';
 import 'dart:html';
 
 import 'package:angular2/angular2.dart';
@@ -9,46 +8,88 @@ import 'package:angular_test/angular_test.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Events', () {
-    tearDown(() => disposeAnyRunningTest());
-    test('defined inside component itself should add listeners', () async {
-      var testBed = new NgTestBed<ComponentWithEventTest>();
-      var testFixture = await testBed.create();
-      Element element =
-          testFixture.rootElement.querySelector('component-with-event > input');
-      expect(element, isNotNull);
-      expect(element.attributes["has-focus"], 'false');
-      element.focus();
-      await testFixture.update();
-      expect(element.attributes["has-focus"], 'true');
+  group('Events defined', () {
+    Element activeElement;
+    NgTestFixture testFixture;
+
+    tearDown(disposeAnyRunningTest);
+
+    /// Runs a comment test based on a populated [activeElement].
+    Future<Null> _commonFocusTest({Element checkElement}) async {
+      checkElement ??= activeElement;
+      expect(activeElement, isNotNull, reason: 'Did not find an element');
+      expect(checkElement.attributes['has-focus'], 'false');
+      await testFixture.update((_) {
+        activeElement.focus();
+      });
+      expect(
+        checkElement.attributes['has-focus'],
+        'true',
+        reason: 'Expected an element to have reported focus',
+      );
+    }
+
+    test('inside a component\'s template should add listeners', () async {
+      const selector = 'component-with-event > input';
+      testFixture = await new NgTestBed<ComponentWithEventTest>().create();
+      activeElement = testFixture.rootElement.querySelector(selector);
+      await _commonFocusTest();
     });
-    test('defined on host should add listeners', () async {
-      var testBed = new NgTestBed<ComponentWithHostEvent>();
-      var testFixture = await testBed.create();
-      Element divElement = testFixture.rootElement
-          .querySelector('component-with-hostevent > div');
+
+    test('inside a component\'s host property should add listeners', () async {
+      const selector = 'component-with-hostevent > div';
+      testFixture = await new NgTestBed<ComponentWithHostEvent>().create();
+      Element divElement = testFixture.rootElement.querySelector(selector);
+      activeElement = divElement.parent;
+      activeElement.tabIndex = 1;
+      await _commonFocusTest(checkElement: divElement);
+    });
+
+    test('inside a component\'s annotations should add listeners', () async {
+      const selector = 'component-with-annotation > div';
+      testFixture = await new NgTestBed<ComponentWithHostAnnotation>().create();
+      Element divElement = testFixture.rootElement.querySelector(selector);
       expect(divElement, isNotNull);
-      Element hostElement = divElement.parent;
-      hostElement.tabIndex = 1;
-      hostElement.focus();
-      await testFixture.update();
-      expect(divElement.attributes['has-focus'], 'true');
+      activeElement = divElement.parent;
+      activeElement.tabIndex = 1;
+      await _commonFocusTest(checkElement: divElement);
+    });
+
+    test('inside a component\'s annotation supports an event', () async {
+      const selector = 'component-with-annotation2 > div';
+      testFixture = await new NgTestBed<ComponentWithHost$Event>().create();
+      Element divElement = testFixture.rootElement.querySelector(selector);
+      expect(divElement, isNotNull);
+      expect(divElement.attributes['was-clicked'], 'false');
+      await testFixture.update((_) {
+        testFixture.rootElement.click();
+      });
+      expect(divElement.attributes['was-clicked'], 'true');
     });
   });
 }
 
 @Component(
-    selector: 'component-with-event-test',
-    template: '<component-with-event></component-with-event>',
-    directives: const [ComponentWithEvent])
+  selector: 'component-with-event-test',
+  template: '<component-with-event></component-with-event>',
+  directives: const [ComponentWithEvent],
+)
 class ComponentWithEventTest {}
 
+/// Tests that `(event)="..."` works.
 @Component(
-    selector: 'component-with-event',
-    template: '<input id="my_element" (focus)="onFocus()" (blur)="onBlur()" '
-        '[attr.has-focus]="hasFocus">')
+  selector: 'component-with-event',
+  template: r'''
+    <input
+      id="my_element"
+      (focus)="onFocus()"
+      (blur)="onBlur()"
+      [attr.has-focus]="hasFocus">
+  ''',
+)
 class ComponentWithEvent {
   bool hasFocus = false;
+
   void onFocus() {
     hasFocus = true;
   }
@@ -58,17 +99,17 @@ class ComponentWithEvent {
   }
 }
 
+/// Tests that `@Component(host: ...)` works.
 @Component(
-    selector: 'component-with-hostevent',
-    host: const {
-      '(focus)': 'onFocus()',
-      '(blur)': 'onBlur()',
-      '(click)': 'toggled = !toggled'
-    },
-    template: '<div [attr.has-focus]="hasFocus"></div>')
+  selector: 'component-with-hostevent',
+  host: const {
+    '(focus)': 'onFocus()',
+    '(blur)': 'onBlur()',
+  },
+  template: '<div [attr.has-focus]="hasFocus"></div>',
+)
 class ComponentWithHostEvent {
   bool hasFocus = false;
-  bool toggled = false;
 
   void onFocus() {
     hasFocus = true;
@@ -76,5 +117,40 @@ class ComponentWithHostEvent {
 
   void onBlur() {
     hasFocus = false;
+  }
+}
+
+/// Tests that `@HostListener()` works.
+@Component(
+  selector: 'component-with-annotation',
+  template: '<div [attr.has-focus]="hasFocus"></div>',
+)
+class ComponentWithHostAnnotation {
+  bool hasFocus = false;
+
+  @HostListener('focus')
+  void onFocus() {
+    hasFocus = true;
+  }
+
+  @HostListener('blur')
+  void onBlur() {
+    hasFocus = false;
+  }
+}
+
+@Component(
+  selector: 'component-with-annotation2',
+  template: '<div [attr.was-clicked]="wasClicked"></div>',
+)
+class ComponentWithHost$Event {
+  bool wasClicked = false;
+
+  @HostListener('click', const [r'$event'])
+  void onClick(MouseEvent event) {
+    if (event == null) {
+      throw 'EXPECTED a $MouseEvent, but got null';
+    }
+    wasClicked = true;
   }
 }
