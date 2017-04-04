913aa15494addff5b39125f3032d5eaa53714a42
matanl <matanl@google.com>
2017-04-04 14:14:19 -0700

    docs(NgIf): Point to a working https:// link (cheatsheet).
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=152178339

--- lib/src/common/directives/ng_if.dart
+++ lib/src/common/directives/ng_if.dart
@@ -30,7 +30,7 @@ import 'package:angular2/core.dart'
 /// </template>
 /// ```
 ///
-/// [guide]: docs/guide/template-syntax.html#ngIf
+/// [guide]: https://webdev.dartlang.org/angular/cheatsheet
 @Directive(selector: "[ngIf]", inputs: const ["ngIf"])
 class NgIf {
   final TemplateRef _templateRef;
