19810c54c173fbf245ac09ae35b068ddc03ff1b5
alorenzen <alorenzen@google.com>
2017-04-04 11:59:32 -0700

    SourceGen: Replace manual searching through imports/exports with LibraryElement.exportNamespace.
    The analyzer already determines which elements are visible from a library, and exposes that information via the namespace, so we don't need to do a recursive search ourselves.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=152160527

--- lib/src/source_gen/common/references.dart
+++ lib/src/source_gen/common/references.dart
@@ -7,22 +7,15 @@ ReferenceBuilder toBuilder(DartType type, List<ImportElement> imports) =>
         .toTyped(_coerceTypeArgs(type, imports));
 
 String _importFrom(DartType dartType, List<ImportElement> imports) {
-  var definingLibrary = dartType.element.library;
   for (final import in imports) {
-    if (_definesLibrary(import.importedLibrary, definingLibrary) &&
-        // We might have some collection of show/hide that hides this symbol.
-        // Easier to avoid then do this wrong, for now (b/35879283).
-        import.combinators.isEmpty) {
+    final exportedElement =
+        import.importedLibrary.exportNamespace?.get(dartType.element.name);
+    if (exportedElement == dartType.element) {
       return import.uri;
     }
   }
   return null;
 }
-
-bool _definesLibrary(LibraryElement importedLibrary, LibraryElement library) =>
-    importedLibrary == library ||
-    importedLibrary.exportedLibraries
-        .any((exportedLibrary) => _definesLibrary(exportedLibrary, library));
 
 Iterable<TypeBuilder> _coerceTypeArgs(
     DartType type, List<ImportElement> imports) {
