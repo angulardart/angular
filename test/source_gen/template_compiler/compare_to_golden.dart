cc34a94111419010478d9f37bb5cb8ee8b57946b
Kevin Moore <kevmoo@google.com>
2017-04-10 13:15:16 -0700

    Fix comparison of golden files
    Fixes https://github.com/dart-lang/angular2/issues/302

--- test/source_gen/template_compiler/compare_to_golden.dart
+++ test/source_gen/template_compiler/compare_to_golden.dart
@@ -1,6 +1,7 @@
 library compare_to_golden;
 
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 import 'dart:mirrors';
 
@@ -12,7 +13,14 @@ Future compareSummaryFileToGolden(String dartFileName,
   var input = getFile(dartFileName, summaryExtension);
   var golden = getFile(dartFileName, goldenExtension);
 
-  expect(await input.readAsString(), await golden.readAsString());
+  var inputFile = new _CompareFile(await input.readAsString());
+  var goldenFile = new _CompareFile(await golden.readAsString());
+
+  // the file should match exactly, except for imports/exports
+  expect(inputFile.sourceWithoutPorts, goldenFile.sourceWithoutPorts);
+
+  // the imports/exports should match, but without caring about order
+  expect(inputFile.ports, unorderedEquals(goldenFile.ports));
 }
 
 File getFile(String dartFileName, String extension) =>
@@ -27,3 +35,38 @@ final String _testFilesDir = p.join(_scriptDir(), 'test_files');
 
 String _scriptDir() =>
     p.dirname(currentMirrorSystem().findLibrary(#compare_to_golden).uri.path);
+
+class _CompareFile {
+  static final RegExp _portMatch = new RegExp("^(?:im|ex)port *");
+
+  final String sourceWithoutPorts;
+  final List<String> ports;
+
+  _CompareFile._(this.sourceWithoutPorts, this.ports);
+
+  factory _CompareFile(String source) {
+    var buffer = new StringBuffer();
+    var ports = <String>[];
+
+    // might be null!
+    bool doingPorts;
+
+    for (var line in LineSplitter.split(source)) {
+      // only do import/exports "once"
+      if (_portMatch.hasMatch(line) && doingPorts != false) {
+        if (doingPorts == null) {
+          doingPorts = true;
+        }
+
+        ports.add(line);
+      } else {
+        if (doingPorts == true) {
+          doingPorts = false;
+        }
+        buffer.writeln(line);
+      }
+    }
+
+    return new _CompareFile._(buffer.toString(), ports);
+  }
+}
