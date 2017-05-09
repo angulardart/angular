4e6de5a5744e3cc8b9fd7a6eeb413249a4eaaac3
het <het@google.com>
2017-05-09 09:21:08 -0700

    migrate xhr_mock_test off of reflective test bed
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=155512273

--- test/compiler/xhr_mock_test.dart
+++ test/compiler/xhr_mock_test.dart
@@ -1,38 +1,35 @@
+@Tags(const ['codegen'])
 @TestOn('browser')
 library angular2.test.compiler.xhr_mock_test;
 
-import "dart:async";
+import 'dart:async';
 
-import "package:angular2/src/compiler/xhr_mock.dart" show MockXHR;
-import "package:angular2/src/testing/internal.dart";
+import 'package:angular2/src/compiler/xhr_mock.dart' show MockXHR;
 import 'package:test/test.dart';
 
-import "../test_util.dart";
+import '../test_util.dart';
 
 void main() {
-  group("MockXHR", () {
+  group('MockXHR', () {
     MockXHR xhr;
     setUp(() {
       xhr = new MockXHR();
     });
-    expectResponse(Future<String> request, String url, String response,
-        [done = null]) {
+    expectResponse(Future<String> request, String url, String response) {
       String onResponse(String text) {
         if (identical(response, null)) {
-          throw '''Unexpected response ${ url} -> ${ text}''';
+          throw 'Unexpected response $url -> $text';
         } else {
           expect(text, response);
-          if (done != null) done();
         }
         return text;
       }
 
       String onError(String error) {
         if (!identical(response, null)) {
-          throw '''Unexpected error ${ url}''';
+          throw 'Unexpected error $url';
         } else {
-          expect(error, '''Failed to load ${ url}''');
-          if (done != null) done();
+          expect(error, 'Failed to load $url');
         }
         return error;
       }
@@ -40,78 +37,68 @@ void main() {
       request.then(onResponse, onError: onError);
     }
 
-    test("should return a response from the definitions", () async {
-      return inject([AsyncTestCompleter], (AsyncTestCompleter completer) {
-        var url = "/foo";
-        var response = "bar";
-        xhr.when(url, response);
-        expectResponse(xhr.get(url), url, response, () => completer.done());
-        xhr.flush();
-      });
+    test('should return a response from the definitions', () async {
+      var url = '/foo';
+      var response = 'bar';
+      xhr.when(url, response);
+      expectResponse(xhr.get(url), url, response);
+      xhr.flush();
     });
-    test("should return an error from the definitions", () async {
-      return inject([AsyncTestCompleter], (AsyncTestCompleter completer) {
-        var url = "/foo";
-        xhr.when(url, null);
-        expectResponse(xhr.get(url), url, null, () => completer.done());
-        xhr.flush();
-      });
+    test('should return an error from the definitions', () async {
+      var url = '/foo';
+      xhr.when(url, null);
+      expectResponse(xhr.get(url), url, null);
+      xhr.flush();
     });
-    test("should return a response from the expectations", () async {
-      return inject([AsyncTestCompleter], (AsyncTestCompleter completer) {
-        var url = "/foo";
-        var response = "bar";
-        xhr.expect(url, response);
-        expectResponse(xhr.get(url), url, response, () => completer.done());
-        xhr.flush();
-      });
+    test('should return a response from the expectations', () async {
+      var url = '/foo';
+      var response = 'bar';
+      xhr.expect(url, response);
+      expectResponse(xhr.get(url), url, response);
+      xhr.flush();
     });
-    test("should return an error from the expectations", () async {
-      return inject([AsyncTestCompleter], (AsyncTestCompleter completer) {
-        var url = "/foo";
-        xhr.expect(url, null);
-        expectResponse(xhr.get(url), url, null, () => completer.done());
-        xhr.flush();
-      });
+    test('should return an error from the expectations', () async {
+      var url = '/foo';
+      xhr.expect(url, null);
+      expectResponse(xhr.get(url), url, null);
+      xhr.flush();
     });
-    test("should not reuse expectations", () {
-      var url = "/foo";
-      var response = "bar";
+    test('should not reuse expectations', () {
+      var url = '/foo';
+      var response = 'bar';
       xhr.expect(url, response);
       xhr.get(url);
       xhr.get(url);
       expect(() {
         xhr.flush();
-      }, throwsWith("Unexpected request /foo"));
+      }, throwsWith('Unexpected request /foo'));
     });
-    test("should return expectations before definitions", () async {
-      return inject([AsyncTestCompleter], (AsyncTestCompleter completer) {
-        var url = "/foo";
-        xhr.when(url, "when");
-        xhr.expect(url, "expect");
-        expectResponse(xhr.get(url), url, "expect");
-        expectResponse(xhr.get(url), url, "when", () => completer.done());
-        xhr.flush();
-      });
+    test('should return expectations before definitions', () async {
+      var url = '/foo';
+      xhr.when(url, 'when');
+      xhr.expect(url, 'expect');
+      expectResponse(xhr.get(url), url, 'expect');
+      expectResponse(xhr.get(url), url, 'when');
+      xhr.flush();
     });
-    test("should throw when there is no definitions or expectations", () {
-      xhr.get("/foo");
+    test('should throw when there is no definitions or expectations', () {
+      xhr.get('/foo');
       expect(() {
         xhr.flush();
-      }, throwsWith("Unexpected request /foo"));
+      }, throwsWith('Unexpected request /foo'));
     });
-    test("should throw when flush is called without any pending requests", () {
+    test('should throw when flush is called without any pending requests', () {
       expect(() {
         xhr.flush();
-      }, throwsWith("No pending requests to flush"));
+      }, throwsWith('No pending requests to flush'));
     });
-    test("should throw on unsatisfied expectations", () {
-      xhr.expect("/foo", "bar");
-      xhr.when("/bar", "foo");
-      xhr.get("/bar");
+    test('should throw on unsatisfied expectations', () {
+      xhr.expect('/foo', 'bar');
+      xhr.when('/bar', 'foo');
+      xhr.get('/bar');
       expect(() {
         xhr.flush();
-      }, throwsWith("Unsatisfied requests: /foo"));
+      }, throwsWith('Unsatisfied requests: /foo'));
     });
   });
 }
