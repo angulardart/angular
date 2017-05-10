3d5da338ddda8efa2ab83c704c21cf2afd5f58a4
leonsenft <leonsenft@google.com>
2017-05-10 16:09:07 -0700

    Migrates away from reflective test pattern
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=155688365

--- test/core/linker/reflector_component_resolver_test.dart
+++ test/core/linker/reflector_component_resolver_test.dart
@@ -1,41 +1,17 @@
-@TestOn('browser')
-library angular2.test.core.linker.reflector_component_resolver_test;
-
-import "package:angular2/core.dart" show provide;
-import "package:angular2/src/core/linker/component_factory.dart"
-    show ComponentFactory;
-import "package:angular2/src/core/linker/component_resolver.dart"
-    show ComponentResolver, ReflectorComponentResolver;
-import "package:angular2/src/core/reflection/reflection.dart"
-    show reflector, ReflectionInfo;
-import "package:angular2/src/testing/internal.dart";
+import 'package:angular2/angular2.dart';
+import 'package:angular2/src/core/linker/component_resolver.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group("Compiler", () {
-    var someCompFactory;
-    beforeEachProviders(() =>
-        [provide(ComponentResolver, useClass: ReflectorComponentResolver)]);
-    setUp(() async {
-      await inject([ComponentResolver], (_compiler) {
-        someCompFactory = new ComponentFactory(null, null, null);
-        reflector.registerType(
-            SomeComponent, new ReflectionInfo([someCompFactory]));
-      });
-    });
-    test("should read the template from an annotation", () async {
-      return inject([AsyncTestCompleter, ComponentResolver],
-          (AsyncTestCompleter completer, ComponentResolver compiler) {
-        compiler
-            .resolveComponent(SomeComponent)
-            .then((ComponentFactory compFactory) {
-          expect(compFactory, someCompFactory);
-          completer.done();
-          return null;
-        });
-      });
-    });
+  test('should read factory from reflection info', () async {
+    final myAppComponentFactory = new ComponentFactory(null, null, null);
+    final reflectionInfo = new ReflectionInfo([myAppComponentFactory]);
+    reflector.registerType(MyAppComponent, reflectionInfo);
+    final componentResolver = new ReflectorComponentResolver();
+    final componentFactory =
+        await componentResolver.resolveComponent(MyAppComponent);
+    expect(componentFactory, myAppComponentFactory);
   });
 }
 
-class SomeComponent {}
+class MyAppComponent {}
