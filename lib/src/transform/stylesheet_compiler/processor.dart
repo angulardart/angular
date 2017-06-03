83416d2b6e92dc4cfcc1a9e8ce15a127660ea870
leonsenft <leonsenft@google.com>
2017-06-02 17:30:29 -0700

    Reverts use of URL resolver for package:barback assets
    The URL resolver for package:barback assets doesn't support URLs with less than
    3 path segments, whereas the one for package:build assets does.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=157894961

--- lib/src/transform/stylesheet_compiler/processor.dart
+++ lib/src/transform/stylesheet_compiler/processor.dart
@@ -3,14 +3,27 @@ import 'dart:async';
 import 'package:angular2/src/compiler/config.dart';
 import 'package:angular2/src/transform/common/asset_reader.dart';
 import 'package:angular2/src/compiler/source_module.dart';
+import 'package:angular2/src/source_gen/common/url_resolver.dart';
 import 'package:angular2/src/transform/common/logging.dart';
 import 'package:angular2/src/transform/common/names.dart';
 import 'package:angular2/src/transform/common/options.dart';
 import 'package:angular2/src/transform/common/ng_compiler.dart';
-import 'package:angular2/src/transform/common/url_resolver.dart';
 import 'package:angular2/src/transform/common/zone.dart' as zone;
 import 'package:analyzer/dart/ast/token.dart' show Keyword;
 import 'package:barback/barback.dart';
+import 'package:build/build.dart' as build;
+
+/// Converts a Barback asset ID to an asset URL.
+String _assetIdToUrl(AssetId assetId) {
+  final buildAssetId = new build.AssetId(assetId.package, assetId.path);
+  return toAssetUri(buildAssetId);
+}
+
+/// Converts an asset URL to a Barback asset ID.
+AssetId _urlToAssetId(String url) {
+  final buildAssetId = new build.AssetId.resolve(url);
+  return new AssetId(buildAssetId.package, buildAssetId.path);
+}
 
 AssetId shimmedStylesheetAssetId(AssetId cssAssetId) => new AssetId(
     cssAssetId.package, toShimmedStylesheetExtension(cssAssetId.path));
@@ -20,7 +33,7 @@ AssetId nonShimmedStylesheetAssetId(AssetId cssAssetId) => new AssetId(
 
 Future<Iterable<Asset>> processStylesheet(AssetReader reader,
     AssetId stylesheetId, TransformerOptions options) async {
-  final stylesheetUrl = toAssetUri(stylesheetId);
+  final stylesheetUrl = _assetIdToUrl(stylesheetId);
   var templateCompiler = zone.templateCompiler;
   if (templateCompiler == null) {
     var config = new CompilerConfig(
@@ -33,7 +46,7 @@ Future<Iterable<Asset>> processStylesheet(AssetReader reader,
         templateCompiler.compileStylesheet(stylesheetUrl, cssText);
 
     return sourceModules.map((SourceModule module) => new Asset.fromString(
-        fromUri(module.moduleUrl), writeSourceModule(module)));
+        _urlToAssetId(module.moduleUrl), writeSourceModule(module)));
   }, operationName: 'processStylesheet', assetId: stylesheetId);
 }
 
