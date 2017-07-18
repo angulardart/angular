891d894f13ea8ab9e6e8d541fb2e5700149d744b
matanl <matanl@google.com>
2017-07-18 14:06:30 -0700

    cleanup(DI): Prepare to implement DI 1.5's typed Provider.
    * Start moving the DI runtime to "src/di/**.dart".
    * Type all "dynamic" fields as "Object".
    * Add an (unused) generic type [T] to `Provider`.
    
    Work on https://github.com/dart-lang/angular/issues/407.
    
    Created a new `RuntimeProvider` interface that means that
    the provider can be used to create a `ReflectiveInjector`
    at runtime. This will be removed from everything but the
    `SlowProvider` interface in a future CL/after cleanups.
    
    PiperOrigin-RevId: 162400737

---
+++ angular/lib/src/di/README.md
@@ -0,0 +1,5 @@
+This is a transitional folder of [AngularDart DI 1.5][DI], where interfaces
+and libraries that are cleaned up as part of this effort are being moved to
+`src/di` (out of `src/core/di`).
+
+[DI]: https://github.com/dart-lang/angular/projects/4
---
+++ angular/lib/src/di/provider.dart
@@ -0,0 +1,111 @@
+import 'package:meta/meta.dart';
+
+/// A marker that represents a lack-of-value for the `useValue` parameter.
+@visibleForTesting
+const Object noValueProvided = '__noValueProvided__';
+
+/// Describes at compile-time how an Injector should be generated.
+///
+/// A [Provider] is a binding between a _token_ and an implementation that may
+/// be _provided_ either by invoking a constructor, a function, or referring to
+/// a literal value.
+abstract class Provider<T> implements RuntimeProvider<T> {
+  const factory Provider(
+    Object token, {
+    Type useClass,
+    Object useValue,
+    Object useExisting,
+    Function useFactory,
+    List<Object> deps,
+    bool multi,
+  }) = SlowProvider<T>._;
+}
+
+/// An alias for `new Provider`; see [Provider].
+Provider provide(
+  Object token, {
+  Type useClass,
+  Object useValue: noValueProvided,
+  Object useExisting,
+  Function useFactory,
+  List<Object> deps,
+  bool multi: false,
+}) =>
+    new Provider(token,
+        useClass: useClass,
+        useValue: useValue,
+        useExisting: useExisting,
+        useFactory: useFactory,
+        deps: deps,
+        multi: multi);
+
+/// A marker interface that says the provider can be inspected at runtime.
+@visibleForTesting
+abstract class RuntimeProvider<T> {
+  /// Either a [Type] or [OpaqueToken] that is an identifier for this provider.
+  Object get token;
+
+  /// If provided, creates an instance of this class to satisfy this dependency.
+  Type get useClass;
+
+  /// If provided, uses this constant value to satisfy this dependency.
+  Object get useValue;
+
+  /// If provided, returns the same instance as if this token was provided.
+  Object get useExisting;
+
+  /// If provided, invokes this method to satisfy this dependency.
+  Function get useFactory;
+
+  /// If provided, determines what dependencies are injected into [useFactory].
+  List<Object> get dependencies;
+
+  /// If `true`, providers are collected as a List instead of a single instance.
+  bool get multi;
+}
+
+/// Legacy implementation of [Provider].
+///
+/// Contains configuration for every possibility of provider, requiring that
+/// runtime injector implementations need to inspect the various properties and
+/// determine how to configure themselves.
+@visibleForTesting
+class SlowProvider<T> implements Provider<T> {
+  @override
+  final Object token;
+
+  @override
+  final Type useClass;
+
+  @override
+  final Object useValue;
+
+  @override
+  final Object useExisting;
+
+  @override
+  final Function useFactory;
+
+  @override
+  final List<Object> dependencies;
+
+  // Only here because the compiler currently looks for this hidden field.
+  //
+  // TODO(matanl): Remove while cleaning up the DI compiler.
+  final bool _multi;
+
+  const SlowProvider._(
+    this.token, {
+    this.useClass,
+    this.useValue: noValueProvided,
+    this.useExisting,
+    this.useFactory,
+    List<Object> deps,
+    bool multi: false,
+  })
+      : dependencies = deps,
+        _multi = multi;
+
+  @override
+  bool get multi => _multi;
+}
