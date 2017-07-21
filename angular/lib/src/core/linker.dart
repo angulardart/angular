fdaa71ed4587c30ca06a8c10e8659bd2b5644c44
matanl <matanl@google.com>
2017-07-21 10:38:38 -0700

    chore(Core): Export 'SlowComponentLoader' as public API.
    PiperOrigin-RevId: 162757337

--- angular/lib/src/core/linker.dart
+++ angular/lib/src/core/linker.dart
@@ -3,7 +3,8 @@
 export "linker/component_factory.dart" show ComponentRef, ComponentFactory;
 export "linker/component_loader.dart" show ComponentLoader;
 export "linker/component_resolver.dart" show ComponentResolver;
-export "linker/dynamic_component_loader.dart" show DynamicComponentLoader;
+export "linker/dynamic_component_loader.dart"
+    show DynamicComponentLoader, SlowComponentLoader;
 export "linker/element_ref.dart" show ElementRef;
 export "linker/exceptions.dart"
     show ExpressionChangedAfterItHasBeenCheckedException;
