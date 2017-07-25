2bb2cab15e7866d1d4e8bcea44893ce4cfa77c7e
alorenzen <alorenzen@google.com>
2017-07-25 13:32:20 -0700

    Move ComponentFixuture to angular_test_bed.
    This is the only place it's actually used.
    
    PiperOrigin-RevId: 163114867

--- angular/lib/src/testing/test_component_builder.dart
+++ angular/lib/src/testing/test_component_builder.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:html';
 
 import 'package:angular/core.dart'
@@ -9,62 +8,8 @@ import 'package:angular/core.dart'
         ElementRef,
         ChangeDetectorRef;
 import 'package:angular/di.dart' show Injectable;
-import 'package:angular/src/core/linker/app_view_utils.dart';
 import 'package:angular/src/core/render/api.dart' show sharedStylesHost;
-import 'package:angular/src/debug/debug_node.dart'
-    show DebugElement, getDebugNode;
-import 'package:angular/src/platform/dom/dom_tokens.dart' show DOCUMENT;
 import 'package:angular/src/platform/dom/shared_styles_host.dart';
-
-/// Fixture for debugging and testing a component.
-class ComponentFixture {
-  /// The DebugElement associated with the root element of this component.
-  DebugElement _debugElement;
-
-  /// The instance of the root component class.
-  dynamic componentInstance;
-
-  /// The native element at the root of the component.
-  dynamic nativeElement;
-
-  /// The [ElementRef] for the element at the root of the component.
-  ElementRef elementRef;
-
-  /// The [ComponentRef] for the component.
-  ComponentRef componentRef;
-
-  /// The [ChangeDetectorRef] for the component.
-  ChangeDetectorRef changeDetectorRef;
-
-  ComponentFixture(ComponentRef ref) {
-    changeDetectorRef = ref.changeDetectorRef;
-    elementRef = ref.location;
-    _debugElement = (getDebugNode(elementRef.nativeElement) as DebugElement);
-    componentInstance = ref.instance;
-    assert(componentInstance != null);
-    nativeElement = elementRef.nativeElement;
-    componentRef = ref;
-  }
-
-  DebugElement get debugElement {
-    if (_debugElement == null) {
-      throw new Exception(
-          'DebugElement is not available in DART_CODEGEN_MODE=release');
-    }
-    return _debugElement;
-  }
-
-  /// Trigger a change detection cycle for the component.
-  void detectChanges([bool checkForNoChanges = true]) {
-    AppViewUtils.resetChangeDetection();
-    changeDetectorRef.detectChanges();
-  }
-
-  /// Trigger component destruction.
-  void destroy() {
-    componentRef.destroy();
-  }
-}
 
 var _nextRootElementId = 0;
 
@@ -93,27 +38,5 @@ class TestComponentBuilder {
   TestComponentBuilder overrideProviders(Type type, List<dynamic> providers) {
     var clone = _clone();
     return clone;
-  }
-
-  /// Builds and returns a ComponentFixture.
-  Future<ComponentFixture> createAsync(Type rootComponentType) {
-    AppViewUtils.resetChangeDetection();
-    var rootElId = '''root${ _nextRootElementId ++}''';
-    var rootEl = new DivElement()..id = rootElId;
-    var doc = _injector.get(DOCUMENT);
-    // TODO(juliemr): can/should this be optional?
-    var oldRoots = doc.querySelectorAll("[id^=root]");
-    for (var i = 0; i < oldRoots.length; i++) {
-      oldRoots[i].remove();
-    }
-    (doc as HtmlDocument).body.append(rootEl);
-    DynamicComponentLoader loader = _injector.get(DynamicComponentLoader);
-    appViewUtils = _injector.get(AppViewUtils);
-    Future<ComponentRef> promise = loader.load(rootComponentType, _injector);
-    return promise.then((componentRef) {
-      querySelector('#$rootElId')
-          .append(componentRef.location.nativeElement as Element);
-      return new ComponentFixture(componentRef);
-    });
   }
 }
