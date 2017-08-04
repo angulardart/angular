35a8c20a8d63e5a431553aae908581f5898b8dba
tsander <tsander@google.com>
2017-08-03 18:11:07 -0700

    Expose setUpControl so that users can more easily write their own form implementations if they wish. This also matches how setUpControlGroup is already exposed.
    PiperOrigin-RevId: 164211290

--- angular/lib/src/common/forms.dart
+++ angular/lib/src/common/forms.dart
@@ -30,7 +30,8 @@ export "forms/directives/ng_form_model.dart" show NgFormModel;
 export "forms/directives/ng_model.dart" show NgModel;
 export "forms/directives/select_control_value_accessor.dart"
     show NgSelectOption, SelectControlValueAccessor;
-export "forms/directives/shared.dart" show composeValidators, setUpControlGroup;
+export "forms/directives/shared.dart"
+    show composeValidators, setUpControlGroup, setUpControl;
 export "forms/directives/validators.dart"
     show
         RequiredValidator,
