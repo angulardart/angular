34ba965845b1f545044395d9c09bc17b0829d7be
matanl <matanl@google.com>
2017-08-08 17:45:54 -0700

    feat(Compiler): Add more initReflector-emitting logic.
    This is intentionally not using `code_builder`, since I want to wait for 2.0.0
    to land internally and not add migration debt. This won't change significantly
    in the future (if anything we will delete code), so as long as it is readable
    current structure should be fine.
    
    Partial work towards https://github.com/dart-lang/angular/issues/532.
    
    PiperOrigin-RevId: 164669987

--- angular_compiler/lib/src/emitter/reflector.dart
+++ angular_compiler/lib/src/emitter/reflector.dart
@@ -1,3 +1,7 @@
+import 'package:analyzer/dart/element/element.dart';
+
+import '../analyzer/di/dependencies.dart';
+import '../analyzer/di/tokens.dart';
 import '../analyzer/reflector.dart';
 
 /// Generates `.dart` source code given a [ReflectableOutput].
@@ -9,7 +13,7 @@ class ReflectableEmitter {
 
   const ReflectableEmitter(
     this._output, {
-    this.reflectorSource: '$_package/src/core/reflection/reflection.dart',
+    this.reflectorSource: '$_package/src/di/reflector.dart',
   });
 
   bool get _linkingNeeded => _output.urlsNeedingInitReflector.isNotEmpty;
@@ -51,8 +55,94 @@ class ReflectableEmitter {
       }
     }
     if (_registrationNeeded) {
-      output.writeln('  // TODO: Register functions and classes.');
+      for (final element in _output.registerFunctions) {
+        output.writeln(_registerFactory(element));
+      }
+      for (final element in _output.registerClasses) {
+        if (element.registerComponentFactory) {
+          output.writeln(_registerComponent(element.name));
+        }
+        if (element.factory != null) {
+          output.writeln(_registerFactory(element.factory));
+        }
+        if (element.registerAnnotation != null) {
+          // TODO(matanl): Complete.
+        }
+      }
     }
     return (output..writeln('}')).toString();
   }
+
+  String _registerComponent(String name) =>
+      '  _ngRef.registerComponent(\n    $name,\n    ${name}NgFactory,\n  );';
+
+  String _nameOfInvocation(DependencyInvocation invocation) {
+    final bound = invocation.bound;
+    if (bound is ConstructorElement) {
+      return bound.returnType.element.name;
+    }
+    if (bound is FunctionElement) {
+      return bound.name;
+    }
+    throw new UnsupportedError('Invalid type: ${bound.runtimeType}.');
+  }
+
+  String _invocationParams(DependencyInvocation invocation) =>
+      new Iterable.generate(invocation.positional.length, (i) => 'p$i')
+          .join(', ');
+
+  String _generateFactory(DependencyInvocation invocation) =>
+      '(${_generateParameters(invocation.positional).join(',\n')}) => '
+      '${_invocationOf(invocation)}(${_invocationParams(invocation)})';
+
+  Iterable<String> _generateParameters(List<DependencyElement> params) sync* {
+    for (var i = 0; i < params.length; i++) {
+      String type = 'dynamic';
+      final element = params[i];
+      final token = element.token;
+      if (token is TypeTokenElement) {
+        type = token.url.fragment;
+      }
+      yield '$type p$i';
+    }
+  }
+
+  String _invocationOf(DependencyInvocation invocation) {
+    final bound = invocation.bound;
+    if (bound is ConstructorElement) {
+      return 'new ${_nameOfInvocation(invocation)}';
+    }
+    if (bound is FunctionElement) {
+      return bound.name;
+    }
+    throw new UnsupportedError('Invalid type: ${bound.runtimeType}.');
+  }
+
+  String _registerFactory(DependencyInvocation invocation) =>
+      (new StringBuffer()
+            ..writeln('  _ngRef.registerFactory(')
+            ..writeln('    ${_nameOfInvocation(invocation)},')
+            ..writeln('    ${_generateFactory(invocation)},')
+            ..writeln('  );')
+            ..writeln('  _ngRef.registerDependencies(')
+            ..writeln('    ${_nameOfInvocation(invocation)},')
+            ..writeln('    ${_generateDependencies(invocation)},')
+            ..writeln('  );'))
+          .toString();
+
+  String _generateDependencies(DependencyInvocation invocation) {
+    final output = new StringBuffer('const [');
+    for (final param in invocation.positional) {
+      output
+        ..write('const [')
+        ..write(_generateToken(param.token))
+        ..write(',')
+        ..write('],');
+    }
+    return (output..write(']')).toString();
+  }
+
+  String _generateToken(TokenElement token) => token is TypeTokenElement
+      ? token.url.fragment
+      : 'const OpaqueToken(${(token as OpaqueTokenElement).identifier})';
 }
