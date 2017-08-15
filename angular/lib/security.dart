3b6c5dccb60b6fbdb532324651c1c835cff8a30f
kathyw <kathyw@google.com>
2017-08-15 10:01:47 -0700

    Add a library description to angular/security.dart.
    PiperOrigin-RevId: 165321120

--- angular/lib/security.dart
+++ angular/lib/security.dart
@@ -1,3 +1,17 @@
+/// API for customizing AngularDart's built-in security checks.
+///
+/// Import this library as follows:
+///
+/// ```
+/// import 'package:angular/security.dart';
+/// ```
+///
+/// For examples of using this API, see the [Security page][]
+/// in the [AngularDart guide][].
+///
+/// [Security page]: https://webdev.dartlang.org/angular/guide/security
+/// [AngularDart guide]: https://webdev.dartlang.org/angular/guide
+
 library angular.security;
 
 export 'src/security/dom_sanitization_service.dart';
