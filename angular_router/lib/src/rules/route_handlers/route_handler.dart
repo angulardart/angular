d3038bded824115d5b9f92e3ca55372351d54471
alorenzen <alorenzen@google.com>
2017-09-08 17:19:20 -0700

    Move router v1 to _angular_router_deprecated.
    PiperOrigin-RevId: 168068280

--- angular_router/lib/src/rules/route_handlers/route_handler.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-import '../../instruction.dart' show RouteData;
-
-abstract class RouteHandler {
-  dynamic componentType;
-  Future<dynamic> resolveComponentType();
-  RouteData data;
-}
