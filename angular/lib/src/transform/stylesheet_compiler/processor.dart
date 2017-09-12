69a51e1dc045c3f205d512e937c6b79f985423e4
nbosch <nbosch@google.com>
2017-09-12 13:25:05 -0700

    Switch StylesheetCompiler to the Builder interface
    PiperOrigin-RevId: 168432377

--- angular/lib/src/transform/stylesheet_compiler/processor.dart
+++ angular/lib/src/transform/stylesheet_compiler/processor.dart
@@ -1,49 +1,25 @@
 import 'dart:async';
 
 import 'package:analyzer/dart/ast/token.dart' show Keyword;
-import 'package:barback/barback.dart';
-import 'package:build/build.dart' as build;
+import 'package:build/build.dart';
 import 'package:angular/src/compiler/source_module.dart';
 import 'package:angular/src/source_gen/common/url_resolver.dart';
-import 'package:angular/src/transform/common/logging.dart';
-import 'package:angular/src/transform/common/names.dart';
 import 'package:angular/src/transform/common/ng_compiler.dart';
 import 'package:angular/src/transform/common/zone.dart' as zone;
 import 'package:angular_compiler/angular_compiler.dart';
 
-/// Converts a Barback asset ID to an asset URL.
-String _assetIdToUrl(AssetId assetId) {
-  final buildAssetId = new build.AssetId(assetId.package, assetId.path);
-  return toAssetUri(buildAssetId);
-}
+Future<Map<AssetId, String>> processStylesheet(
+    AssetReader reader, AssetId stylesheetId, CompilerFlags flags) async {
+  final stylesheetUrl = toAssetUri(stylesheetId);
+  final templateCompiler = zone.templateCompiler ??
+      createTemplateCompiler(new NgAssetReader.fromBuildStep(reader), flags);
+  final cssText = await reader.readAsString(stylesheetId);
+  final sourceModules =
+      templateCompiler.compileStylesheet(stylesheetUrl, cssText);
 
-/// Converts an asset URL to a Barback asset ID.
-AssetId _urlToAssetId(String url) {
-  final buildAssetId = new build.AssetId.resolve(url);
-  return new AssetId(buildAssetId.package, buildAssetId.path);
-}
-
-AssetId shimmedStylesheetAssetId(AssetId cssAssetId) => new AssetId(
-    cssAssetId.package, toShimmedStylesheetExtension(cssAssetId.path));
-
-AssetId nonShimmedStylesheetAssetId(AssetId cssAssetId) => new AssetId(
-    cssAssetId.package, toNonShimmedStylesheetExtension(cssAssetId.path));
-
-Future<Iterable<Asset>> processStylesheet(
-    NgAssetReader reader, AssetId stylesheetId, CompilerFlags flags) async {
-  final stylesheetUrl = _assetIdToUrl(stylesheetId);
-  var templateCompiler = zone.templateCompiler;
-  if (templateCompiler == null) {
-    templateCompiler = createTemplateCompiler(reader, flags);
-  }
-  final cssText = await reader.readText(stylesheetId.toString());
-  return logElapsedAsync(() async {
-    final sourceModules =
-        templateCompiler.compileStylesheet(stylesheetUrl, cssText);
-
-    return sourceModules.map((SourceModule module) => new Asset.fromString(
-        _urlToAssetId(module.moduleUrl), writeSourceModule(module)));
-  }, operationName: 'processStylesheet', assetId: stylesheetId);
+  return new Map.fromIterable(sourceModules,
+      key: (module) => new AssetId.resolve((module as SourceModule).moduleUrl),
+      value: (module) => writeSourceModule(module));
 }
 
 /// Writes the full Dart code for the provided [SourceModule].
--- angular/lib/src/transform/stylesheet_compiler/transformer.dart
+++ angular/lib/src/transform/stylesheet_compiler/transformer.dart
@@ -1,47 +1,29 @@
 import 'dart:async';
 
-import 'package:barback/barback.dart';
+import 'package:build/build.dart';
 import 'package:angular/src/transform/common/names.dart';
-import 'package:angular/src/transform/common/zone.dart' as zone;
 import 'package:angular_compiler/angular_compiler.dart';
 
 import 'processor.dart';
 
 /// Pre-compiles CSS stylesheet files to Dart code for Angular 2.
-class StylesheetCompiler extends Transformer implements LazyTransformer {
+class StylesheetCompiler implements Builder {
   final CompilerFlags _flags;
 
   StylesheetCompiler(this._flags);
 
   @override
-  bool isPrimary(AssetId id) {
-    return id.path.endsWith(CSS_EXTENSION);
-  }
+  final buildExtensions = {
+    CSS_EXTENSION: const [
+      SHIMMED_STYLESHEET_EXTENSION,
+      NON_SHIMMED_STYLESHEET_EXTENSION
+    ]
+  };
 
   @override
-  declareOutputs(DeclaringTransform transform) {
-    // Note: we check this assumption below.
-    _getExpectedOutputs(transform.primaryId).forEach(transform.declareOutput);
-  }
-
-  List<AssetId> _getExpectedOutputs(AssetId cssId) =>
-      [shimmedStylesheetAssetId(cssId), nonShimmedStylesheetAssetId(cssId)];
-
-  @override
-  Future apply(Transform transform) async {
-    final reader = new NgAssetReader.fromBarback(transform);
-    return zone.exec(() async {
-      var primaryId = transform.primaryInput.id;
-      var outputs = await processStylesheet(reader, primaryId, _flags);
-      var expectedIds = _getExpectedOutputs(primaryId);
-      outputs.forEach((Asset compiledStylesheet) {
-        var id = compiledStylesheet.id;
-        if (!expectedIds.contains(id)) {
-          throw new StateError(
-              'Unexpected output for css processing of $primaryId: $id');
-        }
-        transform.addOutput(compiledStylesheet);
-      });
-    }, log: transform.logger);
+  Future build(BuildStep buildStep) async {
+    final outputs =
+        await processStylesheet(buildStep, buildStep.inputId, _flags);
+    outputs.forEach(buildStep.writeAsString);
   }
 }
