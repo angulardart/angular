168f481416ade84bb7b3898b9747348268307984
matanl <matanl@google.com>
2017-09-20 16:28:22 -0700

    feat(Compiler): Add the beginnings of a GeneratedInjector (emitter+test).
    Once hooked up, we will use `@Injector.generate` to create a parallel class as
    part of the `.template.dart` generation process. This is an **experimental**
    feature.
    
    It currently does not support any of the "hierarchical" flags (i.e. @Self, @SkipSelf), as the intended use case is as part of the bootstrap (root injector) right now. It is relatively easy to support @Optional() in a future commit.
    
    PiperOrigin-RevId: 169469132

---
+++ angular_compiler/test/emitter/injector_test.dart
@@ -0,0 +1,60 @@
+import 'package:angular_compiler/angular_compiler.dart';
+import 'package:test/test.dart';
+
+import '../src/resolve.dart';
+
+void main() {
+  const angular = 'package:angular';
+  const libInjector = '$angular/src/di/injector.dart';
+
+  test('should support a no-op', () {
+    final emitter = new InjectorEmitter({});
+    expect(emitter.emitImports(), isEmpty);
+    expect(emitter.emitInjector(), isEmpty);
+  });
+
+  test('should support a simple injector', () async {
+    final library = await resolveLibrary(r'''
+      @providers
+      class Example {}
+      class ExamplePrime {}
+
+      const providers = const [
+        const Provider(Example, useClass: ExamplePrime),  
+      ];
+    ''');
+    final providers = const ProviderReader().parseModule(
+      library.getType('Example').metadata.first.computeConstantValue(),
+    );
+    final injector = new InjectorEmitter({
+      'ExampleInjector': providers,
+    });
+    expect(
+      injector.emitImports(),
+      "import '$libInjector' as _injector;",
+    );
+    expect(
+        injector.emitInjector(),
+        ''
+        'class ExampleInjector\$Generated extends _injector.GeneratedInjector {\n'
+        '  ExampleInjector\$Generated([_injector.Injector parent]) : super(parent);\n'
+        '  @override\n'
+        '  T injectFromSelf<T>(\n'
+        '    Object token, {\n'
+        '    _injector.OrElseInject<T> orElse: _injector.throwsNotFound,\n'
+        '  }) {\n'
+        '    switch (token) {\n'
+        '      case Example:\n'
+        '        return _provide0();\n'
+        '      default:\n'
+        '        return orElse(this, token);\n'
+        '    }\n'
+        '  }\n'
+        '  Example _field0;\n'
+        '  Example _provide0() {\n'
+        '    return _field0 ??= new ExamplePrime();\n'
+        '  }\n'
+        '\n'
+        '}\n');
+  });
+}
