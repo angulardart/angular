586fb08c130c416c45c656ac70817128c4c84413
leonsenft <leonsenft@google.com>
2017-09-22 10:03:57 -0700

    Resolves generic input types, defaulting to dynamic when unspecified
    PiperOrigin-RevId: 169702530

--- _tests/test/source_gen/template_compiler/find_components_test.dart
+++ _tests/test/source_gen/template_compiler/find_components_test.dart
@@ -48,6 +48,67 @@ void main() {
       class NotMockLikeComponent {}''');
     expect(normalizedComponent.component.analyzedClass.isMockLike, false);
   });
+
+  group('Generic type parameter', () {
+    test('should resolve to dynamic when unspecified', () async {
+      final normalizedComponent = await resolveAndFindComponent('''
+        @Component()
+        class TestComponent<T> {
+          @Input()
+          T value;
+        }''');
+      expect(normalizedComponent.component.inputTypes['value'].name, 'dynamic');
+    });
+
+    test('should resolve to dynamic when unspecified on supertype', () async {
+      final normalizedComponent = await resolveAndFindComponent('''
+        abstract class Base<T> {
+          @Input()
+          T value;
+        }
+
+        @Component()
+        class TestComponent extends Base {}
+      ''');
+      expect(normalizedComponent.component.inputTypes['value'].name, 'dynamic');
+    });
+
+    test('should resolve bounded type', () async {
+      final normalizedComponent = await resolveAndFindComponent('''
+        @Component()
+        class TestComponent<T extends String> {
+          @Input()
+          T value;
+        }''');
+      expect(normalizedComponent.component.inputTypes['value'].name, 'String');
+    });
+
+    test('should resolve bounded type on supertype', () async {
+      final normalizedComponent = await resolveAndFindComponent('''
+        abstract class Base<T> {
+          @Input()
+          T value;
+        }
+
+        @Component()
+        class TestComponent<S extends String> extends Base<S> {}
+      ''');
+      expect(normalizedComponent.component.inputTypes['value'].name, 'String');
+    });
+
+    test('should resolve to specified type', () async {
+      final normalizedComponent = await resolveAndFindComponent('''
+        abstract class Base<T> {
+          @Input()
+          T value;
+        }
+
+        @Component()
+        class TestComponent extends Base<String> {}
+      ''');
+      expect(normalizedComponent.component.inputTypes['value'].name, 'String');
+    });
+  });
 }
 
 Future<NormalizedComponentWithViewDirectives> resolveAndFindComponent(
