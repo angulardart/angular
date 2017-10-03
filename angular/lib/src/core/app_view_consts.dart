7155e806579bbb635806667184f4ec1306de9ce0
alorenzen <alorenzen@google.com>
2017-10-03 16:00:40 -0700

    Clean up angular BUILD file to have four specific dart libraries:
    1) core - the core files that are shared by both the compiler and the runtime. This also includes the old :di target.
    
    2) compiler - this is the actual angular compiler, as well as the source_gen and transformer wrappers.
    
    3) angular - all of the files needed at runtime. This is what most users should depend on.
    
    4) experimental - this remains a separate library to discourage users from depending on it.
    
    I had to clean up some imports to split cycles among these main libraries.
    
    PiperOrigin-RevId: 170929437

---
+++ angular/lib/src/core/app_view_consts.dart
@@ -0,0 +1,8 @@
+const MAX_INTERPOLATION_VALUES = 9;
+
+// List of supported namespaces.
+const NAMESPACE_URIS = const {
+  'xlink': 'http://www.w3.org/1999/xlink',
+  'svg': 'http://www.w3.org/2000/svg',
+  'xhtml': 'http://www.w3.org/1999/xhtml'
+};
