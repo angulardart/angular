3b3649b624fb97ef07d9161bea1d8298bfef658b
matanl <matanl@google.com>
2017-10-04 12:38:09 -0700

    refactor(Bootstrap): Break out two specific bootstrap "modules".
    * bootstrapLegacyModule: Used by default, includes support for reflective APIs.
    * bootstrapMinimalModule: Opt-in via angular/experimental.dart, no reflective.
    
    A few other small refactors:
    
    * Hard-coded list of DomEventPlugins instead of relying on DI to inject them.
    * Hard-coded factory functions that will never changed (less initReflector).
    
    This is a non-breaking change; when applied to the angular_hn sample it drops
    the code-size by over 5Kb with 0 functionality changes - we'll be able to do
    even more after a generated root injector is used.
    
    PiperOrigin-RevId: 171047184

--- angular/lib/angular.dart
+++ angular/lib/angular.dart
@@ -37,5 +37,4 @@ export 'src/core/application_tokens.dart' hide APP_ID_RANDOM_PROVIDER;
 export 'src/core/linker.dart';
 export 'src/core/testability/testability.dart';
 export 'src/platform/bootstrap.dart';
-export 'src/platform/dom/dom_tokens.dart';
 export 'src/platform/dom/events/event_manager.dart' show EventManagerPlugin;
---
+++ angular/lib/src/bootstrap/modules.dart
@@ -0,0 +1,95 @@
+/// Contains the internal dependency injection "modules" required for bootstrap.
+library angular.src.bootstrap.modules;
+
+import 'dart:html';
+import 'dart:math';
+
+import 'package:angular/src/core/application_ref.dart';
+import 'package:angular/src/core/application_tokens.dart';
+import 'package:angular/src/core/linker/app_view_utils.dart';
+import 'package:angular/src/core/linker/component_loader.dart';
+import 'package:angular/src/core/linker/component_resolver.dart';
+import 'package:angular/src/core/linker/dynamic_component_loader.dart';
+import 'package:angular/src/core/testability/testability.dart';
+import 'package:angular/src/core/zone.dart';
+import 'package:angular/src/di/provider.dart';
+import 'package:angular/src/facade/exception_handler.dart';
+import 'package:angular/src/facade/lang.dart';
+import 'package:angular/src/platform/browser/exceptions.dart';
+import 'package:angular/src/platform/dom/events/dom_events.dart';
+import 'package:angular/src/platform/dom/events/event_manager.dart';
+import 'package:angular/src/platform/dom/events/hammer_gestures.dart';
+import 'package:angular/src/platform/dom/events/key_events.dart';
+import 'package:angular/src/security/dom_sanitization_service.dart';
+import 'package:angular/src/security/dom_sanitization_service_impl.dart';
+
+import 'package:meta/meta.dart';
+
+/// Adds support for runtime event plugins.
+///
+/// This may eventually be excluded from the [minimalModule].
+const eventPluginModule = const <Object>[
+  const Provider(EventManager),
+  const Provider(
+    EVENT_MANAGER_PLUGINS,
+    useFactory: createEventPlugins,
+    deps: const [],
+  ),
+];
+
+/// Creates a list of [EventManagerPlugins] to be used by [EventManager].
+List<EventManagerPlugin> createEventPlugins() {
+  // Order here is very important, it is reversed before being registered, and
+  // DomEventsPlugin is a catch-all so it *must* happen last.
+  return [
+    new DomEventsPlugin(),
+    new KeyEventsPlugin(),
+    new HammerGesturesPlugin(new HammerGestureConfig()),
+  ];
+}
+
+/// Strict subset module of AngularDart functionality.
+///
+/// Does not support any service that requires the `initReflector()`-based APIs.
+const bootstrapMinimalModule = const <Object>[
+  // Custom events and fallback if the compiler does not detect an event.
+  eventPluginModule,
+
+  // HTML/DOM sanitization.
+  const Provider(ExceptionHandler, useClass: BrowserExceptionHandler),
+  const Provider(SanitizationService, useExisting: DomSanitizationService),
+  const Provider(DomSanitizationService, useClass: DomSanitizationServiceImpl),
+
+  // Core components of the runtime.
+  const Provider(ApplicationRef, useClass: ApplicationRefImpl),
+  const Provider(NgZone, useFactory: createNgZone, deps: const []),
+  const Provider(APP_ID, useFactory: createRandomAppId, deps: const []),
+  const Provider(AppViewUtils),
+  const Provider(ComponentLoader),
+  const Provider(Testability),
+];
+
+/// Returns the current [Document] of the browser.
+HtmlDocument getDocument() => document;
+
+/// Creates an AngularDart zone, enabling async stack traces in developer mode.
+NgZone createNgZone() => new NgZone(enableLongStackTrace: assertionsEnabled());
+
+/// Creates a random [APP_ID] for use in CSS encapsulation.
+String createRandomAppId() {
+  final random = new Random();
+  String char() => new String.fromCharCode(97 + random.nextInt(26));
+  return '${char()}${char()}${char()}';
+}
+
+/// Compatibility module (extension of [minimalModule]).
+///
+/// Adds support for soft-deprecated runtime reflective-like APIs.
+///
+/// **WARNING**: This API is not considered part of the stable API.
+@experimental
+const bootstrapLegacyModule = const <Object>[
+  bootstrapMinimalModule,
+  const Provider(ComponentResolver, useClass: ReflectorComponentResolver),
+  const Provider(SlowComponentLoader),
+];
