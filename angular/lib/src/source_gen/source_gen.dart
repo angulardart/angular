9aab8b5b38c3c9886a3a51c8b2fb4a5de32f069e
nbosch <nbosch@google.com>
2017-10-09 13:06:25 -0700

    Add Builder factories inside angular package
    We will need these to exist within Angular to configure with build.yaml
    
    PiperOrigin-RevId: 171575917

--- angular/lib/src/source_gen/source_gen.dart
+++ angular/lib/src/source_gen/source_gen.dart
@@ -1,13 +1,41 @@
-import 'package:angular_compiler/angular_compiler.dart';
 import 'package:build/build.dart';
 import 'package:dart_style/dart_style.dart';
+import 'package:logging/logging.dart';
 import 'package:source_gen/source_gen.dart';
+import 'package:angular_compiler/angular_compiler.dart';
 
 import '../transform/common/names.dart';
 import 'template_compiler/generator.dart';
 
 export 'template_compiler/generator.dart'
     show TemplatePlaceholderBuilder, TemplateGenerator;
+
+const _outlineOnlyFlag = '--outline-only';
+
+Builder templateCompiler(List<String> args) {
+  var outlineOnly = args.contains(_outlineOnlyFlag);
+  if (outlineOnly) {
+    args = args.toList()..remove(_outlineOnlyFlag);
+  }
+
+  var flags = new CompilerFlags.parseArgs(
+    args,
+    defaultTo: const CompilerFlags(
+      genDebugInfo: false,
+      useLegacyStyleEncapsulation: true,
+      usePlaceholder: true,
+    ),
+    severity: Level.SEVERE,
+  );
+
+  if (outlineOnly) {
+    return new TemplateOutliner(flags);
+  } else {
+    return createSourceGenTemplateCompiler(flags);
+  }
+}
+
+Builder templatePlaceholderBuilder(_) => const TemplatePlaceholderBuilder();
 
 Builder createSourceGenTemplateCompiler(CompilerFlags flags) =>
     new LibraryBuilder(new TemplateGenerator(flags),
