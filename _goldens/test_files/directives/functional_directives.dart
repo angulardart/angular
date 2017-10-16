2975e8428fe3b8215fcad1105482a9b7eeda7c37
leonsenft <leonsenft@google.com>
2017-10-16 10:03:51 -0700

    Adds golden test for functional directives
    PiperOrigin-RevId: 172338962

---
+++ _goldens/test_files/directives/functional_directives.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+import 'package:angular/angular.dart';
+
+@Injectable()
+class TextService {
+  String generateText() => 'text';
+}
+
+@Directive(selector: '[generateText]')
+void generateTextDirective(HtmlElement element, TextService textService) {
+  element.text = textService.generateText();
+}
+
+@Component(
+  selector: 'app',
+  template: '<div generateText><div></div></div>',
+  directives: const [generateTextDirective],
+  providers: const [TextService],
+  preserveWhitespace: false,
+  visibility: Visibility.none,
+)
+class AppComponent {}
