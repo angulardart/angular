1d3c7d8da130c619655184780f1e94fc276384e3
alorenzen <alorenzen@google.com>
2017-10-30 15:44:03 -0700

    fix(ast): Fix NPE in desugar_visitor.
    PiperOrigin-RevId: 173961567

--- angular_ast/lib/src/expression/micro.dart
+++ angular_ast/lib/src/expression/micro.dart
@@ -15,23 +15,18 @@ final _isBind = new RegExp(r'\S+:');
 /// This means it won't parse correctly with the standard expression parser, and
 /// [parseMicroExpression] is needed to de-sugar the expression into its
 /// multiple intents.
-bool isMicroExpression(String expression) {
-  return expression.startsWith('let') || expression.startsWith(_isBind);
-}
+bool isMicroExpression(String expression) =>
+    expression != null &&
+    (expression.startsWith('let') || expression.startsWith(_isBind));
 
 /// Returns a de-sugared micro AST from [expression].
 NgMicroAst parseMicroExpression(
-  String directive,
-  String expression,
-  int expressionOffset, {
-  String sourceUrl,
-  TemplateAst origin,
-}) {
-  return const NgMicroParser().parse(
-    directive,
-    expression,
-    expressionOffset,
-    sourceUrl: sourceUrl,
-    origin: origin,
-  );
-}
+        String directive, String expression, int expressionOffset,
+        {String sourceUrl, TemplateAst origin}) =>
+    const NgMicroParser().parse(
+      directive,
+      expression,
+      expressionOffset,
+      sourceUrl: sourceUrl,
+      origin: origin,
+    );
