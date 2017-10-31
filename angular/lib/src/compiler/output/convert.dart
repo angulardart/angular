a0c65abac980a411ca9ab51d139ca56afddac379
leonsenft <leonsenft@google.com>
2017-10-31 10:55:57 -0700

    Resolves generics while converting analyzer types to output ASTs
    Private types are replaced with dynamic to prevent visibility errors in
    generated code.
    
    PiperOrigin-RevId: 174059597

--- angular/lib/src/compiler/output/convert.dart
+++ angular/lib/src/compiler/output/convert.dart
@@ -4,9 +4,19 @@ import '../../source_gen/common/url_resolver.dart';
 import '../compile_metadata.dart';
 import 'output_ast.dart' as o;
 
+/// Creates an AST for code generation from [dartType].
+///
+/// Note that private types aren't visible to generated code and will be
+/// replaced with dynamic.
 o.OutputType fromDartType(DartType dartType) {
   if (dartType.isVoid) return null;
   if (dartType is FunctionType) return fromFunctionType(dartType);
+  if (dartType is TypeParameterType) {
+    // Resolve generic type to its bound or dynamic if it has none.
+    final dynamicType = dartType.element.context.typeProvider.dynamicType;
+    dartType = dartType.resolveToBound(dynamicType);
+  }
+  if (dartType.element.isPrivate) return o.DYNAMIC_TYPE;
   final typeArguments = dartType is ParameterizedType
       ? dartType.typeArguments.map(fromDartType).toList()
       : null;
@@ -19,6 +29,7 @@ o.OutputType fromDartType(DartType dartType) {
   );
 }
 
+/// Creates an AST for code generation from [functionType]
 o.FunctionType fromFunctionType(FunctionType functionType) {
   final returnType = fromDartType(functionType.returnType);
   final paramTypes = <o.OutputType>[];
