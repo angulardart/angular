8887a8db88387f80605f62e264ad735f76f1789f
mfairhurst <mfairhurst@google.com>
2017-11-07 13:45:00 -0800

    Parse SVG tags as either void or non-void with no error.
    PiperOrigin-RevId: 174905865

--- angular_ast/lib/src/parser.dart
+++ angular_ast/lib/src/parser.dart
@@ -32,7 +32,111 @@ class NgParser {
     'source',
     'track',
     'wbr',
+  ];
+
+  // SVG is pure html; all tags can be used with or without void syntax. We
+  // already parse closer to XML than HTML, so we simply need to accept these
+  // tags in <closed></closed> and <unclosed /> forms, which is simpler than
+  // dropping into an xml parser for svgs, and better for error recovery.
+  //
+  // https://developer.mozilla.org/en-US/docs/Web/SVG/Element
+  //
+  // Some tags (a, script) overlap with HTML. Exclude those, to prefer correct
+  // HTML semantics to correct SVG semantics.
+  static const _svgElements = const <String>[
+    // 'a', Exclude this because it's also HTML
+    'altGlyph',
+    'altGlyphDef',
+    'altGlyphItem',
+    'animate',
+    'animateColor',
+    'animateMotion',
+    'animateTransform',
+    // 'audio', Exclude this because it's also HTML
+    // 'canvas', Exclude this because it's also HTML
+    'circle',
+    'clipPath',
+    'color-profile',
+    'cursor',
+    'defs',
+    'desc',
+    'discard',
+    'ellipse',
+    'feBlend',
+    'feColorMatrix',
+    'feComponentTransfer',
+    'feComposite',
+    'feConvolveMatrix',
+    'feDiffuseLighting',
+    'feDisplacementMap',
+    'feDistantLight',
+    'feDropShadow',
+    'feFlood',
+    'feFuncA',
+    'feFuncB',
+    'feFuncG',
+    'feFuncR',
+    'feGaussianBlur',
+    'feImage',
+    'feMerge',
+    'feMergeNode',
+    'feMorphology',
+    'feOffset',
+    'fePointLight',
+    'feSpecularLighting',
+    'feSpotLight',
+    'feTile',
+    'feTurbulence',
+    'filter',
+    // 'font', Exclude this because it's also HTML
+    'font-face',
+    'font-face-format',
+    'font-face-name',
+    'font-face-src',
+    'font-face-uri',
+    'foreignObject',
+    'g',
+    'glyph',
+    'glyphRef',
+    'hatch',
+    'hatchpath',
+    'hkern',
+    // 'iframe', Exclude this because it's also HTML
+    'image',
+    'linearGradient',
+    'marker',
+    'mask',
+    'mesh',
+    'meshgradient',
+    'meshpatch',
+    'meshrow',
+    'metadata',
+    'missing-glyph',
+    'mpath',
     'path',
+    'pattern',
+    'polygon',
+    'polyline',
+    'radialGradient',
+    'rect',
+    // 'script', Exclude this because it's also HTML
+    'set',
+    'solidcolor',
+    'stop',
+    // 'style', Exclude this because it's also HTML
+    // 'svg', Exclude this because it's also HTML
+    'switch',
+    'symbol',
+    'text',
+    'textPath',
+    // 'title', Exclude this because it's also HTML
+    'tref',
+    'tspan',
+    'unknown',
+    'use',
+    // 'video', Exclude this because it's also HTML
+    'view',
+    'vkern',
   ];
 
   final bool _toolFriendlyAstOrigin;
@@ -66,6 +170,7 @@ class NgParser {
       ),
       tokens,
       _voidElements,
+      _svgElements,
       exceptionHandler,
     );
     var asts = parser.parse();
