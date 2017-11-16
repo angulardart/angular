ca8589b62d28180be257964e6f3842bb4e9afa07
alorenzen <alorenzen@google.com>
2017-11-15 16:12:27 -0800

    Cleanup angular_forms.dart to export everything from src/directives.dart.
    Previously, NumberValueAccessor was missing.
    
    PiperOrigin-RevId: 175897235

--- angular_forms/lib/angular_forms.dart
+++ angular_forms/lib/angular_forms.dart
@@ -11,34 +11,38 @@ import 'src/directives/radio_control_value_accessor.dart'
     show RadioControlRegistry;
 import 'src/form_builder.dart' show FormBuilder;
 
-export 'src/directives.dart' show formDirectives, RadioButtonState;
-export 'src/directives/abstract_control_directive.dart'
-    show AbstractControlDirective;
-export 'src/directives/checkbox_value_accessor.dart'
-    show CheckboxControlValueAccessor;
-export 'src/directives/control_container.dart' show ControlContainer;
-export 'src/directives/control_value_accessor.dart'
-    show ControlValueAccessor, TouchFunction, ChangeFunction, NG_VALUE_ACCESSOR;
-export 'src/directives/default_value_accessor.dart' show DefaultValueAccessor;
-export 'src/directives/form_interface.dart' show Form;
-export 'src/directives/ng_control.dart' show NgControl;
-export 'src/directives/ng_control_group.dart' show NgControlGroup;
-export 'src/directives/ng_control_name.dart' show NgControlName;
-export 'src/directives/ng_control_status.dart' show NgControlStatus;
-export 'src/directives/ng_form.dart' show NgForm;
-export 'src/directives/ng_form_control.dart' show NgFormControl;
-export 'src/directives/ng_form_model.dart' show NgFormModel;
-export 'src/directives/ng_model.dart' show NgModel;
-export 'src/directives/select_control_value_accessor.dart'
-    show NgSelectOption, SelectControlValueAccessor;
-export 'src/directives/shared.dart'
-    show composeValidators, setUpControlGroup, setUpControl;
-export 'src/directives/validators.dart'
+export 'src/directives.dart'
     show
-        RequiredValidator,
-        MinLengthValidator,
+        composeValidators,
+        setUpControl,
+        setUpControlGroup,
+        formDirectives,
+        AbstractControlDirective,
+        ChangeFunction,
+        CheckboxControlValueAccessor,
+        ControlContainer,
+        ControlValueAccessor,
+        DefaultValueAccessor,
+        Form,
         MaxLengthValidator,
+        MinLengthValidator,
+        NgControl,
+        NgControlGroup,
+        NgControlName,
+        NgControlStatus,
+        NgForm,
+        NgFormControl,
+        NgFormModel,
+        NgModel,
+        NgSelectOption,
+        NG_VALUE_ACCESSOR,
+        NumberValueAccessor,
         PatternValidator,
+        RadioControlValueAccessor,
+        RadioButtonState,
+        RequiredValidator,
+        SelectControlValueAccessor,
+        TouchFunction,
         Validator,
         ValidatorFn;
 export 'src/form_builder.dart' show FormBuilder;
