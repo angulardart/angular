fbec7d289d3015c2086d65ef346a5ca80b52f025
matanl <matanl@google.com>
2017-11-21 13:25:17 -0800

    refactor(Core): Remove "RenderComponentType" from the public API.
    It doesn't serve any public API purpose.
    
    PiperOrigin-RevId: 176552300

--- angular/lib/core.dart
+++ angular/lib/core.dart
@@ -6,6 +6,6 @@ export 'src/core/application_tokens.dart'
 export 'src/core/change_detection.dart';
 export 'src/core/di.dart';
 export 'src/core/metadata.dart';
-export 'src/core/render.dart';
+export 'src/core/render.dart' hide RenderComponentType;
 export 'src/core/zone.dart';
 export 'src/facade/facade.dart';
