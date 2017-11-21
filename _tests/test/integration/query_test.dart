26f739ed2a15a866c38eef114ce6b120b483608c
matanl <matanl@google.com>
2017-11-20 20:06:13 -0800

    test(Core): Add additional test cases for query, including regression cases.
    PiperOrigin-RevId: 176455513

--- _tests/test/integration/query_test.dart
+++ _tests/test/integration/query_test.dart
@@ -1,6 +1,8 @@
 @Tags(const ['codegen'])
 @TestOn('browser')
 
+import 'dart:async';
+
 import 'package:angular/angular.dart';
 import 'package:angular_test/angular_test.dart';
 import 'package:_tests/query_tests.dart';
@@ -18,12 +20,36 @@ void main() {
         new NgTestBed<TestDirectViewChildren>(),
         [1, 2, 3],
       ),
+      directViewChild: new TestCase(
+        new NgTestBed<TestDirectViewChild>(),
+        [1],
+      ),
+      viewChildrenAndEmbedded: new TestCase(
+        new NgTestBed<TestViewChildrenAndEmbedded>(),
+        [1, 3],
+      ),
+      viewChildEmbedded: new TestCase(
+        new NgTestBed<TestDirectViewChildEmbedded>(),
+        [1],
+      ),
+      viewChildNestedOffOn: new TestCase(
+        new NgTestBed<TestViewChildNestedOnOff>(),
+        [],
+      ),
+      viewChildNestedNgIfOffOn: new TestCase(
+        new NgTestBed<TestViewChildNestedNgIfOffOn>(),
+        [],
+      ),
+      viewChildNestedNgIfOffOnAsync: new TestCase(
+        new NgTestBed<TestViewChildNestedNgIfOffOnAsync>(),
+        [],
+      ),
     );
   });
 }
 
 @Component(
-  selector: 'test-view-children',
+  selector: 'test',
   directives: const [
     ValueDirective,
   ],
@@ -37,4 +63,134 @@ class TestDirectViewChildren extends HasChildren<ValueDirective> {
   @override
   @ViewChildren(ValueDirective)
   QueryList actualChildren;
+}
+
+@Component(
+  selector: 'test',
+  directives: const [
+    ValueDirective,
+  ],
+  template: r'''
+    <value [value]="1"></value>
+  ''',
+)
+class TestDirectViewChild extends HasChild<ValueDirective> {
+  @override
+  @ViewChild(ValueDirective)
+  ValueDirective child;
+}
+
+@Component(
+  selector: 'test',
+  directives: const [
+    AlwaysShowDirective,
+    ValueDirective,
+  ],
+  template: r'''
+    <value [value]="1"></value>
+    <template neverShow>
+      <value [value]="2"></value>
+    </template>
+    <template alwaysShow>
+      <value [value]="3"></value>
+    </template>
+  ''',
+)
+class TestViewChildrenAndEmbedded extends HasChildren<ValueDirective> {
+  @override
+  @ViewChildren(ValueDirective)
+  QueryList actualChildren;
+}
+
+@Component(
+  selector: 'test',
+  directives: const [
+    AlwaysShowDirective,
+    ValueDirective,
+  ],
+  template: r'''
+    <template alwaysShow>
+      <value [value]="1"></value>
+    </template>
+  ''',
+)
+class TestDirectViewChildEmbedded extends HasChild<ValueDirective> {
+  @override
+  @ViewChild(ValueDirective)
+  ValueDirective child;
+}
+
+@Component(
+  selector: 'test',
+  directives: const [
+    AlwaysShowDirective,
+    NeverShowDirective,
+    ValueDirective,
+  ],
+  template: r'''
+    <template alwaysShow>
+      <template neverShow>
+        <value [value]="1"></value>
+      </template>
+    </template>
+  ''',
+)
+class TestViewChildNestedOnOff extends HasChild<ValueDirective> {
+  @override
+  @ViewChild(ValueDirective)
+  ValueDirective child;
+}
+
+@Component(
+  selector: 'test',
+  directives: const [
+    NgIf,
+    ValueDirective,
+  ],
+  template: r'''
+    <div *ngIf="outerDiv">
+      <div *ngIf="innerDiv">
+        <value [value]="1"></value>
+      </div>
+    </div>
+  ''',
+)
+class TestViewChildNestedNgIfOffOn extends HasChild<ValueDirective> {
+  var outerDiv = false;
+  var innerDiv = true;
+
+  @override
+  @ViewChild(ValueDirective)
+  ValueDirective child;
+}
+
+@Component(
+  selector: 'test-regression-embedded-ngif-false-true-async',
+  directives: const [
+    NgIf,
+    ValueDirective,
+  ],
+  template: r'''
+    <div *ngIf="outerDiv">
+      <div *ngIf="innerDiv">
+        <value [value]="1"></value>
+      </div>
+    </div>
+  ''',
+)
+class TestViewChildNestedNgIfOffOnAsync extends HasChild<ValueDirective>
+    implements AfterViewInit {
+  var outerDiv = true;
+  var innerDiv = true;
+
+  @override
+  void ngAfterViewInit() {
+    scheduleMicrotask(() {
+      outerDiv = false;
+    });
+  }
+
+  @override
+  @ViewChild(ValueDirective)
+  ValueDirective child;
 }
