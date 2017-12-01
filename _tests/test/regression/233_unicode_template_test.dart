66ada6f150e102089e9666b44c6f4bebf235586c
matanl <matanl@google.com>
2017-11-30 21:39:45 -0800

    test(Core): Add a test proving that unicode characters work!
    A tiny tweak to the library name of another test, too.
    
    Fixes https://github.com/dart-lang/angular/issues/223.
    
    PiperOrigin-RevId: 177550081

---
+++ _tests/test/regression/233_unicode_template_test.dart
@@ -0,0 +1,36 @@
+@Tags(const ['codegen'])
+@TestOn('browser')
+
+import 'package:test/test.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_test/angular_test.dart';
+
+import '233_unicode_template_test.template.dart' as ng_generated;
+
+// Source: https://github.com/dart-lang/angular/issues/233.
+void main() {
+  ng_generated.initReflector();
+
+  test('should be able to compile templates with unicode', () async {
+    final testBed = new NgTestBed<App>();
+    final fixture = await testBed.create();
+    expect(
+      fixture.text,
+      allOf([
+        contains('🎂'),
+        contains('ΓΔ'),
+        contains('↔↕'),
+      ]),
+    );
+  });
+}
+
+@Component(
+  selector: 'app',
+  template: '''
+    <div>🎂</div>         <!-- Misc symbols: causes compilation error -->
+    <div>ΓΔ</div>         <!-- Greek: OK-->
+    <div>↔↕</div>         <!-- Arrows: OK-->
+  ''',
+)
+class App {}
