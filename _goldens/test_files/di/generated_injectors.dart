7dcf73e07e3639557a94c4b21b115d599673f01a
matanl <matanl@google.com>
2018-01-29 13:07:22 -0800

    test(Compiler): Add test cases and golden file data for unsupported useValue.
    Using @Injector.generate + const Provider(..., useValue: ...) currently is unsupported for almost everything that is not a simple literal value (a string, an int, a bool). The intention here is to support everything already supported by @Component (ViewCompiler), and eventually have the same guarantees for both.
    
    This CL adds data to start iterating on fixes and tracking progress.
    
    PiperOrigin-RevId: 183713604

--- _goldens/test_files/di/generated_injectors.dart
+++ _goldens/test_files/di/generated_injectors.dart
@@ -3,7 +3,7 @@ import 'package:angular/angular.dart';
 import 'generated_injectors.template.dart' as ng;
 
 @Injector.generate(const [
-  const Provider(Example, useClass: ExamplePrime),
+  const Provider(Example, useClass: Example),
 
   // TODO(matanl): As soon as ValueProvider is supported, use it.
   const Provider<String>(someMultiToken, useValue: 'A'),
@@ -17,13 +17,35 @@ import 'generated_injectors.template.dart' as ng;
   //   if (identical(token, const OpaqueToken<String
   const Provider<String>(tokenOfDynamic, useValue: 'dynamic'),
   const Provider<String>(tokenOfString, useValue: 'String'),
+
+  // These validate that using `useValue: ...` works with non-literals.
+  const Provider(arbitraryToken1, useValue: instanceOfExample),
+  const Provider(arbitraryToken2, useValue: instanceOfExamplePrime),
+  const Provider(arbitraryToken3, useValue: ExampleTheta.instance),
 ])
 Injector doGenerate() => ng.doGenerate$Injector();
 
-class Example {}
+class Example {
+  const Example();
+}
 
-class ExamplePrime implements Example {}
+class ExamplePrime implements Example {
+  const ExamplePrime._withPrivateConstructor();
+}
+
+class ExampleTheta implements Example {
+  static const instance = const ExampleTheta._withPrivateConstructor();
+
+  const ExampleTheta._withPrivateConstructor();
+}
 
 const someMultiToken = const MultiToken('someMultiToken');
 const tokenOfDynamic = const OpaqueToken<dynamic>('someToken');
 const tokenOfString = const OpaqueToken<String>('someToken');
+
+const arbitraryToken1 = const OpaqueToken('arbitraryToken1');
+const arbitraryToken2 = const OpaqueToken('arbitraryToken2');
+const arbitraryToken3 = const OpaqueToken('arbitraryToken3');
+
+const instanceOfExample = const Example();
+const instanceOfExamplePrime = const ExamplePrime._withPrivateConstructor();
---
+++ _tests/test/di/generated/injector_use_value_test.dart
@@ -0,0 +1,127 @@
+@TestOn('browser')
+import 'package:angular/angular.dart';
+import 'package:test/test.dart';
+
+import 'injector_use_value_test.template.dart' as ng;
+
+// Tests specifically for @Injector.generate + useValue: ...
+//
+// These tend to be complicated and more isolated than other code.
+void main() {
+  Injector injector;
+
+  setUp(() => injector = example());
+
+  // TODO(matanl): Gradually enable actual test cases as they are supported.
+  group('should resolve useValue: targeting a', () {
+    test('class with a const constructor', () {
+      expect(
+        () => injector.get(ClassWithConstConstructor),
+        throwsUnimplementedError,
+      );
+    });
+
+    test('class with a named const constructor', () {
+      expect(
+        () => injector.get(ClassWithNamedConstConstructor),
+        throwsUnimplementedError,
+      );
+    });
+
+    test('class with multiple constructors, at least one const', () {
+      expect(
+        () => injector.get(ClassWithMultipleConstructors),
+        throwsUnimplementedError,
+      );
+    });
+
+    test('class with a private constructor, but a public static field', () {
+      expect(
+        () => injector.get(ClassWithPrivateConstructorAndStaticField),
+        throwsUnimplementedError,
+      );
+    });
+
+    test('class with a private constructor, but a public top-level field', () {
+      expect(
+        () => injector.get(ClassWithPrivateConstructorAndTopLevelField),
+        throwsUnimplementedError,
+      );
+    });
+
+    test('class with a redirecting constructor', () {
+      expect(
+        () => injector.get(ClassWithRedirectingConstructor),
+        throwsUnimplementedError,
+      );
+    });
+  });
+}
+
+@Injector.generate(const [
+  const Provider(
+    ClassWithConstConstructor,
+    useValue: const ClassWithConstConstructor(),
+  ),
+  const Provider(
+    ClassWithNamedConstConstructor,
+    useValue: const ClassWithNamedConstConstructor.someName(),
+  ),
+  const Provider(
+    ClassWithMultipleConstructors,
+    useValue: const ClassWithMultipleConstructors.isConst(),
+  ),
+  const Provider(
+    ClassWithPrivateConstructorAndStaticField,
+    useValue: ClassWithPrivateConstructorAndStaticField.instance,
+  ),
+  const Provider(
+    ClassWithPrivateConstructorAndTopLevelField,
+    useValue: topLevelInstance,
+  ),
+  const Provider(
+    ClassWithRedirectingConstructor,
+    useValue: const ClassWithRedirectingConstructor(),
+  ),
+])
+Injector example() => ng.example$Injector();
+
+// An example of a class with a single, default, "const" constructor.
+class ClassWithConstConstructor {
+  const ClassWithConstConstructor();
+}
+
+// An example of a class with a single, named, "const" constructor.
+class ClassWithNamedConstConstructor {
+  const ClassWithNamedConstConstructor.someName();
+}
+
+// An example of a class with multiple constructors, at least one const.
+class ClassWithMultipleConstructors {
+  ClassWithMultipleConstructors.notConst();
+  const ClassWithMultipleConstructors.isConst();
+}
+
+// An example of a class that can't be created in generated files, but has an
+// existing (public) instance that can be referenced as a static field.
+class ClassWithPrivateConstructorAndStaticField {
+  static const instance = const ClassWithPrivateConstructorAndStaticField._();
+  const ClassWithPrivateConstructorAndStaticField._();
+}
+
+// An example of a class that can't be created in generated files, but has an
+// existing (public) instance that can be referenced as a top-level field.
+const topLevelInstance = const ClassWithPrivateConstructorAndTopLevelField._();
+
+class ClassWithPrivateConstructorAndTopLevelField {
+  const ClassWithPrivateConstructorAndTopLevelField._();
+}
+
+// An example of a class with a redirecting factory constructor.
+abstract class ClassWithRedirectingConstructor {
+  const factory ClassWithRedirectingConstructor() = _ConcreteClass;
+}
+
+class _ConcreteClass implements ClassWithRedirectingConstructor {
+  const _ConcreteClass();
+}
