784f3e0b48fea6cabafd18c622e3e877bdf8a0b9
matanl <matanl@google.com>
2018-01-30 14:29:29 -0800

    test(Core): Add a test that shows that "expression has changed" works.
    PiperOrigin-RevId: 183892699

---
+++ _tests/test/core/change_detection/expect_no_changes_dev_mode_test.dart
@@ -0,0 +1,30 @@
+import 'package:angular_test/angular_test.dart';
+import 'package:test/test.dart';
+import 'package:angular/angular.dart';
+
+import 'expect_no_changes_dev_mode_test.template.dart' as ng_generated;
+
+void main() {
+  ng_generated.initReflector();
+
+  test('should throw during change detection', () async {
+    final testBed = new NgTestBed<IllegalChangeDetectionComponent>();
+    expect(
+      testBed.create(),
+      throwsA(predicate(
+        (e) => '$e'.contains('Expression has changed after it was checked'),
+      )),
+    );
+  });
+}
+
+@Component(
+  selector: 'illegal-change-detection',
+  template: r'''
+    <div>{{counter}}</div>
+  ''',
+)
+class IllegalChangeDetectionComponent {
+  var _counter = 0;
+  int get counter => _counter++;
+}
