d5e2c784e99f1a2b00759c149c63a9167bd940de
Andrew Lorenzen <alorenzen@google.com>
2018-01-30 17:02:45 -0800

    Update travis.sh with mono_repo 0.2.1

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,10 +5,10 @@
 set -e
 
 if [ -z "$PKG" ]; then
-  echo -e "[31mPKG environment variable must be set![0m"
+  echo -e "\033[31mPKG environment variable must be set!\033[0m"
   exit 1
 elif [ -z "$TASK" ]; then
-  echo -e "[31mTASK environment variable must be set![0m"
+  echo -e "\033[31mTASK environment variable must be set!\033[0m"
   exit 1
 fi
 
@@ -17,91 +17,93 @@ pub upgrade
 
 case $PKG in
 _goldens) echo
-  echo -e "[1mPKG: _goldens[22m"
+  echo -e "\033[1mPKG: _goldens\033[22m"
+  echo -e "  Running `tool/travis.sh`"
   tool/travis.sh
   ;;
 _tests) echo
-  echo -e "[1mPKG: _tests[22m"
+  echo -e "\033[1mPKG: _tests\033[22m"
+  echo -e "  Running `tool/travis.sh`"
   tool/travis.sh
   ;;
 angular_test) echo
-  echo -e "[1mPKG: angular_test[22m"
+  echo -e "\033[1mPKG: angular_test\033[22m"
+  echo -e "  Running `tool/travis.sh`"
   tool/travis.sh
   ;;
 angular_router) echo
-  echo -e "[1mPKG: angular_router[22m"
+  echo -e "\033[1mPKG: angular_router\033[22m"
+  echo -e "  Running `tool/travis.sh`"
   tool/travis.sh
-  ;;
-*) echo -e "No before_script specified for PKG '${PKG}'."
   ;;
 esac
 
 case $TASK in
 dartanalyzer) echo
-  echo -e "[1mTASK: dartanalyzer[22m"
+  echo -e "\033[1mTASK: dartanalyzer\033[22m"
   dartanalyzer --fatal-warnings .
   ;;
 dartanalyzer_1) echo
-  echo -e "[1mTASK: dartanalyzer_1[22m"
+  echo -e "\033[1mTASK: dartanalyzer_1\033[22m"
   dartanalyzer --fatal-warnings lib test
   ;;
 test) echo
-  echo -e "[1mTASK: test[22m"
+  echo -e "\033[1mTASK: test\033[22m"
   pub run test
   ;;
 test_1) echo
-  echo -e "[1mTASK: test_1[22m"
+  echo -e "\033[1mTASK: test_1\033[22m"
   pub run test -p vm -x codegen
   ;;
 test_10) echo
-  echo -e "[1mTASK: test_10[22m"
+  echo -e "\033[1mTASK: test_10\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/symbol_inspector
   ;;
 test_11) echo
-  echo -e "[1mTASK: test_11[22m"
+  echo -e "\033[1mTASK: test_11\033[22m"
   pub run test -p vm
   ;;
 test_12) echo
-  echo -e "[1mTASK: test_12[22m"
+  echo -e "\033[1mTASK: test_12\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --timeout=4x -x skip_on_travis -j1
   ;;
 test_13) echo
-  echo -e "[1mTASK: test_13[22m"
+  echo -e "\033[1mTASK: test_13\033[22m"
   pub run test --precompiled=build --platform=chrome -j1
   ;;
 test_2) echo
-  echo -e "[1mTASK: test_2[22m"
+  echo -e "\033[1mTASK: test_2\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/common
   ;;
 test_3) echo
-  echo -e "[1mTASK: test_3[22m"
+  echo -e "\033[1mTASK: test_3\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/compiler
   ;;
 test_4) echo
-  echo -e "[1mTASK: test_4[22m"
+  echo -e "\033[1mTASK: test_4\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/core
   ;;
 test_5) echo
-  echo -e "[1mTASK: test_5[22m"
+  echo -e "\033[1mTASK: test_5\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/di
   ;;
 test_6) echo
-  echo -e "[1mTASK: test_6[22m"
+  echo -e "\033[1mTASK: test_6\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/integration
   ;;
 test_7) echo
-  echo -e "[1mTASK: test_7[22m"
+  echo -e "\033[1mTASK: test_7\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/platform
   ;;
 test_8) echo
-  echo -e "[1mTASK: test_8[22m"
+  echo -e "\033[1mTASK: test_8\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/security
   ;;
 test_9) echo
-  echo -e "[1mTASK: test_9[22m"
+  echo -e "\033[1mTASK: test_9\033[22m"
   pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/source_gen
   ;;
-*) echo -e "[31mNot expecting TASK '${TASK}'. Error![0m"
+*) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
   exit 1
   ;;
 esac
