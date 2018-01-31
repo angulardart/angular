612fcf5f8b551c30dfa92e116d3956a41717b553
Andrew Lorenzen <alorenzen@google.com>
2018-01-30 17:30:20 -0800

    Add travis.yml files for each sub-package, and use mono_repo to generate root config.
    Closes #765
    
    PiperOrigin-RevId: 183920156

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,15 +1,109 @@
 #!/bin/bash
-
-# Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
-# for details. All rights reserved. Use of this source code is governed by a
-# BSD-style license that can be found in the LICENSE file.
+# Created with https://github.com/dart-lang/mono_repo
 
 # Fast fail the script on failures.
 set -e
 
-# Use only one worker per task to limit resource usage on travis
-export BUILD_MAX_WORKERS_PER_TASK=1
+if [ -z "$PKG" ]; then
+  echo -e "\033[31mPKG environment variable must be set!\033[0m"
+  exit 1
+elif [ -z "$TASK" ]; then
+  echo -e "\033[31mTASK environment variable must be set!\033[0m"
+  exit 1
+fi
 
 pushd $PKG
 pub upgrade
-./tool/travis.sh
+
+case $PKG in
+_goldens) echo
+  echo -e "\033[1mPKG: _goldens\033[22m"
+  echo -e "  Running `tool/travis.sh`"
+  tool/travis.sh
+  ;;
+_tests) echo
+  echo -e "\033[1mPKG: _tests\033[22m"
+  echo -e "  Running `tool/travis.sh`"
+  tool/travis.sh
+  ;;
+angular_test) echo
+  echo -e "\033[1mPKG: angular_test\033[22m"
+  echo -e "  Running `tool/travis.sh`"
+  tool/travis.sh
+  ;;
+angular_router) echo
+  echo -e "\033[1mPKG: angular_router\033[22m"
+  echo -e "  Running `tool/travis.sh`"
+  tool/travis.sh
+  ;;
+esac
+
+case $TASK in
+dartanalyzer) echo
+  echo -e "\033[1mTASK: dartanalyzer\033[22m"
+  dartanalyzer --fatal-warnings .
+  ;;
+dartanalyzer_1) echo
+  echo -e "\033[1mTASK: dartanalyzer_1\033[22m"
+  dartanalyzer --fatal-warnings lib test
+  ;;
+test) echo
+  echo -e "\033[1mTASK: test\033[22m"
+  pub run test
+  ;;
+test_1) echo
+  echo -e "\033[1mTASK: test_1\033[22m"
+  pub run test -p vm -x codegen
+  ;;
+test_10) echo
+  echo -e "\033[1mTASK: test_10\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/symbol_inspector
+  ;;
+test_11) echo
+  echo -e "\033[1mTASK: test_11\033[22m"
+  pub run test -p vm
+  ;;
+test_12) echo
+  echo -e "\033[1mTASK: test_12\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --timeout=4x -x skip_on_travis -j1
+  ;;
+test_13) echo
+  echo -e "\033[1mTASK: test_13\033[22m"
+  pub run test --precompiled=build --platform=chrome -j1
+  ;;
+test_2) echo
+  echo -e "\033[1mTASK: test_2\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/common
+  ;;
+test_3) echo
+  echo -e "\033[1mTASK: test_3\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/compiler
+  ;;
+test_4) echo
+  echo -e "\033[1mTASK: test_4\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/core
+  ;;
+test_5) echo
+  echo -e "\033[1mTASK: test_5\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/di
+  ;;
+test_6) echo
+  echo -e "\033[1mTASK: test_6\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/integration
+  ;;
+test_7) echo
+  echo -e "\033[1mTASK: test_7\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/platform
+  ;;
+test_8) echo
+  echo -e "\033[1mTASK: test_8\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/security
+  ;;
+test_9) echo
+  echo -e "\033[1mTASK: test_9\033[22m"
+  pub run test --precompiled=build --platform=chrome --tags=codegen --exclude-tags=known_pub_serve_failure -j1 test/source_gen
+  ;;
+*) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+  exit 1
+  ;;
+esac
