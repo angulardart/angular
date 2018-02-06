7bae49acbb0276a1579e0161f7673509dbb5932b
leonsenft <leonsenft@google.com>
2018-02-06 12:46:14 -0800

    Adds test case and golden files for `Visibility.local` behavior
    There's currently a discrepancy between the documentation and the behavior
    which will be fixed.
    
    PiperOrigin-RevId: 184723807

---
+++ _goldens/test_files/visibility.dart
@@ -0,0 +1,43 @@
+import 'package:angular/angular.dart';
+
+abstract class Dependency {}
+
+@Component(
+  selector: 'dependent',
+  template: '',
+)
+class Dependent {
+  Dependent(Dependency _);
+}
+
+@Component(
+  selector: 'dependency-with-dependent-in-view',
+  template: '<dependent></dependent>',
+  directives: const [Dependent],
+  providers: const [
+    const Provider(Dependency, useExisting: DependencyWithDependentInView),
+  ],
+  visibility: Visibility.local,
+)
+class DependencyWithDependentInView implements Dependency {}
+
+@Component(
+  selector: 'dependency-with-content',
+  template: '<ng-content></ng-content>',
+  providers: const [
+    const Provider(Dependency, useExisting: DependencyWithContent),
+  ],
+  visibility: Visibility.local,
+)
+class DependencyWithContent implements Dependency {}
+
+@Component(
+  selector: 'dependency-and-dependent-in-view',
+  template: '''
+    <dependency>
+      <dependent></dependent>
+    </dependency>
+  ''',
+  directives: const [Dependency, Dependent],
+)
+class DependencyAndDependentInView {}
