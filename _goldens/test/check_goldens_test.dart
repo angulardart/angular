0c9376503ab6db127ab913113e92bd747c576ffb
matanl <matanl@google.com>
2018-02-14 12:34:07 -0800

    fix(Compiler): No longer ingnore classes not annotated with `@Injectable`.
    Closes https://github.com/dart-lang/angular/issues/849.
    
    PiperOrigin-RevId: 185727049

--- _goldens/test/check_goldens_test.dart
+++ _goldens/test/check_goldens_test.dart
@@ -13,8 +13,9 @@ final _isBazel = Platform.environment['RUNFILES'] != null;
 
 void main() {
   Glob findFiles;
+  String path;
   if (_isBazel) {
-    final path = p.join(
+    path = p.join(
       p.current,
       const String.fromEnvironment('BAZEL_ROOT_PATH'),
     );
@@ -23,20 +24,23 @@ void main() {
       throw new ArgumentError('BAZEL_ROOT_PATH not defined (via -D)');
     }
     if (!new Directory(path).existsSync()) {
-      throw new ArgumentError(
+      throw new StateError(
         'Directory not found: $path (in ${Directory.current})',
       );
     }
     findFiles = new Glob(p.join(path, _inputFiles));
   } else {
-    final path = 'build';
+    path = 'build';
     if (!new File(p.join(path, 'test', '.packages')).existsSync()) {
       // Build runner specific: We require --precompiled=build.
-      throw new ArgumentError('Test must be executed via --precompiled=build');
+      throw new StateError('Test must be executed via --precompiled=build');
     }
     findFiles = new Glob(p.join(path, _inputFiles));
   }
   final inputDartFiles = findFiles.listSync();
+  if (inputDartFiles.isEmpty) {
+    throw new StateError('Expected 1 or more tests, got 0 from $findFiles');
+  }
   for (final mode in _outputModes) {
     group('Goldens for "$mode" should be updated in', () {
       for (final file in inputDartFiles) {
