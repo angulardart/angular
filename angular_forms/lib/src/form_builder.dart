30c702aa0d2d7c096f69f5197238449993d911f3
alorenzen <alorenzen@google.com>
2018-02-20 11:11:40 -0800

    Deprecate instance methods in FormBuilder.
    Replaced with static methods instead.
    
    PiperOrigin-RevId: 186330916

--- angular_forms/lib/src/form_builder.dart
+++ angular_forms/lib/src/form_builder.dart
@@ -44,54 +44,62 @@ import 'model.dart' as model_module;
 /// ```
 @Injectable()
 class FormBuilder {
-  /// Construct a new [ControlGroup] with the given map of configuration.
-  /// Valid keys for the `extra` parameter map are [optionals] and [validator].
-  ///
-  /// See the [ControlGroup] constructor for more details.
+  @Deprecated('Use static method FormBuilder.controlGroup instead.')
   model_module.ControlGroup group(Map<String, dynamic> controlsConfig,
       [Map<String, dynamic> extra]) {
-    var controls = _reduceControls(controlsConfig);
     var optionals =
         ((extra != null ? extra['optionals'] : null) as Map<String, bool>);
     ValidatorFn validator =
         extra != null ? extra['validator'] as ValidatorFn : null;
+    return controlGroup(controlsConfig,
+        validator: validator, optionals: optionals);
+  }
+
+  /// Construct a new [ControlGroup] with the given map of configuration.
+  /// Valid keys for the `extra` parameter map are [optionals] and [validator].
+  ///
+  /// See the [ControlGroup] constructor for more details.
+  static model_module.ControlGroup controlGroup(
+      Map<String, dynamic> controlsConfig,
+      {Map<String, bool> optionals,
+      ValidatorFn validator}) {
+    var controls = _reduceControls(controlsConfig);
     return new model_module.ControlGroup(controls, optionals, validator);
   }
 
   /// Construct a new [Control] with the given [value], and [validator].
-  model_module.Control control(Object value, [ValidatorFn validator]) {
-    return new model_module.Control(value, validator);
-  }
+  @Deprecated('Use new Control(value, validator) directly')
+  model_module.Control control(Object value, [ValidatorFn validator]) =>
+      new model_module.Control(value, validator);
+
+  @Deprecated('Use static method FormBuilder.controlArray instead.')
+  model_module.ControlArray array(List<dynamic> controlsConfig,
+          [ValidatorFn validator]) =>
+      controlArray(controlsConfig, validator);
 
   /// Construct an array of [Control]s from the given [controlsConfig] array of
   /// configuration, with the given optional [validator].
-  model_module.ControlArray array(List<dynamic> controlsConfig,
+  static model_module.ControlArray controlArray(List<dynamic> controlsConfig,
       [ValidatorFn validator]) {
-    var controls = controlsConfig.map((c) => _createControl(c)).toList();
+    var controls = controlsConfig.map(_createControl).toList();
     return new model_module.ControlArray(controls, validator);
   }
 
-  Map<String, model_module.AbstractControl> _reduceControls(
-      Map<String, dynamic> controlsConfig) {
-    Map<String, model_module.AbstractControl> controls = {};
-    controlsConfig.forEach((String controlName, dynamic controlConfig) {
-      controls[controlName] = _createControl(controlConfig);
-    });
-    return controls;
-  }
+  static Map<String, model_module.AbstractControl> _reduceControls(
+          Map<String, dynamic> controlsConfig) =>
+      controlsConfig.map((controlName, controlConfig) =>
+          new MapEntry(controlName, _createControl(controlConfig)));
 
-  model_module.AbstractControl _createControl(dynamic controlConfig) {
-    if (controlConfig is model_module.Control ||
-        controlConfig is model_module.ControlGroup ||
-        controlConfig is model_module.ControlArray) {
+  static model_module.AbstractControl _createControl(dynamic controlConfig) {
+    if (controlConfig is model_module.AbstractControl) {
       return controlConfig;
     } else if (controlConfig is List) {
       var value = controlConfig[0];
       ValidatorFn validator =
           controlConfig.length > 1 ? controlConfig[1] as ValidatorFn : null;
-      return control(value, validator);
+      return new model_module.Control(value, validator);
     } else {
-      return control(controlConfig);
+      return new model_module.Control(controlConfig, null);
     }
   }
 }
--- angular_forms/test/form_builder_test.dart
+++ angular_forms/test/form_builder_test.dart
@@ -7,58 +7,51 @@ Map<String, dynamic> _syncValidator(AbstractControl c) {
 }
 
 void main() {
-  var syncValidator = _syncValidator;
-
   group('Form Builder', () {
-    var b;
-    setUp(() {
-      b = new FormBuilder();
-    });
     test('should create controls from a value', () {
-      var g = b.group({'login': 'some value'});
-      expect(g.controls['login'].value, 'some value');
+      var group = FormBuilder.controlGroup({'login': 'some value'});
+      expect(group.controls['login'].value, 'some value');
     });
     test('should create controls from an array', () {
-      var g = b.group({
+      var group = FormBuilder.controlGroup({
         'login': ['some value'],
-        'password': ['some value', syncValidator]
+        'password': ['some value', _syncValidator]
       });
-      expect(g.controls['login'].value, 'some value');
-      expect(g.controls['password'].value, 'some value');
-      expect(g.controls['password'].validator == syncValidator, true);
+      expect(group.controls['login'].value, 'some value');
+      expect(group.controls['password'].value, 'some value');
+      expect(group.controls['password'].validator == _syncValidator, true);
     });
     test('should use controls', () {
-      var g = b.group({'login': b.control('some value', syncValidator)});
-      expect(g.controls['login'].value, 'some value');
-      expect(g.controls['login'].validator == syncValidator, true);
+      var group = FormBuilder
+          .controlGroup({'login': new Control('some value', _syncValidator)});
+      expect(group.controls['login'].value, 'some value');
+      expect(group.controls['login'].validator == _syncValidator, true);
     });
     test('should create groups with optional controls', () {
-      var g = b.group({
-        'login': 'some value'
-      }, {
-        'optionals': {'login': false}
-      });
-      expect(g.contains('login'), false);
+      var group = FormBuilder
+          .controlGroup({'login': 'some value'}, optionals: {'login': false});
+      expect(group.contains('login'), false);
     });
     test('should create groups with a custom validator', () {
-      var g = b.group({'login': 'some value'}, {'validator': syncValidator});
-      expect(g.validator == syncValidator, true);
+      var group = FormBuilder
+          .controlGroup({'login': 'some value'}, validator: _syncValidator);
+      expect(group.validator == _syncValidator, true);
     });
     test('should create control arrays', () {
-      var c = b.control('three');
-      var a = b.array([
+      var control = new Control('three');
+      var array = FormBuilder.controlArray([
         'one',
-        ['two', syncValidator],
-        c,
-        b.array(['four'])
-      ], syncValidator);
-      expect(a.value, [
+        ['two', _syncValidator],
+        control,
+        FormBuilder.controlArray(['four'])
+      ], _syncValidator);
+      expect(array.value, [
         'one',
         'two',
         'three',
         ['four']
       ]);
-      expect(a.validator == syncValidator, true);
+      expect(array.validator == _syncValidator, true);
     });
   });
 }
