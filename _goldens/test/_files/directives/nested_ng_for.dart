28ccc51f1fdc91e71a41a4b45723bd5018b93ff7
leonsenft <leonsenft@google.com>
2018-02-20 12:17:00 -0800

    Adds golden test to see delta of upcoming change
    PiperOrigin-RevId: 186342794

---
+++ _goldens/test/_files/directives/nested_ng_for.dart
@@ -0,0 +1,14 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'nested-ng-for',
+  template: '''
+    <span *ngFor="let row of matrix">
+      <div *ngFor="let value of row">{{value}}</div>
+    </span>
+  ''',
+  directives: const [NgFor],
+)
+class NestedNgForComponent {
+  List<List<int>> matrix;
+}
