b7828212d81fb43ee84b9fb22b217cfa77085e2a
alorenzen <alorenzen@google.com>
2018-02-27 16:31:10 -0800

    cleanup(angular_forms): Actually fill in the generic type for AbstractControlDirective.
    PiperOrigin-RevId: 187251477

--- angular_forms/lib/src/directives/control_container.dart
+++ angular_forms/lib/src/directives/control_container.dart
@@ -1,21 +1,12 @@
-import '../model.dart' show AbstractControl;
+import '../model.dart' show ControlGroup;
 import 'abstract_control_directive.dart' show AbstractControlDirective;
 import 'form_interface.dart' show Form;
 
-/// A directive that contains multiple [NgControl]s.
+/// A directive that contains multiple [NgControl]s contained in a
+/// [ControlGroup].
 ///
 /// Only used by the forms package.
-class ControlContainer<T extends AbstractControl>
-    extends AbstractControlDirective<T> {
-  String name;
-
+abstract class ControlContainer extends AbstractControlDirective<ControlGroup> {
   /// Get the form to which this container belongs.
-  Form get formDirective => null;
-
-  /// Get the path to this container.
-  @override
-  List<String> get path => null;
-
-  @override
-  T get control => null;
+  Form get formDirective;
 }
