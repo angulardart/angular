d54c00c82edcc6f4f2d96cd931026c6a0d72b56e
Matan Lurey <matan@lurey.org>
2018-03-06 18:26:36 -0800

    Reformat the 'dev' package, and align SDK versions.

---
+++ dev/lib/find.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:glob/glob.dart';
+
+/// Returns a list of all `pubspec.yaml` files that are relevant to us.
+List<File> findRelevantPubspecs() => (_include.listSync().toList()
+      ..sort((a, b) => a.path.compareTo(b.path))
+      ..removeWhere((f) => _exclude.any((g) => g.matches(f.path))))
+    .cast<File>();
+
+// Find all packages.
+final _include = new Glob('**/pubspec.yaml');
+
+// ... But exclude ones not relevant here.
+// TODO: Perhaps just import .gitignore as well.
+final _exclude = [
+  new Glob('dev/**'),
+  new Glob('angular/tools/**'),
+  new Glob('**/build/**'),
+];
