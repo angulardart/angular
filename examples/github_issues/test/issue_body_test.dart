037abf174daa7db004393c235dc7ccb8014b8016
Matan Lurey <matan@lurey.org>
2018-03-15 10:41:46 -0700

    Examples: Move github_issues from site-webdev to this repository.
    Minor changes in order to remove the dependency on pkg/pageloader, and to use the latest versions of build/build_runner (and patterns they use). Added to travis as well.
    
    Closes #1029
    
    PiperOrigin-RevId: 189209226

---
+++ examples/github_issues/test/issue_body_test.dart
@@ -0,0 +1,29 @@
+@TestOn('browser')
+library angular_simple_test;
+
+import 'package:examples.github_issues/api.dart';
+import 'package:examples.github_issues/ui.dart';
+import 'package:angular_test/angular_test.dart';
+import 'package:test/test.dart';
+
+import 'issue_body_test.template.dart' as ng;
+
+void main() {
+  ng.initReflector();
+  tearDown(disposeAnyRunningTest);
+
+  group('$IssueBodyComponent', () {
+    test('should properly render markdown', () async {
+      var testBed = new NgTestBed<IssueBodyComponent>();
+      var fixture = await testBed.create(beforeChangeDetection: (c) {
+        c.issue = new GithubIssue(
+          description: '**Hello World**',
+        );
+      });
+      expect(
+        fixture.rootElement.innerHtml,
+        contains('<strong>Hello World</strong>'),
+      );
+    });
+  });
+}
