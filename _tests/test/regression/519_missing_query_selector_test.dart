f080874d776d9ae8c057d21b382bd02de8c3e8c3
Matan Lurey <matan@lurey.org>
2018-03-15 18:30:29 -0700

    fix(Compiler): Fail on missing selector for queries.

---
+++ _tests/test/regression/519_missing_query_selector_test.dart
@@ -0,0 +1,23 @@
+@TestOn('vm')
+import 'package:_tests/compiler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('should fail on a missing @ViewChild() query', () async {
+    await compilesExpecting("""
+      import 'dart:html';
+      import '$ngImport';
+
+      @Component(
+        selector: 'hero',
+        template: '<div></div>',
+      )
+      class HeroComponent {
+        @ViewChild()
+        Element div;
+      }
+    """, errors: [
+      contains('Missing selector argument for "@ViewChild"'),
+    ]);
+  });
+}
