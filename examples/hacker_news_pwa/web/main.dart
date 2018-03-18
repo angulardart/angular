75950fddd69914963e41cd2c10f5797febd48f61
Kevin Moore <kevmoo@google.com>
2018-03-17 17:55:31 -0700

    Remove use of pkg:http

--- examples/hacker_news_pwa/web/main.dart
+++ examples/hacker_news_pwa/web/main.dart
@@ -1,8 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular/experimental.dart';
 import 'package:angular_router/angular_router.dart';
-import 'package:http/http.dart';
-import 'package:http/browser_client.dart';
 import 'package:pwa/client.dart' as pwa;
 
 // We are ignoring files that will be generated at compile-time.
@@ -17,15 +15,12 @@ import 'main.template.dart' as ng;
 @GenerateInjector(const [
   // HTTP and Services.
   const ClassProvider(HackerNewsService),
-  const FactoryProvider(BaseClient, createBrowserClient),
   const ValueProvider.forToken(baseUrl, defaultBaseUrl),
 
   // SPA Router.
   routerProviders,
 ])
 final InjectorFactory hackerNewsApp = ng.hackerNewsApp$Injector;
-
-BrowserClient createBrowserClient() => new BrowserClient();
 
 void main() {
   // Install service worker.
