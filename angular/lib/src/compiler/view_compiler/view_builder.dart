fa645f63018bb2cadeae3f50aec00fd477f36624
het <het@google.com>
2018-03-20 16:03:42 -0700

    cleanup(Compiler): Rename 'isSimpleNgIf' to 'isPureHtml' to distinguish this case from the case where the NgIf contains a single child component.
    PiperOrigin-RevId: 189829349

--- angular/lib/src/compiler/view_compiler/view_builder.dart
+++ angular/lib/src/compiler/view_compiler/view_builder.dart
@@ -276,12 +276,12 @@ class ViewBuilderVisitor implements TemplateAstVisitor<void, CompileElement> {
 
   void visitEmbeddedTemplate(EmbeddedTemplateAst ast, CompileElement parent) {
     var nodeIndex = view.nodes.length;
-    var isSimpleNgIf = !visitingProjectedContent && _isSimpleNgIf(ast);
-    if (isSimpleNgIf) {
+    var isPureHtml = !visitingProjectedContent && _isPureHtml(ast);
+    if (isPureHtml) {
       view.hasInlinedView = true;
     }
     NodeReference nodeReference =
-        view.createViewContainerAnchor(parent, nodeIndex, ast, isSimpleNgIf);
+        view.createViewContainerAnchor(parent, nodeIndex, ast, isPureHtml);
     var directives =
         ast.directives.map((directiveAst) => directiveAst.directive).toList();
     var compileElement = new CompileElement(
@@ -298,7 +298,7 @@ class ViewBuilderVisitor implements TemplateAstVisitor<void, CompileElement> {
         ast.references,
         logger,
         hasTemplateRefQuery: parent.hasTemplateRefQuery,
-        isInlined: isSimpleNgIf);
+        isInlined: isPureHtml);
     view.nodes.add(compileElement);
     nestedViewCount++;
     var embeddedView = new CompileView(
@@ -310,12 +310,12 @@ class ViewBuilderVisitor implements TemplateAstVisitor<void, CompileElement> {
         compileElement,
         ast.variables,
         view.deferredModules,
-        isInlined: isSimpleNgIf);
+        isInlined: isPureHtml);
 
     // Create a visitor for embedded view and visit all nodes.
     var embeddedViewVisitor = new ViewBuilderVisitor(
         embeddedView, parser, stylesCompileResult,
-        isInlinedView: isSimpleNgIf);
+        isInlinedView: isPureHtml);
     templateVisitAll(
         embeddedViewVisitor,
         ast.children,
@@ -323,11 +323,11 @@ class ViewBuilderVisitor implements TemplateAstVisitor<void, CompileElement> {
             embeddedView.declarationElement);
     nestedViewCount += embeddedViewVisitor.nestedViewCount;
 
-    if (!isSimpleNgIf) {
+    if (!isPureHtml) {
       compileElement.beforeChildren(false);
     }
     _addRootNodeAndProject(compileElement, ast.ngContentIndex, parent);
-    if (!isSimpleNgIf) {
+    if (!isPureHtml) {
       compileElement.afterChildren(0);
     }
     if (ast.hasDeferredComponent) {
@@ -909,14 +909,14 @@ void writeInputUpdater(
       .add(o.fn(arguments, statements, o.BOOL_TYPE).toDeclStmt(name));
 }
 
-final _isPureHtml = new IsPureHtmlVisitor();
+final _pureHtmlVisitor = new IsPureHtmlVisitor();
 
-bool _isSimpleNgIf(EmbeddedTemplateAst ast) {
+bool _isPureHtml(EmbeddedTemplateAst ast) {
   if (ast.directives.length != 1) return false;
   var isNgIf = ast.directives.single.directive.identifier.name == 'NgIf';
   if (!isNgIf) return false;
 
-  return ast.children.every((t) => t.visit(_isPureHtml, null));
+  return ast.children.every((t) => t.visit(_pureHtmlVisitor, null));
 }
 
 /// Constructs name of global function that can be used to update an input
