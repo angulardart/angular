d5aef2f753f0a9211b34e3a3c28e0857dc8755fe
Matan Lurey <matan@lurey.org>
2018-03-28 13:57:43 -0700

    feat(Ast): Add some support, tests for minify whitespace.
    Not ready for use, as seen by the skipped tests - but I hope to keep the implementation as simple as possible, even if it is non-perfect optimization wise. It's always possible to improve the optimizations later.
    
    Closes #1147
    
    PiperOrigin-RevId: 190828520

---
+++ angular_ast/test/whitespace_test.dart
@@ -0,0 +1,73 @@
+import 'package:angular_ast/angular_ast.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('whitespace-only nodes', () {
+    test('non-adjacent to interpolations should be removed', () {
+      expect(
+        _parseAndMinifiy(
+          ''
+              '<div>\n'
+              '  <span>Hello World</span>\n'
+              '</div>\n',
+        ),
+        '<div><span>Hello World</span></div>',
+      );
+    });
+
+    test('adjacent to interpolations should be retained', () {
+      expect(
+        _parseAndMinifiy(
+          ''
+              '<div>\n'
+              '  <span>Hello {{name}}!</span>\n'
+              '</div>\n',
+        ),
+        '<div><span>Hello {{name}}!</span></div>',
+      );
+    });
+  });
+
+  // TODO: Add tests/support for &ngsp; and friends.
+
+  test('should remove inside interpolation on the LHS', () {
+    expect(
+      _parseAndMinifiy('\n    \n    {{value1}}'),
+      '{{value1}}',
+    );
+  });
+
+  test('should remove inside interpolation on the LHS and RHS', () {
+    expect(
+      _parseAndMinifiy('\n    \n    {{value1}}  {{value2}}  \n      '),
+      '{{value1}} {{value2}}',
+    );
+  }, skip: 'Currently failing');
+
+  test('should not remove between text, interpolation', () {
+    expect(
+      _parseAndMinifiy('<span> prefix {{value1}} postfix </span>\n      '),
+      '<span>prefix {{value1}} postfix</span>',
+    );
+  }, skip: 'Currently failing');
+
+  test('should not remove between text, interpolation across lines', () {
+    expect(
+      _parseAndMinifiy('<span>\n prefix {{value1}} postfix \n</span>\n      '),
+      '<span>prefix {{value1}} postfix</span>',
+    );
+  }, skip: 'Currently failing');
+}
+
+String _parseAndMinifiy(String template) {
+  final nodes = parse(template, sourceUrl: 'whitespace_test.dart');
+  final buffer = new StringBuffer();
+  for (final node in _minimizing.visitAllRoot(nodes)) {
+    buffer.write(_humanize(node));
+  }
+  return buffer.toString();
+}
+
+final _minimizing = const MinimizeWhitespaceVisitor();
+final _humanizing = const HumanizingTemplateAstVisitor();
+String _humanize(TemplateAst astNode) => astNode.accept(_humanizing);
