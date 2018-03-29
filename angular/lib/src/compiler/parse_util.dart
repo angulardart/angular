86613da3ac3484ac6f81de8578473c2f4d93f32b
Matan Lurey <matan@lurey.org>
2018-03-29 14:24:27 -0700

    fix(Compiler): ParserError implements BuildError.

--- angular/lib/src/compiler/parse_util.dart
+++ angular/lib/src/compiler/parse_util.dart
@@ -1,14 +1,18 @@
+import 'package:angular_compiler/cli.dart';
 import 'package:source_span/source_span.dart';
 
 enum ParseErrorLevel { WARNING, FATAL }
 
-abstract class ParseError {
-  final SourceSpan span;
-  final String msg;
-  final ParseErrorLevel level;
+abstract class ParseError implements BuildError {
+  final SourceSpan _span;
+  final String _msg;
+  final ParseErrorLevel _level;
 
-  ParseError(this.span, this.msg, [this.level = ParseErrorLevel.FATAL]);
+  ParseError(this._span, this._msg, [this._level = ParseErrorLevel.FATAL]);
 
   @override
-  String toString() => span.message('$level: $msg');
+  String get message => _span.message('$_level: $_msg');
+
+  @override
+  String toString() => message;
 }
