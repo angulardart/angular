ade894522ea70258c0e9a4c03edd958bc306f375
Matan Lurey <matan@lurey.org>
2018-04-09 10:13:57 -0700

    chore(Repo): Add an SDK= option for testing a custom SDK build.
    Closes #1208
    
    PiperOrigin-RevId: 192145118

--- tool/travis.sh
+++ tool/travis.sh
@@ -22,9 +22,18 @@ if [ -z "$TASK" ]; then
   exit 1
 fi
 
+if [ -z "$SDK" ]; then
+  SDK=''
+else
+  echo -e "\033[31mNOTICE: Using custom 'SDK' path of '${SDK}'"
+  echo -e "\033[31mIf this run fails, make sure this is pointing to the 'bin' directory"
+  echo -e "\033[31mFor example, /path/to/sdk/bin"
+  SDK="${SDK}/"
+fi
+
 # Navigate to the correct sub-directory, and run "pub upgrade".
 pushd $PKG
-PUB_ALLOW_PRERELEASE_SDK=quiet pub upgrade
+PUB_ALLOW_PRERELEASE_SDK=quiet ${SDK}pub upgrade
 EXIT_CODE=1
 
 # Run the correct task type.
@@ -32,37 +41,37 @@ case $TASK in
   analyze)
     echo -e '\033[1mTASK: Dart Analyzer [analyzer]\033[22m'
     echo -e 'dartanalyzer --fatal-warnings .'
-    dartanalyzer --fatal-warnings .
+    ${SDK}dartanalyzer --fatal-warnings .
     ;;
 
   build)
     echo -e '\033[1mTASK: Building Only [build]\033[22m'
     echo -e 'pub run build_runner build --fail-on-severe'
-    pub run build_runner build --fail-on-severe
+    ${SDK}pub run build_runner build --fail-on-severe
     ;;
 
   build:release)
     echo -e '\033[1mTASK: Building Only [build:release]\033[22m'
     echo -e 'pub run build_runner build --config=release --fail-on-severe'
-    pub run build_runner build --config=release --fail-on-severe
+    ${SDK}pub run build_runner build --config=release --fail-on-severe
     ;;
 
   test)
     echo -e '\033[1mTASK: Testing [test]\033[22m'
     echo -e 'pub run build_runner test --fail-on-severe -- -P travis'
-    pub run build_runner test --fail-on-severe -- -P travis
+    ${SDK}pub run build_runner test --fail-on-severe -- -P travis
     ;;
 
   test:nobuild)
     echo -e '\033[1mTASK: Testing [test]\033[22m'
     echo -e 'pub run test -P travis'
-    pub run test -P travis
+    ${SDK}pub run test -P travis
     ;;
 
   test:release)
     echo -e '\033[1mTASK: Testing [test:release]\033[22m'
     echo -e 'pub run build_runner test --config=release --fail-on-severe -- -P travis'
-    pub run build_runner test --config=release --fail-on-severe -- -P travis
+    ${SDK}pub run build_runner test --config=release --fail-on-severe -- -P travis
     ;;
 
   *)
