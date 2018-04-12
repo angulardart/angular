3cfa31ec6571ed1f3be1af5d114e1221d8a6220b
matanl <matanl@google.com>
2018-04-12 14:15:23 -0700

    refactor(Benchmarks): Use runApp to greatly de-duplicate bootstrap code.
    PiperOrigin-RevId: 192668217

--- _benchmarks/web/di/tree_bindings_directive.dart
+++ _benchmarks/web/di/tree_bindings_directive.dart
@@ -1,17 +1,7 @@
 import 'package:_benchmarks/common.dart';
 import 'package:_benchmarks/di/create_tree_bindings_directive.template.dart'
-    as ng;
-import 'package:angular/angular.dart';
+    as benchmark;
 
 void main() {
-  bootstrapStatic(
-    BenchmarkComponent,
-    [
-      provide(
-        runBenchmarkOn,
-        useValue: ng.CreateTreeBindingsBenchmarkNgFactory,
-      )
-    ],
-    ng.initReflector,
-  );
+  runBenchmarkApp(benchmark.CreateTreeBindingsBenchmarkNgFactory);
 }
