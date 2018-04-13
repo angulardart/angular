4029eee606464ed271d0fd3ece1532bc59cedd02
Matan Lurey <matan@lurey.org>
2018-04-13 11:33:36 -0700

    fix(Compiler): Make the @Component-failure better.

--- angular_compiler/lib/src/cli/messages/messages.dart
+++ angular_compiler/lib/src/cli/messages/messages.dart
@@ -1,3 +1,6 @@
+import 'package:meta/meta.dart';
+import 'package:source_span/source_span.dart';
+
 import '../messages.dart';
 
 /// Concrete implementation of [Messages] for external users.
@@ -11,7 +14,20 @@ class $Messages extends Messages {
       '  * Check your "import" statements for missing or incorrect URLs\n'
       '\n'
       'If you are still stuck, file an issue and include this error message:\n'
-      '  $urlFileBugs';
+      '$urlFileBugs';
+
+  @override
+  String unresolvedSource(
+    Iterable<SourceSpan> sourceSpans, {
+    String message: 'Was not resolved',
+    @required String reason,
+  }) {
+    final buffer = new StringBuffer(reason)..writeln()..writeln();
+    for (final sourceSpans in sourceSpans) {
+      buffer.writeln(sourceSpans.message(message));
+    }
+    return buffer.toString();
+  }
 
   @override
   final urlFileBugs = 'https://github.com/dart-lang/angular/issues/new';
