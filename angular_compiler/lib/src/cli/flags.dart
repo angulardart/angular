dc868a06f83f5af5c2906613f09a79fb4463f50b
matanl <matanl@google.com>
2018-05-02 16:45:10 -0700

    refactor(Compiler): Remove the (never used) "useFastBoot: false" option.
    This was meant to invoke "initReflector()" if-and-only-if you were using a legacy bootstrap method AND the "@deferred" feature. Because the use of the feature is so minimal, this is an unnecessary flag/abstraction, and it is always possible to use SlowComponentLoader yourself to mimic this functionality.
    
    (When SlowComponentLoader is deleted, this feature will cease to exist anyway)
    
    PiperOrigin-RevId: 195174022

--- angular_compiler/lib/src/cli/flags.dart
+++ angular_compiler/lib/src/cli/flags.dart
@@ -6,7 +6,6 @@ import 'package:meta/meta.dart';
 const _argDebugMode = 'debug';
 const _argProfileFor = 'profile';
 const _argLegacyStyle = 'use_legacy_style_encapsulation';
-const _argFastBoot = 'fast_boot';
 const _argLegacyWhitespace = 'use_legacy_preserve_whitespace';
 
 /// Compiler-wide configuration (flags) to allow opting in/out.
@@ -42,11 +41,6 @@ class CompilerFlags {
           'in order to profile performance or other runtime information.',
     )
     ..addFlag(
-      _argFastBoot,
-      defaultsTo: null,
-      help: 'Whether to support AngularDart v5\'s "fastBoot" functionality.',
-    )
-    ..addFlag(
       _argLegacyWhitespace,
       defaultsTo: null,
       help: 'Whether to opt-in/out of the new preserveWhitespace: false',
@@ -59,13 +53,6 @@ class CompilerFlags {
 
   /// May emit extra code suitable for profiling or tooling.
   final Profile profileFor;
-
-  /// Whether to support AngularDart v5's "fastBoot" functionality.
-  ///
-  /// If `false`, additional code may be generated that negatively affects
-  /// startup performance and code size, but allows compatibility with
-  /// functionality such as `SlowComponentLoader` and `ReflectiveInjector`.
-  final bool useFastBoot;
 
   /// Whether to opt-in to supporting a legacy mode of style encapsulation.
   ///
@@ -94,7 +81,6 @@ class CompilerFlags {
     this.genDebugInfo: false,
     this.ignoreNgPlaceholderForGoldens: false,
     this.profileFor: Profile.none,
-    this.useFastBoot: true,
     this.useLegacyStyleEncapsulation: false,
     this.useNewPreserveWhitespace: false,
   });
@@ -140,7 +126,6 @@ class CompilerFlags {
         _argDebugMode,
         _argProfileFor,
         _argLegacyStyle,
-        _argFastBoot,
         _argLegacyWhitespace,
         // TODO(matanl): A better strategy for negatable options.
         'no-$_argLegacyWhitespace',
@@ -171,11 +156,6 @@ class CompilerFlags {
       log('Invalid value for "$_argLegacyStyle": $useLegacyStyle');
       useLegacyStyle = null;
     }
-    var useFastBoot = options[_argFastBoot];
-    if (useFastBoot != null && useFastBoot is! bool) {
-      log('Invalid value for "$_argFastBoot": $useFastBoot');
-      useFastBoot = null;
-    }
     var useLegacyWhitespace = options[_argLegacyWhitespace];
     if (useLegacyWhitespace != null && useLegacyWhitespace is! bool) {
       log('Invalid value for "$_argLegacyWhitespace": $useLegacyWhitespace');
@@ -186,7 +166,6 @@ class CompilerFlags {
       profileFor: _toProfile(profileFor, log) ?? defaultTo.profileFor,
       useLegacyStyleEncapsulation:
           useLegacyStyle ?? defaultTo.useLegacyStyleEncapsulation,
-      useFastBoot: useFastBoot ?? defaultTo.useFastBoot,
       useNewPreserveWhitespace: useLegacyWhitespace != null
           ? !useLegacyWhitespace
           : defaultTo.useNewPreserveWhitespace,
