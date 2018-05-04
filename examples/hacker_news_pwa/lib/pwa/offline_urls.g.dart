d2d08fa2149a06472d13041cb66846d07fc7d4ed
Kevin Moore <kevmoo@google.com>
2018-05-04 14:36:43 -0700

    no need to precache icons

--- examples/hacker_news_pwa/lib/pwa/offline_urls.g.dart
+++ examples/hacker_news_pwa/lib/pwa/offline_urls.g.dart
@@ -1,16 +1,6 @@
 /// URLs for offline cache.
 final List<String> offlineUrls = [
   './',
-  './images/icons/android-icon-144x144.png',
-  './images/icons/android-icon-192x192.png',
-  './images/icons/android-icon-36x36.png',
-  './images/icons/android-icon-48x48.png',
-  './images/icons/android-icon-512x512.png',
-  './images/icons/android-icon-72x72.png',
-  './images/icons/android-icon-96x96.png',
-  './images/icons/favicon-16x16.png',
-  './images/icons/favicon-32x32.png',
-  './images/icons/favicon-96x96.png',
   './images/logo.svg',
   './main.dart.js',
   './main.dart.js_1.part.js',
