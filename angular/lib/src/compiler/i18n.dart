0bbec29b2c6ff579543735db556a81a2289aa99b
leonsenft <leonsenft@google.com>
2018-05-14 14:03:29 -0700

    feature(Core): validates ASTs in an `@i18n` context can be internationalized
    * Reports an error when an `@i18n`-annotated node doesn't have a single, text
      node child.
    
    PiperOrigin-RevId: 196563314

--- angular/lib/src/compiler/i18n.dart
+++ angular/lib/src/compiler/i18n.dart
@@ -3,36 +3,41 @@ import 'package:angular_ast/angular_ast.dart' as ast;
 import 'i18n/message.dart';
 import 'i18n/metadata.dart';
 import 'template_ast.dart' as ng;
+import 'template_parser.dart' show TemplateContext;
 
 export 'i18n/message.dart';
 export 'i18n/metadata.dart';
 
-// TODO(leonsenft): improve handling of error cases.
-/// Internationalizes the child [nodes] of a [metadata]-annotated node.
+/// Internationalizes the children of a [metadata]-annotated [parent] node.
 ///
-/// The [nodes] are converted to an internationalization-aware AST that
-/// handles rendering the translation for the current locale.
+/// The children are converted to an internationalization-aware AST that handles
+/// rendering the translation for the current locale.
 ///
-/// The provided [ngContentIndex] should match text within the parent element's
+/// The provided [ngContentIndex] should match text within the [parent]'s
 /// context.
 List<ng.TemplateAst> internationalize(
-  List<ast.StandaloneTemplateAst> nodes,
+  ast.StandaloneTemplateAst parent,
   I18nMetadata metadata,
   int ngContentIndex,
+  TemplateContext context,
 ) {
-  if (nodes.isEmpty) {
-    throw new UnsupportedError('Too few nodes');
-  }
-  if (nodes.length > 1) {
-    throw new UnsupportedError('Too many nodes');
+  if (parent.childNodes.length != 1) {
+    context.reportError(
+      'Expected a single, non-empty text node child in an "@i18n" context',
+      parent.sourceSpan,
+    );
+    return [];
   }
   final result = <ng.TemplateAst>[];
-  final node = nodes.first;
+  final node = parent.childNodes.first;
   if (node is ast.TextAst) {
     final message = new I18nMessage(node.value, metadata);
     result.add(new ng.I18nTextAst(message, ngContentIndex, node.sourceSpan));
   } else {
-    throw new UnsupportedError('Expected a text node');
+    context.reportError(
+      'Only text is supported in an "@i18n" context',
+      node.sourceSpan,
+    );
   }
   return result;
 }
