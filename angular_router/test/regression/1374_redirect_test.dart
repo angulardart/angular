6bfc13f1928f3b39c54242353cbd0dd5385579ab
leonsenft <leonsenft@google.com>
2018-06-12 09:53:21 -0700

    fix(Router): preserve `NavigationParams` on redirection
    Fixes https://github.com/dart-lang/angular/issues/1374.
    
    PiperOrigin-RevId: 200231946

---
+++ angular_router/test/regression/1374_redirect_test.dart
@@ -0,0 +1,108 @@
+@TestOn('browser')
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:angular_router/testing.dart';
+import 'package:angular_test/angular_test.dart';
+import 'package:test/test.dart';
+
+import '1374_redirect_test.template.dart' as ng;
+
+void main() {
+  tearDown(disposeAnyRunningTest);
+
+  test('redirect should push new state', () async {
+    final urlChanges = await redirect();
+    expect(urlChanges, ['/to']);
+  });
+
+  test('redirect should replace state', () async {
+    final params = new NavigationParams(replace: true);
+    final urlChanges = await redirect(params);
+    expect(urlChanges, ['replace: /to']);
+  });
+
+  test("redirect shouldn't update URL", () async {
+    final params = new NavigationParams(updateUrl: false);
+    final urlChanges = await redirect(params);
+    expect(urlChanges, isEmpty);
+  });
+
+  test('redirect on outlet registration should replace URL', () async {
+    final testBed = NgTestBed
+        .forComponent<TestInitialRedirectComponent>(
+            ng.TestInitialRedirectComponentNgFactory)
+        .addInjector(injector);
+    final testFixture = await testBed.create();
+    final locationStrategy = testFixture.assertOnlyInstance.locationStrategy;
+    expect(locationStrategy.urlChanges, ['replace: /to']);
+  });
+}
+
+/// Performs a navigation that should be redirected with [params].
+///
+/// Returns any URL changes that occurred due to navigation.
+Future<List<String>> redirect([NavigationParams params]) async {
+  final testBed = NgTestBed
+      .forComponent<TestRedirectComponent>(ng.TestRedirectComponentNgFactory)
+      .addInjector(injector);
+  final testFixture = await testBed.create();
+  final urlChanges = testFixture.assertOnlyInstance.locationStrategy.urlChanges
+    // TODO(leonsenft): due to https://github.com/dart-lang/angular/issues/1389,
+    // the initial navigation to '/' succeeds despite there being no matching
+    // route. URL changes due to this navigation are cleared as they're of no
+    // interest to the tests calling this function. To be removed when the issue
+    // is fixed.
+    ..clear();
+  final router = testFixture.assertOnlyInstance.router;
+  final result = await router.navigate('/from', params);
+  expect(result, NavigationResult.SUCCESS);
+  return urlChanges;
+}
+
+@GenerateInjector(routerProvidersTest)
+InjectorFactory injector = ng.injector$Injector;
+
+@Component(selector: 'to', template: '')
+class ToComponent {}
+
+@Component(
+  selector: 'test',
+  template: '<router-outlet [routes]="routes"></router-outlet>',
+  directives: const [RouterOutlet],
+)
+class TestRedirectComponent {
+  static final routes = [
+    new RouteDefinition(path: '/to', component: ng.ToComponentNgFactory),
+    new RouteDefinition.redirect(path: '/from', redirectTo: '/to'),
+  ];
+
+  final MockLocationStrategy locationStrategy;
+  final Router router;
+
+  TestRedirectComponent(
+    @Inject(LocationStrategy) this.locationStrategy,
+    this.router,
+  );
+}
+
+@Component(
+  selector: 'test',
+  template: '<router-outlet [routes]="routes"></router-outlet>',
+  directives: const [RouterOutlet],
+)
+class TestInitialRedirectComponent {
+  static final routes = [
+    new RouteDefinition(path: '/to', component: ng.ToComponentNgFactory),
+    new RouteDefinition.redirect(path: '/.*', redirectTo: '/to'),
+  ];
+
+  final MockLocationStrategy locationStrategy;
+  final Router router;
+
+  TestInitialRedirectComponent(
+    @Inject(LocationStrategy) this.locationStrategy,
+    this.router,
+  );
+}
