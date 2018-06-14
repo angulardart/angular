cd29c9dc34fc2dfe033b55ca564b1439469e7d60
matanl <matanl@google.com>
2018-06-14 09:41:24 -0700

    chore(Test): Remove all *.template_debug.golden files.
    Will rename *.template_release back to *.template (no _release) in later CL.
    
    PiperOrigin-RevId: 200569680

--- _goldens/tool/src/builder.dart
+++ _goldens/tool/src/builder.dart
@@ -7,25 +7,6 @@ BuilderOptions _withoutExtensions(BuilderOptions options) =>
       ..addAll(options.config)
       ..remove('extensions'));
 
-/// Returns a [Builder] to generate (debug) template files for comparison.
-///
-/// Based on `build.yaml` configuration, this will either emit:
-/// * `.golden` files: Checked into the repository.
-/// * `.check` files: Temporarily generated, at HEAD, to compare against.
-///
-/// See `README.md` for this package for details.
-Builder debugBuilder(BuilderOptions options) {
-  return angular.templateCompiler(
-    _withoutExtensions(options),
-    defaultFlags: const CompilerFlags(
-      genDebugInfo: true,
-      i18nEnabled: true,
-      ignoreNgPlaceholderForGoldens: true,
-    ),
-    templateExtension: options.config['extensions']['debug'],
-  );
-}
-
 /// Returns a [Builder] to generate (release) template files for comparison.
 ///
 /// Based on `build.yaml` configuration, this will either emit:
