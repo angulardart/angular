96d420959c10198381d38af9143911e1fe67dbb6
leonsenft <leonsenft@google.com>
2018-06-29 15:25:56 -0700

    cleanup(Compiler): remove unused code
    PiperOrigin-RevId: 202717238

--- angular/lib/src/source_gen/common/annotation_matcher.dart
+++ angular/lib/src/source_gen/common/annotation_matcher.dart
@@ -40,11 +40,6 @@ bool isComponent(ElementAnnotation annotation) =>
 bool isDirective(ElementAnnotation annotation) =>
     _matchTypes([Component, Directive], annotation);
 
-/// Checks if an [ElementAnnotation] node is an annotation with directive
-/// fields.
-bool hasDirectives(ElementAnnotation annotation) =>
-    _matchTypes([Component, Directive], annotation);
-
 /// Checks if an [ElementAnnotation] node implements [Pipe].
 bool isPipe(ElementAnnotation annotation) => matchAnnotation(Pipe, annotation);
 
