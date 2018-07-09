8227f21568f87722825e7c85b7a140a10869dd52
Matan Lurey <matan@lurey.org>
2018-07-09 13:17:13 -0700

    fix(Perf): Fix double-invoke of 'mapNestedViews'.
    It was possible to fix this _without_ adding `firstOrNull` as  helper function, but would complicate the compiler (needing to create a statement instead of an expression).
    
    Closes #1456
    
    PiperOrigin-RevId: 203817618

--- angular/lib/src/core/linker/app_view_utils.dart
+++ angular/lib/src/core/linker/app_view_utils.dart
@@ -95,6 +95,9 @@ List<T> flattenNodes<T>(List<List<T>> nodes) {
   return result;
 }
 
+/// Returns the first item of [items], or `null` if the list is empty.
+T firstOrNull<T>(List<T> items) => items.isNotEmpty ? items.first : null;
+
 dynamic interpolate0(dynamic p) {
   if (p is String) return p;
   if (p is SafeValue) return p;
