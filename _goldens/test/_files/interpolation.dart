ef90eb852a6e26d4108277df0860a26a42965b07
Matan Lurey <matan@lurey.org>
2018-07-09 13:11:09 -0700

    chore(Test): Add more goldens for interpolation.
    Closes #1463
    
    PiperOrigin-RevId: 203816740

--- _goldens/test/_files/interpolation.dart
+++ _goldens/test/_files/interpolation.dart
@@ -2,24 +2,74 @@ import 'package:angular/angular.dart';
 
 @Component(
   selector: 'interpolation',
-  template: '''
-<div>
-  {{foo}}
-</div>
-''',
+  preserveWhitespace: true,
+  template: r'''
+    <div>
+      {{foo}}
+    </div>
+  ''',
 )
 class InterpolationComponent {
-  String foo = 'hello';
+  var foo = 'hello';
 }
 
 @Component(
   selector: 'interpolation',
-  template: '''
-<div>
-  {{foo}}
-</div>
-''',
+  template: r'''
+    <div>
+      {{foo}}
+    </div>
+  ''',
 )
 class InterpolationComponentNoWhitespace {
-  String foo = 'hello';
+  var foo = 'hello';
+}
+
+@Component(selector: 'interpolation', template: r'''
+    <div>
+      {{greeting}} {{noun}}!
+    </div>
+  ''')
+class InterpolationTwoAdjacent {
+  var greeting = 'Hello';
+  var noun = 'World';
+}
+
+@Component(
+  selector: 'interpolation',
+  template: r'''
+    <div>
+      Welcome {{name}} to the {{business}} of {{location}}!
+    </div>
+  ''',
+)
+class InterpolationLongerSentence {
+  var name = 'Joe';
+  var business = 'Bank';
+  var location = 'tomorrow';
+}
+
+@Component(
+  selector: 'interpolation',
+  template: r'''
+    <div>{{"Welcome"}} {{"Home"}}!</div>
+  ''',
+)
+class InterpolationLiterals {}
+
+@Component(
+  selector: 'interpolation',
+  directives: [
+    NgFor,
+  ],
+  template: r'''
+    <ng-container *ngFor="let bar of bars">
+      <ng-container *ngFor="let foo of bar">
+        Hello {{callMethod(bar)}}: {{foo}}!
+      </ng-container>
+    </ng-container>
+  ''',
+)
+class InterpolationLocals {
+  String callMethod(Object input) => input.toString();
 }
