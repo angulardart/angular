64317c9c42a11315f280a5f84fa8af6fb582f04c
matanl <matanl@google.com>
2018-07-16 20:05:21 -0700

    test(DI): Add a test case for using `ReflectiveInjector.resolveStatic...``
    ... based on internal requirements around `resolveAndInstantiate`.
    
    PiperOrigin-RevId: 204849963

--- _tests/test/regression/1358_rebind_injector_test.dart
+++ _tests/test/regression/1358_rebind_injector_test.dart
@@ -5,29 +5,64 @@ import 'package:test/test.dart';
 import '1358_rebind_injector_test.template.dart' as ng;
 
 void main() {
-  ng.initReflector();
-
   ReflectiveInjector parentInjector;
 
-  setUp(() {
-    parentInjector = ReflectiveInjector.resolveAndCreate([
-      new ClassProvider(Model),
-      new ValueProvider(Place, new Place('Parent')),
-    ]);
+  group('Dynamic (uses initReflector)', () {
+    setUpAll(() {
+      ng.initReflector();
+    });
+
+    setUp(() {
+      parentInjector = ReflectiveInjector.resolveAndCreate([
+        new ClassProvider(Model),
+        new ValueProvider(Place, new Place('Parent')),
+      ]);
+    });
+
+    test('should have the expected bindings at the parent level', () {
+      expect((parentInjector.get(Model) as Model).place.name, 'Parent');
+    });
+
+    test('should have the expected bindings at the child level', () {
+      final childInjector = parentInjector.resolveAndCreateChild([
+        new ValueProvider(Place, new Place('Child')),
+      ]);
+      expect(
+        (childInjector.resolveAndInstantiate(Model) as Model).place.name,
+        'Child',
+      );
+    });
   });
 
-  test('should have the expected bindings at the parent level', () {
-    expect((parentInjector.get(Model) as Model).place.name, 'Parent');
-  });
-
-  test('should have the expected bindings at the child level', () {
-    final childInjector = parentInjector.resolveAndCreateChild([
-      new ValueProvider(Place, new Place('Child')),
-    ]);
-    expect(
-      (childInjector.resolveAndInstantiate(Model) as Model).place.name,
-      'Child',
+  group('Static (no initReflector)', () {
+    final modelProvider = new FactoryProvider(
+      Model,
+      (Place place) => new Model(place),
+      deps: const [Place],
     );
+
+    setUp(() {
+      parentInjector = ReflectiveInjector.resolveStaticAndCreate([
+        modelProvider,
+        new ValueProvider(Place, new Place('Parent')),
+      ]);
+    });
+
+    test('should have the expected bindings at the parent level', () {
+      expect((parentInjector.get(Model) as Model).place.name, 'Parent');
+    });
+
+    test('should have the expected bindings at the child level', () {
+      final childInjector = parentInjector.resolveAndCreateChild([
+        new ValueProvider(Place, new Place('Child')),
+      ]);
+      expect(
+        (childInjector.resolveAndInstantiate(modelProvider) as Model)
+            .place
+            .name,
+        'Child',
+      );
+    });
   });
 }
 
