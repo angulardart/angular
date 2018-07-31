54a996f91178008ed709776647a44bdce88b6105
leonsenft <leonsenft@google.com>
2018-07-31 10:39:37 -0700

    feat(Generics): parse the `on` argument from `Typed` metadata
    PiperOrigin-RevId: 206786749

--- angular_compiler/lib/src/analyzer/view/typed_reader.dart
+++ angular_compiler/lib/src/analyzer/view/typed_reader.dart
@@ -8,6 +8,28 @@ import '../common.dart';
 import '../link.dart';
 import '../types.dart';
 
+/// A statically parsed `Typed` used to specify type arguments on a directive.
+class TypedElement {
+  /// An optional identifier used to select specific instances of the directive.
+  final String on;
+
+  /// The fully typed directive's type.
+  final TypeLink typeLink;
+
+  TypedElement(this.typeLink, {this.on});
+
+  @override
+  int get hashCode => on.hashCode ^ typeLink.hashCode;
+
+  @override
+  bool operator ==(Object o) {
+    if (o is TypedElement) {
+      return on == o.on && typeLink == o.typeLink;
+    }
+    return false;
+  }
+}
+
 // TODO(leonsenft): ensure failure messages have an actionable context.
 /// Parses types from compile-time constant `Typed` expressions.
 class TypedReader {
@@ -23,20 +45,20 @@ class TypedReader {
   /// Parses the value of a compile-time constant `Typed` expression.
   ///
   /// Returns a [TypeLink] that represents the type defined by [typedObject].
-  TypeLink parse(DartObject typedObject) {
+  TypedElement parse(DartObject typedObject) {
     if (!$Typed.isExactlyType(typedObject.type)) {
       throwFailure(''
           'Expected an expression of type "Typed", but got '
           '"${typedObject.type}"');
     }
-    return _parseTyped(typedObject);
+    return _parseTyped(typedObject, root: true);
   }
 
   /// Parses the value of a compile-time constant [object].
   TypeLink _parse(DartObject object) {
     final constant = ConstantReader(object);
     if (constant.instanceOf($Typed)) {
-      return _parseTyped(object);
+      return _parseTyped(object).typeLink;
     } else if (constant.isSymbol) {
       return _parseSymbol(object);
     } else if (constant.isType) {
@@ -69,17 +91,38 @@ class TypedReader {
       linkTypeOf(typeObject.toTypeValue());
 
   /// Parses a [Typed], e.g. `Typed<List<String>>()`.
-  TypeLink _parseTyped(DartObject typedObject) {
+  ///
+  /// [root] is true when [typedObject] represents the root `Typed` instance
+  /// of a `Typed` expression.
+  TypedElement _parseTyped(DartObject typedObject, {bool root = false}) {
     final type = typeArgumentOf(typedObject);
     if (type is ParameterizedType && type.typeParameters.isNotEmpty) {
+      String on;
+      final reader = ConstantReader(typedObject);
+      final onReader = reader.read('on');
+      if (onReader.isString) {
+        if (!root) {
+          throwFailure(''
+              'The "on" argument is only supported on the root "Typed" of a '
+              '"Typed" expression');
+        }
+        on = onReader.stringValue;
+      }
       // If the named constructor `Typed.of()` was used to specify type
       // arguments, we parse them from the `typeArguments` field. Otherwise, we
       // use the type arguments of the type itself.
-      final constant = ConstantReader(typedObject).read('typeArguments');
-      final typeArguments = constant.isList
-          ? constant.listValue.map(_parse).toList()
+      final typeArgumentsReader = reader.read('typeArguments');
+      final typeArguments = typeArgumentsReader.isList
+          ? typeArgumentsReader.listValue.map(_parse).toList()
           : type.typeArguments.map(linkTypeOf).toList();
-      return TypeLink(getTypeName(type), getTypeImport(type), typeArguments);
+      return TypedElement(
+        TypeLink(
+          getTypeName(type),
+          getTypeImport(type),
+          typeArguments,
+        ),
+        on: on,
+      );
     } else {
       throwFailure(''
           'Expected a generic type to be used as a type argument of "Typed", '
