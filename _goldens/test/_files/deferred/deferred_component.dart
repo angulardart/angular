b98f178c36ceccc4ce4826ed090b46b2490caabb
matanl <matanl@google.com>
2018-08-09 12:55:25 -0700

    chore(Test): Add more golden file testing around `@deferred`.
    Trying to get more context around the following bugs:
    * https://github.com/dart-lang/angular/issues/1539
    * https://github.com/dart-lang/angular/issues/1540
    
    PiperOrigin-RevId: 208096491

--- _goldens/test/_files/deferred/deferred_component.dart
+++ _goldens/test/_files/deferred/deferred_component.dart
@@ -1,7 +1,31 @@
 import 'package:angular/angular.dart';
 
 @Component(
-  selector: 'deferred-component',
+  selector: 'deferred-child-1',
+  template: 'Deferred Child 1',
+)
+class DeferredChild1Component {}
+
+@Component(
+  selector: 'deferred-child-2',
+  template: 'Deferred Child 2',
+)
+class DeferredChild2Component {}
+
+@Component(
+  selector: 'deferred-child-3',
+  template: 'Deferred Child 3',
+)
+class DeferredChild3Component {}
+
+@Component(
+  selector: 'deferred-child-without-ng-content',
   template: '<div>Child</div>',
 )
-class DeferredChildComponent {}
+class DeferredChildComponentWithoutNgContent {}
+
+@Component(
+  selector: 'deferred-child-with-ng-content',
+  template: '<div><ng-content></ng-content></div>',
+)
+class DeferredChildComponentWithNgContent {}
