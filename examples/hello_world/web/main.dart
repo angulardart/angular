5080465588cb38fa3bd4ebdf19a15a362a608056
Matan Lurey <matan@lurey.org>
2018-08-10 13:35:55 -0700

    WIP: Regression #1539.

--- examples/hello_world/web/main.dart
+++ examples/hello_world/web/main.dart
@@ -6,6 +6,34 @@ void main() => runApp(ng.HelloWorldComponentNgFactory);
 
 @Component(
   selector: 'hello-world',
-  template: 'Hello World',
+  directives: [
+    NgIf,
+    BugComponent,
+  ],
+  template: r'''
+    <button (click)="showOuter = !showOuter">Toggle</button>
+    <template [ngIf]="showOuter">
+      <i-am-buggy @deferred></i-am-buggy>
+    </template>
+    
+    <div>
+      <button (click)="showOuter2 = !showOuter2">Toggle</button>
+      <template [ngIf]="showOuter2">
+        <template [ngIf]="showInner">
+          <i-am-buggy></i-am-buggy>
+        </template>
+      </template>
+    </div>
+  ''',
 )
-class HelloWorldComponent {}
+class HelloWorldComponent {
+  bool showOuter = false;
+  bool showOuter2 = false;
+  bool showInner = true;
+}
+
+@Component(
+  selector: 'i-am-buggy',
+  template: 'BUG BUG BUG',
+)
+class BugComponent {}
