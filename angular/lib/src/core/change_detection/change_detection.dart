fe2e8403140ed3df9308db0630cd957837ba6c07
alorenzen <alorenzen@google.com>
2018-09-06 15:06:37 -0700

    cleanup(change_detection): Fix exports to use ' instead of ".
    This makes the file consistent with the rest of our codebase.
    
    PiperOrigin-RevId: 211876530

--- angular/lib/src/core/change_detection/change_detection.dart
+++ angular/lib/src/core/change_detection/change_detection.dart
@@ -1,10 +1,10 @@
-export "change_detection_util.dart" show SimpleChange, devModeEqual;
-export "change_detector_ref.dart" show ChangeDetectorRef;
-export "constants.dart"
+export 'change_detection_util.dart' show SimpleChange, devModeEqual;
+export 'change_detector_ref.dart' show ChangeDetectorRef;
+export 'constants.dart'
     show
         ChangeDetectionStrategy,
         ChangeDetectorState,
         isDefaultChangeDetectionStrategy;
-export "differs/default_iterable_differ.dart" show CollectionChangeRecord;
-export "differs/default_keyvalue_differ.dart" show KeyValueChangeRecord;
-export "pipe_transform.dart" show PipeTransform;
+export 'differs/default_iterable_differ.dart' show CollectionChangeRecord;
+export 'differs/default_keyvalue_differ.dart' show KeyValueChangeRecord;
+export 'pipe_transform.dart' show PipeTransform;
