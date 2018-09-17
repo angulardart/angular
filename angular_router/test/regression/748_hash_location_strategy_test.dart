59608e6aa7280cd7c52912bcfe98236060d8a1fd
leonsenft <leonsenft@google.com>
2018-09-17 16:33:43 -0700

    test(Router): test discrepancy between router link href and browser location
    This is a currently failing test case for https://github.com/dart-lang/angular/issues/748.
    
    PiperOrigin-RevId: 213360543

---
+++ angular_router/test/regression/748_hash_location_strategy_test.dart
@@ -0,0 +1,63 @@
+@TestOn('browser')
+import 'dart:html';
+
+import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:angular_test/angular_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import '748_hash_location_strategy_test.template.dart' as ng;
+
+final platformLocation = MockPlatformLocation();
+
+void main() {
+  setUp(() {
+    reset(platformLocation);
+  });
+
+  tearDown(disposeAnyRunningTest);
+
+  test('browser location should match clicked href', () async {
+    final testBed = NgTestBed.forComponent(ng.AppComponentNgFactory,
+        rootInjector: injectorFactory);
+    final testFixture = await testBed.create();
+    expect(testFixture.assertOnlyInstance.anchor.getAttribute('href'), '#/foo');
+    await testFixture.update((c) {
+      c.anchor.click();
+    });
+    verify(platformLocation.pushState(any, any, '#/foo')).called(1);
+  }, skip: 'https://github.com/dart-lang/angular/issues/748');
+}
+
+PlatformLocation platformLocationFactory() => platformLocation;
+
+class MockPlatformLocation extends Mock implements BrowserPlatformLocation {}
+
+@GenerateInjector([
+  routerProvidersHash,
+  const FactoryProvider(PlatformLocation, platformLocationFactory),
+])
+InjectorFactory injectorFactory = ng.injectorFactory$Injector;
+
+@Component(
+  selector: 'app',
+  template: '''
+    <a #routerLink [routerLink]="fooRoute.toUrl()"></a>
+    <router-outlet [routes]="routes"></router-outlet>
+  ''',
+  directives: [RouterLink, RouterOutlet],
+)
+class AppComponent {
+  static final fooRoute = RouteDefinition(
+    path: '/foo',
+    component: ng.FooComponentNgFactory,
+  );
+  static final routes = [fooRoute];
+
+  @ViewChild('routerLink')
+  HtmlElement anchor;
+}
+
+@Component(selector: 'foo', template: '')
+class FooComponent {}
