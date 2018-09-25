fa492e218f7e379e700772248ab276b6cd3cbb85
leonsenft <leonsenft@google.com>
2018-09-25 14:56:12 -0700

    test(Compiler): add regression test for generic component with dynamic type arg
    Tests https://github.com/dart-lang/angular/issues/1622.
    
    PiperOrigin-RevId: 214509396

---
+++ _tests/test/regression/1622_dynamic_generic_type_argument.dart
@@ -0,0 +1,25 @@
+@TestOn('vm')
+import 'package:_tests/compiler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('should support generic component with dynamic type argument', () {
+    return compilesNormally('''
+      import '$ngImport';
+
+      @Component(
+        selector: 'generic',
+        template: '',
+      )
+      class GenericComponent<T> {}
+
+      @Component(
+        selector: 'test',
+        template: '<generic></generic>',
+        directives: [GenericComponent],
+        directiveTypes: [Typed<GenericComponent<dynamic>>()],
+      )
+      class TestComponent {}
+    ''');
+  }, skip: 'https://github.com/dart-lang/angular/issues/1622');
+}
