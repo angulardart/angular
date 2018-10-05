9c2340c5100a9a77728e3c3f74e20b82a5f04a74
leonsenft <leonsenft@google.com>
2018-10-05 13:25:52 -0700

    Update to version 0.14.6
    PiperOrigin-RevId: 215956399

--- _tests/test/compiler/shadow_css_test.dart
+++ _tests/test/compiler/shadow_css_test.dart
@@ -86,53 +86,57 @@ void main() {
 
   test('should add a class to every rule', () {
     var css = 'one {color: red;} two {color: red;}';
-    var expected = 'one.$content {color:red;} two.$content {color:red;}';
+    var expected = 'one.$content {color:red} two.$content {color:red}';
     shimAndExpect(css, expected);
     shimAndExpect(css, expected);
   });
 
   test('should add a class to every selector', () {
     var css = 'one,two {color: red;}';
-    var expected = 'one.$content,two.$content {color:red;}';
+    var expected = 'one.$content,two.$content {color:red}';
     shimAndExpect(css, expected);
   });
 
   test('should support newlines in the selector and content ', () {
     var css = 'one,\ntwo {\ncolor: red;}';
-    var expected = 'one.$content,two.$content {color:red;}';
+    var expected = 'one.$content,two.$content {color:red}';
     shimAndExpect(css, expected);
   });
 
   test('should handle media rules', () {
     var css = '@media screen and (max-width:800px) {div {font-size:50px;}}';
     var expected =
-        '@media screen AND (max-width:800px) {div.$content {font-size:50px;}}';
+        '@media screen AND (max-width:800px) {div.$content {font-size:50px}}';
     shimAndExpect(css, expected);
   });
 
   test('should handle page rules', () {
     var css = '@page {@top-left {color:red;} font-size:50px;}';
-    shimAndExpect(css, css);
+    var expected = '@page {@top-left {color:red} font-size:50px}';
+    shimAndExpect(css, expected);
   });
 
   test('should handle media rules with simple rules', () {
     var css = '@media screen and (max-width: 800px) '
         '{div {font-size: 50px;}} div {}';
     var expected = '@media screen AND (max-width:800px) '
-        '{div.$content {font-size:50px;}} div.$content {}';
+        '{div.$content {font-size:50px}} div.$content {}';
     shimAndExpect(css, expected);
   });
 
   // Check that the browser supports unprefixed CSS animation
   test('should handle keyframes rules', () {
     var css = '@keyframes foo {0% {transform:translate(-50%) scaleX(0);}}';
-    shimAndExpect(css, css);
+    var expected = '@keyframes foo {0% {transform:translate(-50%) scaleX(0)}}';
+    shimAndExpect(css, expected);
   });
 
   test('should handle -webkit-keyframes rules', () {
     var css = '@-webkit-keyframes foo '
         '{0% {-webkit-transform:translate(-50%) scaleX(0);}}';
-    shimAndExpect(css, css);
+    var expected = '@-webkit-keyframes foo '
+        '{0% {-webkit-transform:translate(-50%) scaleX(0)}}';
+    shimAndExpect(css, expected);
   });
 
   test('should handle complicated selectors', () {
@@ -312,9 +316,9 @@ void main() {
 
     test('should support polyfill-unscoped-rule', () {
       var css = "polyfill-unscoped-rule {content: '#menu > .bar';color: blue;}";
-      legacyShimAndExpect(css, '#menu > .bar {color:blue;}');
+      legacyShimAndExpect(css, '#menu > .bar {color:blue}');
       css = 'polyfill-unscoped-rule {content: "#menu > .bar";color: blue;}';
-      legacyShimAndExpect(css, '#menu > .bar {color:blue;}');
+      legacyShimAndExpect(css, '#menu > .bar {color:blue}');
       css = 'polyfill-unscoped-rule {content: \'button[priority="1"]\';}';
       legacyShimAndExpect(css, 'button[priority="1"] {}');
     });
@@ -322,7 +326,7 @@ void main() {
     test('should support multiple instances polyfill-unscoped-rule', () {
       var css = 'polyfill-unscoped-rule {content: "foo";color: blue;}' +
           'polyfill-unscoped-rule {content: "bar";color: red;}';
-      legacyShimAndExpect(css, 'foo {color:blue;} bar {color:red;}');
+      legacyShimAndExpect(css, 'foo {color:blue} bar {color:red}');
     });
 
     test('should handle ::content', () {
@@ -388,7 +392,7 @@ void main() {
 
   test('should leave calc() unchanged', () {
     var css = 'div {height:calc(100% - 55px);}';
-    shimAndExpect(css, 'div.$content {height:calc(100% - 55px);}');
+    shimAndExpect(css, 'div.$content {height:calc(100% - 55px)}');
   });
 
   test('should strip comments', () {
