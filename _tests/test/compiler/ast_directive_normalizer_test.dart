05de2ba77e5d958eedb8cd0a809929b11b7b4eca
alorenzen <alorenzen@google.com>
2018-10-11 13:41:19 -0700

    feat(Compiler): Remove support for "inline" `<style>` in HTML templates.
    **BREAKING**.
    
    Instead, we will *only* support styles defined in `@Component()`.
    
    This better separates the template compiler and the stylesheet compiler.
    
    PiperOrigin-RevId: 216752964

--- _tests/test/compiler/ast_directive_normalizer_test.dart
+++ _tests/test/compiler/ast_directive_normalizer_test.dart
@@ -140,10 +140,8 @@ void main() {
     expect(
       metadata.template.styleUrls,
       orderedEquals([
-        'package:a/3.css',
         'package:a/1.css',
         'package:a/2.css',
-        'package:a/4.css',
       ]),
     );
   });
@@ -196,9 +194,8 @@ void main() {
       metadata.template.styles,
       [
         contains(':host { margin: 10px; }'),
-        contains(':host { padding: 10px; }'),
       ],
-      reason: 'Only a two inline styles should have been processed',
+      reason: 'Only one inline style should have been processed',
     );
   });
 }
