49914f91af7f4a0bb092d87c5c0a0d178bd5686d
leonsenft <leonsenft@google.com>
2018-10-30 14:39:32 -0700

    chore(Goldens): fix typo
    PiperOrigin-RevId: 219368615

--- _goldens/test/_files/dart2js/dart2js_golden.dart
+++ _goldens/test/_files/dart2js/dart2js_golden.dart
@@ -18,7 +18,7 @@ external T defeatDart2JsOptimizations<T>(Object any);
 /// be checked in to `test/dart2js_golden.dart.js`, and must be regenerated
 /// whenever the output would change.
 ///
-/// See .`./README.md` for details on updating the golden file.
+/// See `../README.md` for details on updating the golden file.
 ///
 /// **NOTE**: The test is not executed externally.
 void main() {
