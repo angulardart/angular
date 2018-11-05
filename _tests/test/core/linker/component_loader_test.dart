2e43aa3ca949cf5fd15d560294c3a6fb96537865
leonsenft <leonsenft@google.com>
2018-11-05 13:22:27 -0800

    test(Core): refactor test to remove need for `initReflector()`
    PiperOrigin-RevId: 220160758

--- _tests/test/core/linker/component_loader_test.dart
+++ _tests/test/core/linker/component_loader_test.dart
@@ -1,5 +1,4 @@
 @TestOn('browser')
-
 import 'package:test/test.dart';
 import 'package:angular/angular.dart';
 import 'package:angular_test/angular_test.dart';
@@ -8,34 +7,33 @@ import 'package:angular_test/angular_test.dart';
 import 'component_loader_test.template.dart' as ng;
 
 void main() {
-  ng.initReflector();
+  tearDown(disposeAnyRunningTest);
 
-  group('ComponentLoader', () {
-    tearDown(() => disposeAnyRunningTest());
-
-    test('should be able to load next to a location', () async {
-      final fixture = await NgTestBed<CompWithCustomLocation>().create();
-      expect(fixture.text, 'BeforeAfter');
-      await fixture.update((comp) {
-        comp.loader.loadNextToLocation(
-          ng.DynamicCompNgFactory,
-          comp.location,
-        );
-      });
-      expect(fixture.text, 'BeforeDynamicAfter');
+  test('should be able to load next to a location', () async {
+    final testBed = NgTestBed.forComponent(ng.CompWithCustomLocationNgFactory);
+    final fixture = await testBed.create();
+    expect(fixture.text, 'BeforeAfter');
+    await fixture.update((comp) {
+      comp.loader.loadNextToLocation(
+        ng.DynamicCompNgFactory,
+        comp.location,
+      );
     });
+    expect(fixture.text, 'BeforeDynamicAfter');
+  });
 
-    test('should be able to load into a structural directive', () async {
-      final fixture = await NgTestBed<CompWithDirective>().create();
-      expect(fixture.text, 'BeforeDynamicAfter');
-    });
+  test('should be able to load into a structural directive', () async {
+    final testBed = NgTestBed.forComponent(ng.CompWithDirectiveNgFactory);
+    final fixture = await testBed.create();
+    expect(fixture.text, 'BeforeDynamicAfter');
+  });
 
-    test('should be able to load from a service', () async {
-      final fixture = await NgTestBed<CompWithService>().create();
-      await fixture.update((comp) {
-        final ref = comp.service.loader.loadDetached(ng.DynamicCompNgFactory);
-        expect(ref.location.text, 'Dynamic');
-      });
+  test('should be able to load from a service', () async {
+    final testBed = NgTestBed.forComponent(ng.CompWithServiceNgFactory);
+    final fixture = await testBed.create();
+    await fixture.update((comp) {
+      final ref = comp.service.loader.loadDetached(ng.DynamicCompNgFactory);
+      expect(ref.location.text, 'Dynamic');
     });
   });
 }
