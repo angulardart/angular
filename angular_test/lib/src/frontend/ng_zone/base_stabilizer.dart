0daf6e41fd3279b4cfd32c996752a4f2c762bcfc
matanl <matanl@google.com>
2018-11-07 13:27:17 -0800

    feat(Test): Add FakeTimeNgZoneStabilizer, not published yet.
    ... which operates similar to Quiver's "FakeAsync", but only for Timers.
    
    See "ng_zone_test.dart" for details and examples of the API usage/behavior.
    
    PiperOrigin-RevId: 220518726

---
+++ angular_test/lib/src/frontend/ng_zone/base_stabilizer.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+import 'package:meta/meta.dart';
+import 'package:pedantic/pedantic.dart';
+
+import '../stabilizer.dart';
+import 'intercepted_timer.dart';
+
+/// A stabilizer expected to manage and use [NgZone] *and* its own [Zone].
+abstract class BaseNgZoneStabilizer<T extends Timer> extends NgTestStabilizer {
+  /// Instance of [NgZone] that was created for this stabilizer.
+  final NgZone ngZone;
+
+  /// Timer instances that are currently pending execution.
+  @protected
+  final Set<T> pendingTimers;
+
+  /// Creates the stabilizer wrapping an instance of [NgZone].
+  ///
+  /// Concrete classes should make sure this zone is created with a zone
+  /// specification that utilizes [InterceptedTimer.createTimer] for
+  /// [ZoneSpecification.createTimer].
+  BaseNgZoneStabilizer(this.ngZone, this.pendingTimers);
+
+  @mustCallSuper
+  @override
+  bool get isStable => !ngZone.hasPendingMicrotasks;
+
+  static void _noSideEffects() {}
+
+  @override
+  Future<bool> update([
+    void Function() runAndTrackSideEffects = _noSideEffects,
+  ]) {
+    return Future<void>.sync(() {
+      _triggerSideEffects(runAndTrackSideEffects);
+      return _waitForAsyncEventsOrErrors();
+    }).then((_) => isStable);
+  }
+
+  void _triggerSideEffects(void Function() withCallback) {
+    scheduleMicrotask(() => ngZone.runGuarded(() => withCallback()));
+  }
+
+  /// May be overridden to change what is awaited.
+  ///
+  /// The default implementation is only microtasks in the [NgZone].
+  @protected
+  Future<void> waitForAsyncEvents() => ngZone.onTurnDone.first;
+
+  @protected
+  static StackTrace rebuildStackTrace(List<Object> traceOrChain) {
+    return StackTrace.fromString(traceOrChain.join('\n'));
+  }
+
+  Future<void> _waitForAsyncEventsOrErrors() async {
+    Object uncaughtError;
+    StackTrace uncaughtStack;
+    StreamSubscription<void> onErrorSub;
+
+    // Used instead of .first in order to allow cancellation.
+    onErrorSub = ngZone.onError.listen((e) {
+      uncaughtError = e.error;
+      uncaughtStack = rebuildStackTrace(e.stackTrace);
+      onErrorSub.cancel();
+    });
+
+    // Wait for significant async events.
+    await waitForAsyncEvents();
+
+    // End the microtask loop to give a chance to catch other errors.
+    await Future(() {});
+    unawaited(onErrorSub.cancel());
+
+    // Return the caught error or just a blank future to continue.
+    return uncaughtError != null
+        ? Future.error(uncaughtError, uncaughtStack)
+        : Future.value();
+  }
+}
---
+++ angular_test/lib/src/frontend/ng_zone/fake_time_stabilizer.dart
@@ -0,0 +1,123 @@
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+
+import 'base_stabilizer.dart';
+
+/// Observes [NgZone], a custom parent zone, and custom hooks to stabilize.
+///
+/// * Any microtasks are automatically waited for stability in [update].
+/// * Any timers need to be manually elapsed using the [elapse] hook.
+///
+/// This stabilizer is a good choice for tests that need to assert transitional
+/// state (i.e. "idle", "loading", "loaded"), custom animations that use [Timer]
+/// or [Future.delayed]. The vast majority of tests are better off using the
+/// default ([RealTimeNgZoneStabilizer]) stabilizer.
+///
+/// **NOTE**: _Periodic_ timers are not supported by this stabilizer.
+class FakeTimeNgZoneStabilizer extends BaseNgZoneStabilizer<_FakeTimer> {
+  /// Creates a new stabilizer which manages a custom zone around an [NgZone].
+  ///
+  /// Expects a provided [createNgZone] callback, which will ensure the zone
+  /// is created _within_ the parent zone owned by [FakeTimeNgZoneStabilizer],
+  /// and applies appropriate zone-hooks in order to control the [Timer] API
+  /// using [elapse].
+  factory FakeTimeNgZoneStabilizer(NgZone Function() createNgZone) {
+    // All non-periodic timers that have been started, but not completed.
+    final pendingTimers = Set<_FakeTimer>.identity();
+    // The parent zone that adds hooks around every non-periodic timer.
+    FakeTimeNgZoneStabilizer stabilizer;
+    final timerZone = Zone.current.fork(
+      specification: ZoneSpecification(createTimer: (
+        self,
+        parent,
+        zone,
+        duration,
+        callback,
+      ) {
+        _FakeTimer instance;
+        void removeTimer() {
+          pendingTimers.remove(instance);
+        }
+
+        final wrappedCallback = () {
+          try {
+            callback();
+          } finally {
+            removeTimer();
+          }
+        };
+        instance = _FakeTimer(
+          zone.bindCallback(wrappedCallback),
+          removeTimer,
+          stabilizer._lastElapse + duration,
+        );
+        pendingTimers.add(instance);
+        return instance;
+      }),
+    );
+    return stabilizer = FakeTimeNgZoneStabilizer._(
+      timerZone.run(createNgZone),
+      pendingTimers,
+    );
+  }
+
+  FakeTimeNgZoneStabilizer._(
+    NgZone ngZone,
+    Set<_FakeTimer> pendingTimers,
+  ) : super(ngZone, pendingTimers);
+
+  /// The amount of time since construction that [elapse] has executed on.
+  var _lastElapse = Duration.zero;
+
+  /// Forces elapsing all timers that are less than or equal to duration [time].
+  ///
+  /// After each timers is elapsed, this implicitly calls [update], awaiting any
+  /// pending microtasks, and returns a future that completes when both all
+  /// timers and microtasks are completed.
+  Future<void> elapse(Duration time) async {
+    final waitUntil = _lastElapse + time;
+    await _completeTimers((t) => t._completeAfter <= waitUntil);
+    _lastElapse = waitUntil;
+  }
+
+  Future<void> _completeTimers(bool Function(_FakeTimer) shouldComplete) async {
+    var toComplete = pendingTimers.where(shouldComplete).toList();
+    do {
+      for (final pendingTimer in toComplete) {
+        await update(pendingTimer.complete);
+      }
+      toComplete = pendingTimers.where(shouldComplete).toList();
+    } while (toComplete.isNotEmpty);
+  }
+}
+
+/// A simplified fake timer that does not wrap an actual timer.
+class _FakeTimer implements Timer {
+  final void Function() _complete;
+  final void Function() _clearPendingStatus;
+  final Duration _completeAfter;
+
+  _FakeTimer(this._complete, this._clearPendingStatus, this._completeAfter);
+
+  @override
+  var isActive = true;
+
+  @override
+  int get tick => 0;
+
+  @override
+  void cancel() {
+    if (isActive) {
+      _clearPendingStatus();
+      isActive = false;
+    }
+  }
+
+  void complete() {
+    if (isActive) {
+      _complete();
+      cancel();
+    }
+  }
+}
