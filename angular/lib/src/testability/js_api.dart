9c310dc5018dfef3c59e209fbd0ebe8441a309b1
matanl <matanl@google.com>
2018-11-12 14:46:35 -0800

    feat(Test): Add additional JS API support for Testability.
    This is a no-op change, as it only declares additional APIs that are not yet in use. In a follow-up CL, I will switch the "js_util.get/set" methods to use these APIs instead, 1 by 1, since these changes may only break e2e tests.
    
    PiperOrigin-RevId: 221160821

--- angular/lib/src/testability/js_api.dart
+++ angular/lib/src/testability/js_api.dart
@@ -1,8 +1,29 @@
 @JS()
 library angular.src.testability.js_api;
 
+import 'dart:html';
+
 import 'package:js/js.dart';
 import 'package:meta/meta.dart';
+
+/// A function that returns a testability API for a provided DOM element.
+typedef JsGetAngularTestability = JsTestability Function(Element);
+
+/// A function that returns all testability APIs currently available.
+typedef JsGetAllAngularTestabilities = List<JsTestability> Function();
+
+/// Globally scoped [JsTestabilityRegistry] accessors.
+@JS('self')
+external JsTestabilityRegistry get jsTestabilityGlobals;
+
+/// An array of stability functions shared across major frameworks.
+///
+/// Other non-AngularDart stabilizers may also be registered here!
+@JS('frameworkStabilizers')
+external List<void Function(bool didAsyncWork)> get frameworkStabilizers;
+
+@JS('frameworkStabilizers')
+external set frameworkStabilizers(List<void Function(bool)> list);
 
 /// A JavaScript interface for interacting with AngularDart's `Testability` API.
 ///
@@ -12,7 +33,7 @@ import 'package:meta/meta.dart';
 abstract class JsTestability {
   external factory JsTestability({
     @required bool Function() isStable,
-    @required void Function(void Function(bool)) whenStable,
+    @required void Function(void Function(bool didAsyncWork)) whenStable,
   });
 
   /// Returns whether the application is considered stable.
@@ -33,5 +54,26 @@ abstract class JsTestability {
   /// invoked, [callback] is invoked with a value of `false` for `didWork`,
   /// indicating that no asynchronous work was awaited before execution.
   /// Otherwise a value of `true` is passed.
-  void whenStable(void Function(bool didWork) callback);
+  void whenStable(void Function(bool) callback);
+}
+
+@JS()
+@anonymous
+abstract class JsTestabilityRegistry {
+  external factory JsTestabilityRegistry({
+    @required JsGetAngularTestability getAngularTestability,
+    @required JsGetAllAngularTestabilities getAllAngularTestabilities,
+  });
+
+  /// Given a DOM [rootElement], returns the [JsTestability] interface for it.
+  ///
+  /// It may throw a runtime error if the element provided does not have a
+  /// testability interface (for example, it may not represent the root element
+  /// of an AngularDart app, or e2e testing was not enabled for that app).
+  external JsGetAngularTestability get getAngularTestability;
+  external set getAngularTestability(JsGetAngularTestability fn);
+
+  /// Returns an array of _all_ [JsTestability] interfaces in the browser.
+  external JsGetAllAngularTestabilities get getAllAngularTestabilities;
+  external set getAllAngularTestabilities(JsGetAllAngularTestabilities fn);
 }
