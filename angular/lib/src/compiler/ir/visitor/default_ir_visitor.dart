1476a076378f2f0d78a54b75364314d0be873382
alorenzen <alorenzen@google.com>
2018-11-15 13:44:48 -0800

    Add new IR model to compiler.
    Right now, this is just the root "Component" node. In follow-up CLs, I'll add the "ComponentView" and "HostView" classes and start using them in the actual compiler pipeline.
    
    PiperOrigin-RevId: 221682466

---
+++ angular/lib/src/compiler/ir/visitor/default_ir_visitor.dart
@@ -0,0 +1,22 @@
+import '../model.dart';
+
+/// An [IRVisitor] which simply returns null for every node visited.
+///
+/// This is intended to be a base class for other [IRVisitors], allowing other
+/// visitors to implement only the methods needed.
+class DefaultIRVisitor<R, C> implements IRVisitor<R, C> {
+  @override
+  R visitComponent(Component component, [C context]) => null;
+
+  /// Visits all nodes provided, aggregating the result.
+  ///
+  /// If the visitor returns [null], then the result is removed from the list.
+  List<T> visitAll<T extends R>(Iterable<IRNode> nodes, [C context]) {
+    final results = <T>[];
+    for (var node in nodes) {
+      var result = node.accept(this, context) as T;
+      if (result != null) results.add(result);
+    }
+    return results;
+  }
+}
