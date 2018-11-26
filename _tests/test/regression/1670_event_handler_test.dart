1f637e116fad6d01fda772bcfaed177631b97910
matanl <matanl@google.com>
2018-11-26 13:44:15 -0800

    test(Core): Show other inconsistencies in event handling with tear-offs.
    Specifically (https://github.com/dart-lang/angular/issues/1670) that top-level methods, and methods accessed via property access (e.g. "chained") produce incorrect code that does not actually call the method.
    
    It's not clear we actually want chained to work (since we aren't invoking a method necessarily). If so, we should make this a compile-time error instead of just silently doing nothing.
    
    PiperOrigin-RevId: 222879753

---
+++ _tests/test/regression/1670_event_handler_test.dart
@@ -0,0 +1,82 @@
+@TestOn('browser')
+import 'package:angular/angular.dart';
+import 'package:angular_test/angular_test.dart';
+import 'package:test/test.dart';
+
+import '1670_event_handler_test.template.dart' as ng;
+
+void main() {
+  tearDown(disposeAnyRunningTest);
+
+  test('should support top-level methods tear-offs for events', () async {
+    final testBed = NgTestBed.forComponent<TestTopLevelMethods>(
+      ng.TestTopLevelMethodsNgFactory,
+    );
+    final fixture = await testBed.create();
+    overrideTopLevelDoCapture = expectAsync0(() {});
+    await fixture.update((_) {
+      fixture.rootElement.querySelector('button').click();
+    });
+  }, skip: 'https://github.com/dart-lang/angular/issues/1670');
+
+  test('should support static methods tear-offs for events', () async {
+    final testBed = NgTestBed.forComponent<TestStaticMethods>(
+      ng.TestStaticMethodsNgFactory,
+    );
+    final fixture = await testBed.create();
+    TestStaticMethods.overrideDoCapture = expectAsync0(() {});
+    await fixture.update((_) {
+      fixture.rootElement.querySelector('button').click();
+    });
+  });
+
+  test('should support chained method tear-offs for events', () async {
+    final testBed = NgTestBed.forComponent<TestChainedMethods>(
+      ng.TestChainedMethodsNgFactory,
+    );
+    final fixture = await testBed.create();
+    fixture.assertOnlyInstance.bar.overrideDoCapture = expectAsync0(() {});
+    await fixture.update((_) {
+      fixture.rootElement.querySelector('button').click();
+    });
+  }, skip: 'https://github.com/dart-lang/angular/issues/1670');
+}
+
+void Function() overrideTopLevelDoCapture = () {};
+void topLevelDoCapture() {
+  overrideTopLevelDoCapture();
+}
+
+@Component(
+  selector: 'test',
+  exports: [topLevelDoCapture],
+  template: r'<button (click)="topLevelDoCapture"></button>',
+)
+class TestTopLevelMethods {}
+
+@Component(
+  selector: 'test',
+  exports: [],
+  template: r'<button (click)="TestStaticMethods.doCapture"></button>',
+)
+class TestStaticMethods {
+  static void Function() overrideDoCapture = () {};
+  static void doCapture() {
+    overrideDoCapture();
+  }
+}
+
+@Component(
+  selector: 'test',
+  template: r'<button (click)="bar.doCapture"></button>',
+)
+class TestChainedMethods {
+  final bar = Bar();
+}
+
+class Bar {
+  void Function() overrideDoCapture = () {};
+  void doCapture() {
+    overrideDoCapture();
+  }
+}
