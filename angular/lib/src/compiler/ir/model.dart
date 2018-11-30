333ce630032430cbb40f8faf4af01dbf55d2ede4
alorenzen <alorenzen@google.com>
2018-11-29 16:45:59 -0800

    Start using IR in offline compiler.
    This is just the first checkpoint, to put a stake in the sand, for where we will start the split between the "frontend" and "backend" of the compiler. This is by no means intended to be a final state for any of the parts involved.
    
    RELNOTES=None
    (grafted from 4975a9209acd560ded4b24beec21504c653cb253)
    PiperOrigin-RevId: 223435324

--- angular/lib/src/compiler/ir/model.dart
+++ angular/lib/src/compiler/ir/model.dart
@@ -1,3 +1,7 @@
+import 'package:meta/meta.dart';
+import 'package:angular/src/compiler/compile_metadata.dart';
+import 'package:angular/src/compiler/template_ast.dart';
+
 /// Base class for all intermediate representation (IR) data model classes.
 abstract class IRNode {
   R accept<R, C>(IRVisitor<R, C> visitor, [C context]);
@@ -6,25 +10,69 @@ abstract class IRNode {
 /// The core reusable UI building blocks for an application.
 class Component implements IRNode {
   final String name;
+  final ViewEncapsulation encapsulation;
 
   final List<View> views;
 
-  Component(this.name, {this.views = const []});
+  final List<String> styles;
+  final List<String> styleUrls;
+
+  Component(this.name,
+      {this.encapsulation = ViewEncapsulation.emulated,
+      this.views = const [],
+      this.styles = const [],
+      this.styleUrls = const []});
 
   @override
   R accept<R, C>(IRVisitor<R, C> visitor, [C context]) =>
       visitor.visitComponent(this, context);
 }
 
+/// Defines template and style encapsulation options available for Component's
+/// [View].
+///
+/// See [View#encapsulation].
+enum ViewEncapsulation {
+  /// Emulate `Native` scoping of styles by adding an attribute containing
+  /// surrogate id to the Host Element and pre-processing the style rules
+  /// provided via [View#styles] or [View#stylesUrls], and
+  /// adding the new Host Element attribute to all selectors.
+  ///
+  /// This is the default option.
+  emulated,
+
+  /// Don't provide any template or style encapsulation.
+  none,
+}
+
 abstract class View extends IRNode {
   List<IRNode> get children;
+
+  // TODO(alorenzen): Replace with IR model classes.
+  CompileDirectiveMetadata get cmpMetadata;
+  List<TemplateAst> get parsedTemplate;
+  List<CompileTypedMetadata> get directiveTypes;
+  List<CompilePipeMetadata> get pipes;
 }
 
 class ComponentView implements View {
   @override
   final List<IRNode> children;
+  @override
+  final CompileDirectiveMetadata cmpMetadata;
+  @override
+  final List<TemplateAst> parsedTemplate;
+  @override
+  final List<CompileTypedMetadata> directiveTypes;
+  @override
+  final List<CompilePipeMetadata> pipes;
 
-  ComponentView({this.children = const []});
+  ComponentView(
+      {this.children = const [],
+      @required this.cmpMetadata,
+      this.parsedTemplate = const [],
+      this.directiveTypes = const [],
+      this.pipes = const []});
 
   @override
   R accept<R, C>(IRVisitor<R, C> visitor, [C context]) =>
@@ -34,7 +82,21 @@ class ComponentView implements View {
 class HostView implements View {
   final ComponentView componentView;
 
-  HostView(this.componentView);
+  @override
+  final CompileDirectiveMetadata cmpMetadata;
+  @override
+  final List<TemplateAst> parsedTemplate;
+  @override
+  final List<CompileTypedMetadata> directiveTypes;
+  @override
+  final List<CompilePipeMetadata> pipes = const [];
+
+  HostView(
+    this.componentView, {
+    @required this.cmpMetadata,
+    this.parsedTemplate = const [],
+    this.directiveTypes = const [],
+  });
 
   @override
   R accept<R, C>(IRVisitor<R, C> visitor, [C context]) =>
