65eb364cd3b76667d9e8ef33243713c60bdfac6d
alorenzen <alorenzen@google.com>
2018-11-29 16:23:02 -0800

    Add ComponentView and HostView to new IR model.
    PiperOrigin-RevId: 223431802

--- angular/lib/src/compiler/ir/model.dart
+++ angular/lib/src/compiler/ir/model.dart
@@ -7,13 +7,46 @@ abstract class IRNode {
 class Component implements IRNode {
   final String name;
 
-  Component(this.name);
+  final List<View> views;
+
+  Component(this.name, {this.views = const []});
 
   @override
   R accept<R, C>(IRVisitor<R, C> visitor, [C context]) =>
       visitor.visitComponent(this, context);
 }
 
+abstract class View extends IRNode {
+  List<IRNode> get children;
+}
+
+class ComponentView implements View {
+  @override
+  final List<IRNode> children;
+
+  ComponentView({this.children = const []});
+
+  @override
+  R accept<R, C>(IRVisitor<R, C> visitor, [C context]) =>
+      visitor.visitComponentView(this, context);
+}
+
+class HostView implements View {
+  final ComponentView componentView;
+
+  HostView(this.componentView);
+
+  @override
+  R accept<R, C>(IRVisitor<R, C> visitor, [C context]) =>
+      visitor.visitHostView(this, context);
+
+  @override
+  List<IRNode> get children => [componentView];
+}
+
 abstract class IRVisitor<R, C> {
   R visitComponent(Component component, [C context]);
+
+  R visitComponentView(ComponentView componentView, [C context]);
+  R visitHostView(HostView hostView, [C context]);
 }
