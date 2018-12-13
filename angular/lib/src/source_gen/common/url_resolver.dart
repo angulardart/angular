d3b4e950120fd570c51e8f364a7c92d0228fa331
alorenzen <alorenzen@google.com>
2018-12-13 15:37:12 -0800

    Remove source_gen/common/names.dart.
    Move css names into css builder.
    
    Move templateExtension code into url_resolver.dart.
    
    PiperOrigin-RevId: 225448204

--- angular/lib/src/source_gen/common/url_resolver.dart
+++ angular/lib/src/source_gen/common/url_resolver.dart
@@ -79,3 +79,34 @@ Uri _toAssetScheme(Uri absoluteUri) {
   var pathSegments = absoluteUri.pathSegments.toList()..insert(1, 'lib');
   return Uri(scheme: 'asset', pathSegments: pathSegments);
 }
+
+/// Returns `uri` with its extension updated to [_templateExtension].
+String toTemplateExtension(String uri) =>
+    _toExtension(uri, _allExtensions, _templateExtension);
+
+/// Returns `uri` with its extension updated to `toExtension` if its
+/// extension is currently in `fromExtension`.
+String _toExtension(
+    String uri, Iterable<String> fromExtensions, String toExtension) {
+  if (uri == null) return null;
+  if (uri.endsWith(toExtension)) return uri;
+  for (var extension in fromExtensions) {
+    if (uri.endsWith(extension)) {
+      return '${uri.substring(0, uri.length - extension.length)}'
+          '$toExtension';
+    }
+  }
+  throw ArgumentError.value(
+      uri,
+      'uri',
+      'Provided value ends with an unexpected extension. '
+      'Expected extension(s): [${fromExtensions.join(', ')}].');
+}
+
+const _templateExtension = '.template.dart';
+
+/// Note that due to the implementation of `_toExtension`, ordering is
+/// important. For example, putting '.dart' first in this list will cause
+/// incorrect behavior because it will (incompletely) match '.template.dart'
+/// files.
+const _allExtensions = [_templateExtension, '.ng_placeholder', '.dart'];
