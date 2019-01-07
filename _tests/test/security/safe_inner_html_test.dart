bcc7f32147a60e8d3a6f1ee8c8c6710d72ac3fe7
deboer <deboer@google.com>
2019-01-07 13:30:35 -0800

    Improve the safe_inner_html tests. -> adds () to make the assertions match on the correct element instead of inert <script> tags. -> replace <script> tags, which do not execute with <img onload> injection -> add tests for property interpolation
    PiperOrigin-RevId: 228225045

--- _tests/test/security/safe_inner_html_test.dart
+++ _tests/test/security/safe_inner_html_test.dart
@@ -17,19 +17,31 @@ void main() {
     test('normally, "innerHtml" should be sanitized', () async {
       var testBed = NgTestBed<NormalInnerHtmlTest>();
       var testRoot = await testBed.create();
-      expect(testRoot.text, contains('Secure'));
+      expect(testRoot.text, contains('(Secure)'));
     });
 
     test('"safeInnerHtml" should be trusted', () async {
       var testBed = NgTestBed<TrustedSafeInnerHtmlTest>();
       var testRoot = await testBed.create();
-      expect(testRoot.text, contains('Unsafe'));
+      expect(testRoot.text, contains('(Unsafe)'));
     });
 
     test('"innerHtml" should be trusted', () async {
       var testBed = NgTestBed<TrustedInnerHtmlTest>();
       var testRoot = await testBed.create();
-      expect(testRoot.text, contains('Unsafe'));
+      expect(testRoot.text, contains('(Unsafe)'));
+    });
+
+    test('normally, interpolated innerHtml should be sanitized', () async {
+      var testBed = NgTestBed<InterpolatedNormalInnerHtmlTest>();
+      var testRoot = await testBed.create();
+      expect(testRoot.text, contains('(Secure)'));
+    });
+
+    test('SafeHtml should be passed through interpolation', () async {
+      var testBed = NgTestBed<InterpolatedTrustedInnerHtmlTest>();
+      var testRoot = await testBed.create();
+      expect(testRoot.text, contains('(Unsafe)'));
     });
 
     test('unsafe HTML should throw', () async {
@@ -39,26 +51,30 @@ void main() {
   });
 }
 
+// <script> tags are inert in innerHTML: https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#Security_considerations
+// placeholder from //gws/suite/img/img_test.js
+const String DANGEROUS_HTML = '''
+  <img src='data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
+       onload="document.querySelector('.other-element').innerText = 'Unsafe'">
+   </img>
+''';
+
 @Component(
   selector: 'test',
   template: r'''
-       <span class="other-element">Secure</span>
+       (<span class="other-element">Secure</span>)
        <div [innerHtml]="trustedHtml"></div>
     ''',
 )
 class NormalInnerHtmlTest {
-  String get trustedHtml => r'''
-    <script>
-      document.querySelector('.other-element').innerText = 'Unsafe';
-    </script>
-  ''';
+  String get trustedHtml => DANGEROUS_HTML;
 }
 
 @Component(
   selector: 'test',
   directives: [SafeInnerHtmlDirective],
   template: r'''
-       <span class="other-element">Secure</span>
+       (<span class="other-element">Secure</span>)
        <div [safeInnerHtml]="trustedHtml"></div>
     ''',
 )
@@ -67,17 +83,14 @@ class TrustedSafeInnerHtmlTest {
   final SafeHtml trustedHtml;
 
   TrustedSafeInnerHtmlTest(DomSanitizationService domSecurityService)
-      : trustedHtml = domSecurityService.bypassSecurityTrustHtml(r'''
-        <script>
-          document.querySelector('.other-element').innerText = 'Unsafe';
-        </script>
-      ''');
+      : trustedHtml =
+            domSecurityService.bypassSecurityTrustHtml(DANGEROUS_HTML);
 }
 
 @Component(
   selector: 'test',
   template: r'''
-       <span class="other-element">Secure</span>
+       (<span class="other-element">Secure</span>)
        <div [innerHtml]="trustedHtml"></div>
     ''',
 )
@@ -86,11 +99,36 @@ class TrustedInnerHtmlTest {
   final SafeHtml trustedHtml;
 
   TrustedInnerHtmlTest(DomSanitizationService domSecurityService)
-      : trustedHtml = domSecurityService.bypassSecurityTrustHtml(r'''
-        <script>
-          document.querySelector('.other-element').innerText = 'Unsafe';
-        </script>
-      ''');
+      : trustedHtml =
+            domSecurityService.bypassSecurityTrustHtml(DANGEROUS_HTML);
+}
+
+@Component(
+  selector: 'test',
+  template: r'''
+       (<span class="other-element">Secure</span>)
+       <div innerHtml="{{trustedHtml}}"></div>
+    ''',
+)
+class InterpolatedNormalInnerHtmlTest {
+  final String trustedHtml = DANGEROUS_HTML;
+}
+
+@Component(
+  selector: 'test',
+  template: r'''
+       (<span class="other-element">Secure</span>)
+       <div innerHtml="{{trustedHtml}}"></div>
+    ''',
+)
+class InterpolatedTrustedInnerHtmlTest {
+  // Value will be passed through interpolate0 and then passed through the
+  // HTML sanitizer.
+  final SafeHtml trustedHtml;
+
+  InterpolatedTrustedInnerHtmlTest(DomSanitizationService domSecurityService)
+      : trustedHtml =
+            domSecurityService.bypassSecurityTrustHtml(DANGEROUS_HTML);
 }
 
 @Component(
