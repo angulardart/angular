262b1c0a2d05ce8c73476cba359260f968a6472a
matanl <matanl@google.com>
2019-01-11 13:11:55 -0800

    refactor(Core): Move "destroy", "detectChanges[Internal]", add more docs.
    PiperOrigin-RevId: 228932463

--- angular/lib/src/core/linker/app_view_base.dart
+++ angular/lib/src/core/linker/app_view_base.dart
@@ -9,8 +9,9 @@ import '../../di/injector/injector.dart';
 /// breaking changes. Statically we validate that every concrete class, at the
 /// end of the day, must implement all these public (or protected) members).
 ///
-/// **NOTE**: This class is not yet complete, most members are in `AppView`.
-abstract class View {
+/// * [T] should be represent a `class` annotated with `@Component`.
+/// * This class is not yet complete, most members are in `AppView`.
+abstract class View<T> {
   /// Implements the semantic elements of the current view.
   ///
   /// For component and embedded views, this means, for the most part, creating
@@ -19,6 +20,25 @@ abstract class View {
   /// for later access (such as in [detectChanges] or [destroy]).
   @protected
   void build();
+
+  /// Destroys the internal state of the view.
+  ///
+  /// If appropriate, any nodes that were added to the DOM by [build] are also
+  /// detached from the DOM and destroyed.
+  void destroy();
+
+  /// Invokes change detection on this view and any child views.
+  ///
+  /// A view that has an uncaught exception, is destroyed, or is otherwise
+  /// not meant to be checked (such as being detached or having a change
+  /// detection mode that skips checks conditionally) should immediately return.
+  void detectChanges();
+
+  /// Backing implementation of `detectChanges` for the current view.
+  ///
+  /// Defaults to an empty method for the rare components with no bindings.
+  @protected
+  void detectChangesInternal() {}
 
   /// Adapts and returns services available at [nodeIndex] as an [Injector].
   ///
@@ -41,6 +61,10 @@ abstract class View {
   ///
   /// By default (i.e. for views with no provided services or references), this
   /// is expected to be an identity function for returning [notFoundResult].
+  ///
+  /// In a generated view, the component view retains some of the information
+  /// for it's children's providers, with each child node representing a
+  /// different [nodeIndex].
   @protected
   dynamic injectorGetInternal(
     Object token,
