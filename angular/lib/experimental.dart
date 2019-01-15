de6920c2b5b30d5d1d7a58f9cad8dac15a427093
matanl <matanl@google.com>
2019-01-14 16:07:22 -0800

    refactor(*): Move setAttr[NS]/createAttr to dom_helpers, refactor a bit.
    * `<AppView>.setAttr` -> `updateAttribute`
    * `<AppView>.setAttrNS` -> `updateAttributeNS`
    * `<AppView>.createAttr` -> `setAttribute`
    
    Additionally, we no longer use `<Element>.attributes.remove(key)`, but instead use `@JS()`-interop to directly call `<Element>.removeAttribute` (pending https://github.com/dart-lang/sdk/issues/35655).
    
    PiperOrigin-RevId: 229275285

--- angular/lib/experimental.dart
+++ angular/lib/experimental.dart
@@ -13,9 +13,9 @@ import 'package:angular/src/runtime.dart';
 import 'package:meta/meta.dart';
 
 import 'src/bootstrap/run.dart' show appInjector;
-import 'src/core/linker/app_view.dart' as app_view;
 import 'src/di/injector/injector.dart';
 import 'src/runtime.dart';
+import 'src/runtime/dom_helpers.dart';
 
 export 'src/bootstrap/modules.dart' show bootstrapLegacyModule;
 export 'src/common/directives/ng_for_identity.dart' show NgForIdentity;
@@ -56,12 +56,12 @@ Injector rootLegacyInjector(InjectorFactory userInjector) {
 ///
 /// **WARNING**: This API is not considered part of the stable API.
 @experimental
-bool isDomRenderDirty() => app_view.domRootRendererIsDirty;
+bool isDomRenderDirty() => domRootRendererIsDirty;
 
 /// Resets the state of [isDomRenderDirty] to `false`.
 ///
 /// **WARNING**: This API is not considered part of the stable API.
 @experimental
 void resetDomRenderDirty() {
-  app_view.domRootRendererIsDirty = false;
+  domRootRendererIsDirty = false;
 }
--- angular/lib/src/runtime/dom_helpers.dart
+++ angular/lib/src/runtime/dom_helpers.dart
@@ -1,9 +1,37 @@
 /// This library is considered separate from rest of `runtime.dart`, as it
 /// imports `dart:html` and `runtime.dart` is currently used on libraries
 /// that expect to only run on the command-line VM.
+library angular.src.runtime.dom_helpers;
+
 import 'dart:html';
 
+import 'package:js/js_util.dart' as js;
 import 'package:meta/dart2js.dart' as dart2js;
+
+// Adds additional (missing) methods to `dart:html`'s [Element].
+//
+// TODO(https://github.com/dart-lang/sdk/issues/35655): Remove.
+
+/// https://developer.mozilla.org/en-US/docs/Web/API/Element/removeAttribute
+void _removeAttribute(Element e, String attribute) {
+  js.callMethod(e, 'removeAttribute', [attribute]);
+}
+
+/// https://developer.mozilla.org/en-US/docs/Web/API/Element/removeAttributeNS
+void _removeAttributeNS(Element e, String namespace, String attribute) {
+  js.callMethod(e, 'removeAttributeNS', [attribute]);
+}
+
+/// Set to `true` when Angular modified the DOM.
+///
+/// May be used in order to optimize polling techniques that attempt to only
+/// process events after a significant change detection cycle (i.e. one that
+/// modified the DOM versus a no-op).
+///
+/// **NOTE**: What sets this to `true` (versus ignores it entirely) is currently
+/// not consistent (it skips some methods that knowingly update the DOM). See
+/// b/122842549.
+var domRootRendererIsDirty = false;
 
 /// Either adds or removes [className] to [element] based on [isAdd].
 ///
@@ -41,4 +69,51 @@ void updateClassBindingNonHtml(Element element, String className, bool isAdd) {
   } else {
     element.classes.remove(className);
   }
+}
+
+/// Updates [attribute] on [element] to reflect [value].
+///
+/// If [value] is `null`, this implicitly _removes_ [attribute] from [element].
+@dart2js.noInline
+void updateAttribute(
+  Element element,
+  String attribute,
+  String value,
+) {
+  if (value == null) {
+    _removeAttribute(element, attribute);
+  } else {
+    setAttribute(element, attribute, value);
+  }
+  domRootRendererIsDirty = true;
+}
+
+/// Similar to [updateAttribute], but supports name-spaced attributes.
+@dart2js.noInline
+void updateAttributeNS(
+  Element element,
+  String namespace,
+  String attribute,
+  String value,
+) {
+  if (value == null) {
+    _removeAttributeNS(element, namespace, attribute);
+  } else {
+    element.setAttributeNS(namespace, attribute, value);
+  }
+  domRootRendererIsDirty = true;
+}
+
+/// Similar to [updateAttribute], but strictly for setting the initial [value].
+///
+/// This is meant as a slight optimization when initially building elements
+/// from the template, as it does not check to see if [value] is `null` (and
+/// the attribute should be removed) nor does it set [domRootRendererIsDirty].
+@dart2js.noInline
+void setAttribute(
+  Element element,
+  String attribute, [
+  String value = '',
+]) {
+  element.setAttribute(attribute, value);
 }
