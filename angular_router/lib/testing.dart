0c1f1c5bfbba516b4cdc4798828f567e390f59cc
greghaines <greghaines@google.com>
2019-02-01 09:13:11 -0800

    feat(Router): Add DI modules for testing in Angular Router   - Follow on to cl/231722909
    PiperOrigin-RevId: 231981778

--- angular_router/lib/testing.dart
+++ angular_router/lib/testing.dart
@@ -1,4 +1,4 @@
-import 'package:angular/angular.dart' show Provider;
+import 'package:angular/angular.dart' show ClassProvider, Module;
 
 import 'src/location.dart';
 import 'src/location/testing/mock_location_strategy.dart';
@@ -7,18 +7,33 @@ import 'src/router/router_impl.dart';
 
 export 'src/location/testing/mock_location_strategy.dart';
 
-/// The main router providers for testing.
+/// The main [Router] providers for testing.
+///
+/// Add these providers to your test bed for testing your route configuration or
+/// components with dependencies on the router.
+///
+/// ```
+/// final testBed = NgTestBed<TestComponent>().addProviders([
+///   routerProvidersTest,
+/// ]);
+/// ```
+const routerProvidersTest = [
+  ClassProvider(LocationStrategy, useClass: MockLocationStrategy),
+  ClassProvider(Location),
+  ClassProvider(Router, useClass: RouterImpl),
+];
+
+/// The main [Router] DI module for testing.
 ///
 /// Add this module to your test bed for testing your route configuration or
 /// components with dependencies on the router.
 ///
 /// ```
-/// final testBed = new NgTestBed<TestComponent>().addProviders([
-///   routerProvidersTest,
-/// ]);
+/// @GenerateInjector.fromModules([routerTestModule])
+/// final InjectorFactory testInjector = ng.testInjector$Injector;
+/// ...
+/// final testBed = NgTestBed.forComponent(
+///     ng.TestComponentNgFactory,
+///     rootInjector: testInjector);
 /// ```
-const routerProvidersTest = [
-  Provider(LocationStrategy, useClass: MockLocationStrategy),
-  Provider(Location),
-  Provider(Router, useClass: RouterImpl),
-];
+const routerTestModule = Module(provide: routerProvidersTest);
