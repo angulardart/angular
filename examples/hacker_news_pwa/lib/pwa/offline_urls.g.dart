c2b3497318d118017d2de86084fc4c95e417e252
Leon Senft <leonsenft@google.com>
2019-02-11 10:21:06 -0800

    chore(Examples): update HNPWA cache timestamp
    Also adds the `.firebase/` directory that is now generated by deploying to
    Firebase Hosting to `.gitignore`.
    
    Closes #1715
    
    PiperOrigin-RevId: 233432029

--- examples/hacker_news_pwa/lib/pwa/offline_urls.g.dart
+++ examples/hacker_news_pwa/lib/pwa/offline_urls.g.dart
@@ -8,4 +8,4 @@ final List<String> offlineUrls = [
 ];
 
 /// Last modified timestamp of the files
-final String lastModified = '2018-08-09T00:00:00+00:00';
+final String lastModified = '2019-02-11T00:00:00+00:00';
