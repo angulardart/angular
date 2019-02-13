80a4aabf607405c9b9ead5bc4b9307d578784f63
matanl <matanl@google.com>
2019-02-13 13:45:10 -0800

    refactor(Compiler): Move our builder to src/ as build.dart.
    It is never intended to be imported by an end-user. This also removes it from being auto-suggested as an import in IntelliJ and other IDEs.
    
    PiperOrigin-RevId: 233821412

--- angular/lib/src/build.dart
+++ angular/lib/src/build.dart
@@ -14,8 +14,8 @@ import 'package:logging/logging.dart';
 import 'package:angular_compiler/angular_compiler.dart';
 import 'package:angular_compiler/cli.dart';
 
-import 'src/compiler/stylesheet_compiler/builder.dart';
-import 'src/source_gen/template_compiler/generator.dart';
+import 'compiler/stylesheet_compiler/builder.dart';
+import 'source_gen/template_compiler/generator.dart';
 
 /// An option to generate a lighter-weight output for complex build systems.
 ///
