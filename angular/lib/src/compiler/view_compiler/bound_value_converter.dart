49036148e793d99fd228a7f8521dd659d287b8c3
alorenzen <alorenzen@google.com>
2019-02-15 14:16:17 -0800

    Refactor BoundValueConverter to move more code into the core base class and out of the subclasses.
    Eventually, I think we can drop the two subclasses and handle the difference in i18n messages as a validation step on the IR. This is just progress towards that end goal.
    
    PiperOrigin-RevId: 234211230

--- angular/lib/src/compiler/view_compiler/bound_value_converter.dart
+++ angular/lib/src/compiler/view_compiler/bound_value_converter.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
 import 'package:source_span/source_span.dart';
+import 'package:angular/src/compiler/i18n/message.dart';
 
 import '../analyzed_class.dart' as analyzer;
 import '../compile_metadata.dart' show CompileDirectiveMetadata;
@@ -15,9 +16,15 @@ void _throwUnrecognized(BoundValue value) {
 
 /// An abstract utility for converting bound values to output expressions.
 abstract class BoundValueConverter {
-  final analyzer.AnalyzedClass _analyzedClass;
+  final CompileDirectiveMetadata _metadata;
+  final o.Expression _implicitReceiver;
+  final NameResolver _nameResolver;
 
-  BoundValueConverter(this._analyzedClass);
+  BoundValueConverter(
+    this._metadata,
+    this._implicitReceiver,
+    this._nameResolver,
+  );
 
   /// Creates a bound value converter for a directive change detector.
   ///
@@ -31,8 +38,7 @@ abstract class BoundValueConverter {
     CompileDirectiveMetadata metadata,
     o.Expression implicitReceiver,
     NameResolver nameResolver,
-  ) =>
-      _DirectiveBoundValueConverter(metadata, implicitReceiver, nameResolver);
+  ) = _DirectiveBoundValueConverter;
 
   /// Creates a bound value converter for expressions in a [view].
   ///
@@ -42,25 +48,44 @@ abstract class BoundValueConverter {
   factory BoundValueConverter.forView(
     CompileView view,
     o.Expression implicitReceiver,
-  ) =>
-      _ViewBoundValueConverter(view, implicitReceiver);
+  ) = _ViewBoundValueConverter;
 
   /// Converts a bound [value] to an expression.
   ///
   /// The [sourceSpan] of [value] is used for reporting errors that may occur
   /// during expression conversion.
   ///
-  /// The [propertyType] is the type of the property to which [value] is bound.
+  /// The [type] is the type of the property to which [value] is bound.
   o.Expression convertToExpression(
     BoundValue value,
     SourceSpan sourceSpan,
-    o.OutputType propertyType,
-  );
+    o.OutputType type,
+  ) {
+    if (value is BoundExpression) {
+      final expression =
+          analyzer.rewriteInterpolate(value.expression, analyzedClass);
+      return convertCdExpressionToIr(
+        _nameResolver,
+        _implicitReceiver,
+        expression,
+        sourceSpan,
+        _metadata,
+        type,
+      );
+    } else if (value is BoundI18nMessage) {
+      return _createI18nMessage(value.message);
+    }
+    _throwUnrecognized(value);
+  }
+
+  o.Expression _createI18nMessage(I18nMessage message);
+
+  analyzer.AnalyzedClass get analyzedClass => _metadata.analyzedClass;
 
   /// Returns whether [value] can change during its lifetime.
   bool isImmutable(BoundValue value) {
     if (value is BoundExpression) {
-      return analyzer.isImmutable(value.expression, _analyzedClass);
+      return analyzer.isImmutable(value.expression, analyzedClass);
     } else if (value is BoundI18nMessage) {
       return true;
     }
@@ -80,7 +105,7 @@ abstract class BoundValueConverter {
   /// Returns whether [value] is a string.
   bool isString(BoundValue value) {
     if (value is BoundExpression) {
-      return analyzer.isString(value.expression, _analyzedClass);
+      return analyzer.isString(value.expression, analyzedClass);
     } else if (value is BoundI18nMessage) {
       return true;
     }
@@ -90,67 +115,27 @@ abstract class BoundValueConverter {
 
 /// Converts values bound by a directive change detector.
 class _DirectiveBoundValueConverter extends BoundValueConverter {
-  final o.Expression _implicitReceiver;
-  final CompileDirectiveMetadata _metadata;
-  final NameResolver _nameResolver;
-
   _DirectiveBoundValueConverter(
-    this._metadata,
-    this._implicitReceiver,
-    this._nameResolver,
-  ) : super(_metadata.analyzedClass);
+    CompileDirectiveMetadata metadata,
+    o.Expression implicitReceiver,
+    NameResolver nameResolver,
+  ) : super(metadata, implicitReceiver, nameResolver);
 
   @override
-  o.Expression convertToExpression(
-    BoundValue value,
-    SourceSpan sourceSpan,
-    o.OutputType type,
-  ) {
-    if (value is BoundExpression) {
-      return convertCdExpressionToIr(
-        _nameResolver,
-        _implicitReceiver,
-        value.expression,
-        sourceSpan,
-        _metadata,
-        type,
-      );
-    } else if (value is BoundI18nMessage) {
-      throw UnsupportedError(
-          'Cannot create internationalized message expression without a view');
-    }
-    _throwUnrecognized(value);
+  o.Expression _createI18nMessage(I18nMessage message) {
+    throw UnsupportedError(
+        'Cannot create internationalized message expression without a view');
   }
 }
 
 // Converts values bound in a view.
 class _ViewBoundValueConverter extends BoundValueConverter {
-  final o.Expression _implicitReceiver;
   final CompileView _view;
 
-  _ViewBoundValueConverter(this._view, this._implicitReceiver)
-      : super(_view.component.analyzedClass);
+  _ViewBoundValueConverter(this._view, o.Expression implicitReceiver)
+      : super(_view.component, implicitReceiver, _view.nameResolver);
 
   @override
-  o.Expression convertToExpression(
-    BoundValue value,
-    SourceSpan sourceSpan,
-    o.OutputType type,
-  ) {
-    if (value is BoundExpression) {
-      final expression =
-          analyzer.rewriteInterpolate(value.expression, _analyzedClass);
-      return convertCdExpressionToIr(
-        _view.nameResolver,
-        _implicitReceiver,
-        expression,
-        sourceSpan,
-        _view.component,
-        type,
-      );
-    } else if (value is BoundI18nMessage) {
-      return _view.createI18nMessage(value.message);
-    }
-    _throwUnrecognized(value);
-  }
+  o.Expression _createI18nMessage(I18nMessage message) =>
+      _view.createI18nMessage(message);
 }
