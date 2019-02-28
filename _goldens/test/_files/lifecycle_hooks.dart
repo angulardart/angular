d411301b6f4b3f5ef21811b43a2daaaa9a3a4f45
matanl <matanl@google.com>
2019-02-27 16:55:48 -0800

    test(Goldens): Add test cases for the remaining life-cycle events.
    We didn't have events that were ever called for:
    * AfterContent{Checked|Init}, AfterView{Checked|Init}
    * DoCheck
    
    We were actually missing:
    * OnPush, AfterChanges
    
    PiperOrigin-RevId: 236020747

--- _goldens/test/_files/lifecycle_hooks.dart
+++ _goldens/test/_files/lifecycle_hooks.dart
@@ -2,17 +2,27 @@ import 'package:angular/angular.dart';
 
 @Component(
   selector: 'lifecycle-hooks',
-  template: '',
+  directives: [
+    EmptyComponent,
+  ],
+  template: r'''
+    <empty-comp></empty-comp>
+    <ng-content></ng-content>
+  ''',
 )
-class LifecycleHooksComponent
+class MostLifecycleHooksComponent extends MostLifecycleHooksSuperclass {}
+
+class MostLifecycleHooksSuperclass
     implements
         OnDestroy,
         OnInit,
         AfterContentChecked,
         AfterContentInit,
         AfterViewChecked,
-        AfterViewInit,
-        DoCheck {
+        AfterViewInit {
+  @Input()
+  String input;
+
   @override
   ngAfterContentChecked() {}
 
@@ -26,11 +36,71 @@ class LifecycleHooksComponent
   ngAfterViewInit() {}
 
   @override
-  ngDoCheck() {}
-
-  @override
   ngOnDestroy() {}
 
   @override
   ngOnInit() {}
+}
+
+@Component(
+  selector: 'empty-comp',
+  template: '',
+)
+class EmptyComponent {}
+
+@Directive(
+  selector: 'do-check',
+)
+class DoCheckDirective implements DoCheck {
+  @override
+  void ngDoCheck() {}
+
+  @Input()
+  String input;
+}
+
+@Component(
+  selector: 'on-changes',
+  template: '',
+)
+class OnChangesComponent implements OnChanges {
+  @override
+  void ngOnChanges(_) {}
+
+  @Input()
+  String input;
+}
+
+@Component(
+  selector: 'after-changes',
+  template: '',
+)
+class AfterChangesComponent implements AfterChanges {
+  @override
+  void ngAfterChanges() {}
+
+  @Input()
+  String input;
+}
+
+@Component(
+  selector: 'uses-lifecylce-hooks',
+  directives: [
+    MostLifecycleHooksComponent,
+    EmptyComponent,
+    DoCheckDirective,
+    OnChangesComponent,
+    AfterChangesComponent,
+  ],
+  template: r'''
+    <lifecycle-hooks [input]="input">
+      <empty-comp></empty-comp>
+    </lifecycle-hooks>
+    <do-check [input]="input"></do-check>
+    <on-changes [input]="input"></on-changes>
+    <after-changes [input]="input"></after-changes>
+  ''',
+)
+class UsesLifecycleHooksComponent {
+  var input = 'Hello World';
 }
