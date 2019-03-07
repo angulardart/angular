fcef3dd910df8c23632b28c69a7653aa41a50518
Mike Fairhurst <mfairhurst@google.com>
2019-03-07 14:18:01 -0800

    Better documentation for FileTracker

--- angular_analyzer_plugin/lib/src/file_tracker.dart
+++ angular_analyzer_plugin/lib/src/file_tracker.dart
@@ -3,12 +3,30 @@ import 'dart:collection';
 import 'package:analyzer/src/summary/api_signature.dart';
 import 'package:angular_analyzer_plugin/src/options.dart';
 
+/// Hashing required for the [FileTracker] to be able to compute signatures.
 abstract class FileHasher {
   ApiSignature getContentHash(String path);
   ApiSignature getUnitElementHash(String path);
 }
 
+/// Compute signatures and relationships between dart summaries & html contents.
+///
+/// We can use the signatures to cache analysis results, just like the Dart
+/// driver does. We also want to be able to discover the relationships between
+/// Dart and HTML files so that when an HTML file is updated, we can efficiently
+/// reanalyze only the dart files that are affected.
+///
+/// Note that Dart files depend on HTML files when a Dart component imports
+/// another Dart component which may have `<ng-content>` tags in its template,
+/// which affect that secondary component's API.
+///
+/// Also note that the Dart analyzer package does not expose file dependency
+/// tracking. Currently, if a Dart file is altered, we reanalyze everything
+/// rather than just dependent files. In practice this works well because
+/// users don't do code completion with the plugin there and so latency there is
+/// less important, and avoiding a cache increases correctness guarantees.
 class FileTracker {
+  /// Define a salt to invalidate all hashes on new feature additions etc.
   static const int salt = 5;
 
   final FileHasher _fileHasher;
@@ -19,6 +37,7 @@ class FileTracker {
   final _dartToHtml = new _RelationshipTracker();
   final _dartFilesWithDartTemplates = new HashSet<String>();
 
+  /// Cache the hashes of files for quicker signature calculation.
   final contentHashes = <String, _FileHash>{};
 
   FileTracker(this._fileHasher, this._options);
@@ -42,6 +61,7 @@ class FileTracker {
     }
   }
 
+  /// Get a content signature for a [path].
   ApiSignature getContentSignature(String path) {
     if (contentHashes[path] == null) {
       rehashContents(path);
@@ -49,7 +69,10 @@ class FileTracker {
     return contentHashes[path].saltedSignature;
   }
 
-  // ignore: avoid_positional_boolean_parameters
+  /// Find Dart sources affected by an HTML change that need reanalysis.
+  ///
+  /// This is used to reanalyze the correct Dart files when HTML files are
+  /// updated.
   List<String> getDartPathsAffectedByHtml(String htmlPath) => _dartToHtml
       .getFilesReferencingFile(htmlPath)
       .map(_dartToDart.getFilesReferencingFile)
@@ -57,9 +80,17 @@ class FileTracker {
       .where(_dartFilesWithDartTemplates.contains)
       .toList();
 
+  /// Find Dart sources that refer directly to [htmlPath].
+  ///
+  /// This is used to be able to analyze [htmlPath], where we need to know which
+  /// components reference that [htmlPath].
   List<String> getDartPathsReferencingHtml(String htmlPath) =>
       _dartToHtml.getFilesReferencingFile(htmlPath);
 
+  /// Get the signature for a Dart file, including its Dart and HTML deps.
+  ///
+  /// This is used to cache analysis results for a Dart file, and must contain
+  /// hashes of all the information that contributed to that analysis.
   ApiSignature getDartSignature(String dartPath) {
     final signature = new ApiSignature()
       ..addInt(salt)
@@ -76,6 +107,15 @@ class FileTracker {
     return signature;
   }
 
+  /// Get the HTML files that affect a Dart file at [dartPath].
+  ///
+  /// This occurs when, for instance, a main component with an inline template
+  /// imports a secondary component with a templateUrl. That main component's
+  /// template string must be analyzed against the latest version of the
+  /// secondary HTML, which may define `<ng-content>`s that affect the result.
+  ///
+  /// This is used to know when Dart files have to be reanalyzed when an HTML
+  /// file is updated.
   List<String> getHtmlPathsAffectingDart(String dartPath) {
     if (_dartFilesWithDartTemplates.contains(dartPath)) {
       return getHtmlPathsAffectingDartContext(dartPath);
@@ -84,15 +124,30 @@ class FileTracker {
     return [];
   }
 
+  /// Get the HTML files that affect APIs of components defined in [dartPath].
+  ///
+  /// This is used to by [getDartSignature] and [getHtmlSignature] to ensure
+  /// that Dart result signatures include the signatures of all HTML files that
+  /// affected that result.
   List<String> getHtmlPathsAffectingDartContext(String dartPath) => _dartToDart
       .getFilesReferencedBy(dartPath)
       .map(_dartToHtml.getFilesReferencedBy)
       .fold<List<String>>(
           <String>[], (list, acc) => list..addAll(acc)).toList();
 
+  /// Get the HTML files that are directly referenced by Dart file [dartPath].
   List<String> getHtmlPathsReferencedByDart(String dartPath) =>
       _dartToHtml.getFilesReferencedBy(dartPath);
 
+  /// Get the HTML files that depend on the state of [htmlPath].
+  ///
+  /// This occurs when, for instance, a main component with a templateUrl
+  /// imports a secondary component with a templateUrl. That main component's
+  /// template HTML file must be analyzed against the latest version of the
+  /// secondary HTML, which may define `<ng-content>`s that affect the result.
+  ///
+  /// This is used to efficiently reanalyze the HTML files that need to be
+  /// reanalyzed when other HTML files are updated.
   List<String> getHtmlPathsReferencingHtml(String htmlPath) => _dartToHtml
       .getFilesReferencingFile(htmlPath)
       .map(_dartToDart.getFilesReferencingFile)
@@ -101,6 +156,10 @@ class FileTracker {
       .fold<List<String>>(<String>[], (list, acc) => list..addAll(acc))
       .toList();
 
+  /// Get the signature for an HTML file, including its Dart and HTML deps.
+  ///
+  /// This is used to cache analysis results for an HTML file, and must contain
+  /// hashes of all the information that contributed to that analysis.
   ApiSignature getHtmlSignature(String htmlPath) {
     final signature = new ApiSignature()
       ..addInt(salt)
@@ -120,10 +179,12 @@ class FileTracker {
     return signature;
   }
 
+  /// Get a salted unit element signature for a dart [path].
   ApiSignature getUnitElementSignature(String path) => new ApiSignature()
     ..addInt(salt)
     ..addBytes(_fileHasher.getUnitElementHash(path).toByteList());
 
+  /// Trigger a rehash of the contents at [path], which is then cached.
   void rehashContents(String path) {
     final signature = _fileHasher.getContentHash(path);
     final bytes = signature.toByteList();
@@ -134,6 +195,7 @@ class FileTracker {
           ..addBytes(bytes));
   }
 
+  /// Note that the latset version of [dartPath] has an inline template or not.
   void setDartHasTemplate(String dartPath, bool hasTemplate) {
     if (hasTemplate) {
       _dartFilesWithDartTemplates.add(dartPath);
@@ -142,9 +204,11 @@ class FileTracker {
     }
   }
 
+  /// Note that the latest version of [dartPath] refers to [htmlPaths].
   void setDartHtmlTemplates(String dartPath, List<String> htmlPaths) =>
       _dartToHtml.setFileReferencesFiles(dartPath, htmlPaths);
 
+  /// Note that the latest version of [dartPath] refers to [imports].
   void setDartImports(String dartPath, List<String> imports) {
     _dartToDart.setFileReferencesFiles(dartPath, imports);
   }
