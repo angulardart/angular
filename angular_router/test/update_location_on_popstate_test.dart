dc3cb068a4b74327ff26438e8d518688b246b22f
leonsenft <leonsenft@google.com>
2019-03-18 22:35:15 -0700

    test(Router): test when a "popstate" navigation should update the location
    These tests currently fail, as we incorrectly assumed that a navigation
    triggered by a location change (i.e. a "popstate" event) would never need to
    update the location.
    
    PiperOrigin-RevId: 239128492

---
+++ angular_router/test/update_location_on_popstate_test.dart
@@ -0,0 +1,92 @@
+@Skip('b/128861087')
+@TestOn('browser')
+import 'package:test/test.dart';
+import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:angular_router/testing.dart';
+import 'package:angular_test/angular_test.dart';
+
+import 'update_location_on_popstate_test.template.dart' as ng;
+
+void main() {
+  Location location;
+  MockLocationStrategy locationStrategy;
+  NgTestFixture<AppComponent> testFixture;
+
+  setUp(() async {
+    final testBed = NgTestBed.forComponent(
+      ng.AppComponentNgFactory,
+      rootInjector: createInjector,
+    );
+    testFixture = await testBed.create(beforeComponentCreated: (injector) {
+      location = injector.provideType(Location);
+      locationStrategy =
+          injector.provideType(LocationStrategy) as MockLocationStrategy;
+    });
+  });
+
+  tearDown(disposeAnyRunningTest);
+
+  test('redirect triggered by "popstate" should update location', () async {
+    await testFixture.update((_) {
+      locationStrategy.simulatePopState('/redirect-from');
+    });
+
+    expect(location.path(), '/redirect-to');
+  });
+
+  test('router hook triggered by "popstate" should update location', () async {
+    await testFixture.update((_) {
+      locationStrategy.simulatePopState('/rewrite-from');
+    });
+
+    expect(location.path(), '/rewrite-to');
+  });
+}
+
+const testModule = Module(
+  include: [routerTestModule],
+  provide: [ClassProvider(RouterHook, useClass: TestRouterHook)],
+);
+
+@GenerateInjector.fromModules([testModule])
+final createInjector = ng.createInjector$Injector;
+
+@Component(
+  selector: 'app',
+  directives: [RouterOutlet],
+  template: '''
+    <router-outlet [routes]="routes"></router-outlet>
+  ''',
+)
+class AppComponent {
+  final routes = [
+    RouteDefinition(
+      path: '',
+      component: ng.RouteComponentNgFactory,
+    ),
+    RouteDefinition.redirect(
+      path: '/redirect-from',
+      redirectTo: '/redirect-to',
+    ),
+    RouteDefinition(
+      path: '/redirect-to',
+      component: ng.RouteComponentNgFactory,
+    ),
+    RouteDefinition(
+      path: '/rewrite-to',
+      component: ng.RouteComponentNgFactory,
+    ),
+  ];
+}
+
+@Component(
+  selector: 'route',
+  template: '',
+)
+class RouteComponent {}
+
+class TestRouterHook extends RouterHook {
+  Future<String> navigationPath(String path, NavigationParams params) =>
+      Future.value(path == '/rewrite-from' ? 'rewrite-to' : path);
+}
