364470b1b43d69bfce13444f5563ef3f3eae1f3f
Mike Fairhurst <mfairhurst@google.com>
2019-04-02 17:03:03 -0700

    Roll selector parse error
    Closes #1749
    
    PiperOrigin-RevId: 241633529

---
+++ angular_analyzer_plugin/lib/src/selector/parse_error.dart
@@ -0,0 +1,26 @@
+import 'package:meta/meta.dart';
+
+/// Mixin used by parser and tokenizer to report [SelectorParseError]s.
+mixin ReportParseErrors {
+  String get str;
+
+  /// Report that the [actual] string in a [Selector] didn't match [expected].
+  void expected(String expected,
+      {@required String actual, @required int offset}) {
+    throw new SelectorParseError(
+        "Expected $expected, got $actual", str, offset, actual.length);
+  }
+
+  /// Report an unexpected string [string] at [offset].
+  void unexpected(String string, int offset) {
+    throw new SelectorParseError(
+        "Unexpected $string", str, offset, string.length);
+  }
+}
+
+/// A type of [FormatException] specific to errors parsing [Selector]s.
+class SelectorParseError extends FormatException {
+  int length;
+  SelectorParseError(String message, String source, int offset, this.length)
+      : super(message, source, offset);
+}
