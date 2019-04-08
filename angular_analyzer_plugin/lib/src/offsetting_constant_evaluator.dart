900d7ed87361007e221a7da19c96a8547f2745ee
Mike Fairhurst <mfairhurst@google.com>
2019-04-08 11:42:37 -0700

    Roll offsetting constant evaluator
    Closes #1775
    
    PiperOrigin-RevId: 242505230

---
+++ angular_analyzer_plugin/lib/src/offsetting_constant_evaluator.dart
@@ -0,0 +1,148 @@
+import 'package:analyzer/dart/ast/ast.dart' as ast;
+import 'package:analyzer/dart/ast/token.dart';
+import 'package:analyzer/src/dart/ast/utilities.dart' as ast_utils;
+
+/// An AST-based constant evaluator that inserts whitespace to preserve offsets.
+///
+/// Evaluates an AST via dart constant semantics, inserting whitespace so that
+/// the result can be analyzed and offsets in that result match the offsets of
+/// the code.
+///
+/// Example:
+///   input:  'adjacent' 'strings' + 'with' + 'addition'
+///   output: 'adjacent   strings     with     addition'
+///
+/// Now we can highlight any arbitrary source range in the output as erroneous,
+/// and it will match the offset of what the user wrote.
+///
+/// Note that AST-evaluation is incomplete. Users may reference variables from
+/// the file or even other files. True constant evaluation therefore requires
+/// a resolved AST.
+///
+/// Example:
+///   const before = 'foo';
+///   ...
+///   ... template: after + 'and $before' * 3;
+///   ...
+///   const after = 'bar';
+///
+/// In this case we report [offsetsAreValid] is false at the end of evaluation.
+/// When false, it means we could not cleanly map the constant value to a string
+/// for error reporting. (in the case above, it is impossible, as the beginning
+/// of the string would have to come at the end and vice versa).
+///
+/// In theory we could produce a mapping that handles these harder cases,
+/// however, it is enough to report to users that they will not get IDE support
+/// for their design.
+class OffsettingConstantEvaluator extends ast_utils.ConstantEvaluator {
+  bool offsetsAreValid = true;
+  Object value;
+  ast.AstNode lastUnoffsettableNode;
+
+  @override
+  Object visitAdjacentStrings(ast.AdjacentStrings node) {
+    final buffer = new StringBuffer();
+    int lastEndingOffset;
+    for (final string in node.strings) {
+      final value = string.accept(this);
+      if (identical(value, ast_utils.ConstantEvaluator.NOT_A_CONSTANT)) {
+        return value;
+      }
+      // preserve offsets across the split by padding
+      if (lastEndingOffset != null) {
+        buffer.write(' ' * (string.offset - lastEndingOffset));
+      }
+      lastEndingOffset = string.offset + string.length;
+      buffer.write(value);
+    }
+    return buffer.toString();
+  }
+
+  @override
+  Object visitBinaryExpression(ast.BinaryExpression node) {
+    if (node.operator.type == TokenType.PLUS) {
+      // ignore: omit_local_variable_types
+      final Object leftOperand = node.leftOperand.accept(this);
+      if (identical(leftOperand, ast_utils.ConstantEvaluator.NOT_A_CONSTANT)) {
+        return leftOperand;
+      }
+      // ignore: omit_local_variable_types
+      final Object rightOperand = node.rightOperand.accept(this);
+      if (identical(rightOperand, ast_utils.ConstantEvaluator.NOT_A_CONSTANT)) {
+        return rightOperand;
+      }
+      // numeric or {@code null}
+      if (leftOperand is String && rightOperand is String) {
+        final gap = node.rightOperand.offset -
+            node.leftOperand.offset -
+            node.leftOperand.length;
+        // ignore: prefer_interpolation_to_compose_strings
+        return leftOperand + (' ' * gap) + rightOperand;
+      }
+    }
+
+    return super.visitBinaryExpression(node);
+  }
+
+  @override
+  Object visitMethodInvocation(ast.MethodInvocation node) {
+    offsetsAreValid = false;
+    lastUnoffsettableNode = node;
+    return super.visitMethodInvocation(node);
+  }
+
+  @override
+  Object visitParenthesizedExpression(ast.ParenthesizedExpression node) {
+    offsetsAreValid = false;
+    lastUnoffsettableNode = node;
+    final preGap = node.expression.offset - node.offset;
+    final postGap = node.offset +
+        node.length -
+        node.expression.offset -
+        node.expression.length;
+    // ignore: omit_local_variable_types
+    final Object value = super.visitParenthesizedExpression(node);
+    if (value is String) {
+      // ignore: prefer_interpolation_to_compose_strings
+      return ' ' * preGap + value + ' ' * postGap;
+    }
+
+    return value;
+  }
+
+  @override
+  Object visitPrefixedIdentifier(ast.PrefixedIdentifier node) {
+    offsetsAreValid = false;
+    lastUnoffsettableNode = node;
+    return super.visitPrefixedIdentifier(node);
+  }
+
+  @override
+  Object visitPropertyAccess(ast.PropertyAccess node) {
+    offsetsAreValid = false;
+    lastUnoffsettableNode = node;
+    return super.visitPropertyAccess(node);
+  }
+
+  @override
+  Object visitSimpleIdentifier(ast.SimpleIdentifier node) {
+    offsetsAreValid = false;
+    lastUnoffsettableNode = node;
+    return super.visitSimpleIdentifier(node);
+  }
+
+  @override
+  Object visitSimpleStringLiteral(ast.SimpleStringLiteral node) {
+    final gap = node.contentsOffset - node.offset;
+    lastUnoffsettableNode = node;
+    // ignore: prefer_interpolation_to_compose_strings
+    return ' ' * gap + node.value + ' ';
+  }
+
+  @override
+  Object visitStringInterpolation(ast.StringInterpolation node) {
+    offsetsAreValid = false;
+    lastUnoffsettableNode = node;
+    return super.visitStringInterpolation(node);
+  }
+}
---
+++ angular_analyzer_plugin/test/offsetting_constant_evaluator_test.dart
@@ -0,0 +1,169 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
+import 'package:analyzer/error/listener.dart';
+import 'package:analyzer/src/dart/ast/utilities.dart' as utils;
+import 'package:analyzer/src/dart/scanner/reader.dart';
+import 'package:analyzer/src/dart/scanner/scanner.dart';
+import 'package:analyzer/src/generated/parser.dart';
+import 'package:analyzer/src/source/source_resource.dart' show FileSource;
+import 'package:angular_analyzer_plugin/src/offsetting_constant_evaluator.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+void main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(OffsettingConstantValueVisitorTest);
+  });
+}
+
+@reflectiveTest
+class OffsettingConstantValueVisitorTest {
+  // ignore: non_constant_identifier_names
+  void assertCaretOffsetIsPreserved(String code) {
+    final pos = code.indexOf('^');
+    expect(pos, greaterThan(-1), reason: 'the code should contain a caret');
+
+    final expression = _parseDartExpression(code);
+
+    final evaluator = new OffsettingConstantEvaluator();
+    final value = expression.accept(evaluator);
+
+    if (value is String) {
+      expect(value.indexOf('^'), equals(pos),
+          reason: "```$value``` moved the caret");
+    } else {
+      fail("Expected string, got $value");
+    }
+  }
+
+  // ignore: non_constant_identifier_names
+  void assertNotOffsettable(String code, {String at}) {
+    final expression = _parseDartExpression(code);
+    final pos = code.indexOf(at);
+    final length = at.length;
+    expect(pos, greaterThan(-1),
+        reason: "```$code```` doesn't contain ```$at```");
+
+    final evaluator = new OffsettingConstantEvaluator();
+    expression.accept(evaluator);
+    expect(evaluator.offsetsAreValid, isFalse);
+    expect(evaluator.lastUnoffsettableNode, isNotNull);
+    expect(evaluator.lastUnoffsettableNode.offset, equals(pos),
+        reason: "The snippet didn't match the suspect node");
+    expect(evaluator.lastUnoffsettableNode.length, equals(length),
+        reason: "The snippet didn't match the suspect node");
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_adjacentStrings() {
+    assertCaretOffsetIsPreserved("'my template^' 'which continues'");
+    assertCaretOffsetIsPreserved("'my template' 'which continues ^'");
+    assertCaretOffsetIsPreserved("r'my template'    r'which continues ^'");
+    assertCaretOffsetIsPreserved("'my template'\n       'which continues ^'");
+    assertCaretOffsetIsPreserved("'no gap''then continue ^'");
+    assertCaretOffsetIsPreserved("'' 'after empty string ^'");
+    assertCaretOffsetIsPreserved(
+        "'my template'\n\n       'which continues' ' and continues ^'");
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_computedStringsLookRight() {
+    final expression =
+        _parseDartExpression("('my template'\n) + 'which continues^'");
+    final value = expression.accept(new OffsettingConstantEvaluator());
+    expect(value, equals("  my template       which continues^ "));
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_concatenatedAfterParenthesis() {
+    assertCaretOffsetIsPreserved("('my template^') + 'which continues'");
+    assertCaretOffsetIsPreserved("('my template') + 'which continues^'");
+    assertCaretOffsetIsPreserved("('my template'  ) + 'which continues^'");
+    assertCaretOffsetIsPreserved("('my template'\n) + 'which continues^'");
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_concatenatedStrings() {
+    assertCaretOffsetIsPreserved("'my template^' + 'which continues'");
+    assertCaretOffsetIsPreserved("'my template' + 'which continues ^'");
+    assertCaretOffsetIsPreserved("r'my template' +    r'which continues ^'");
+    assertCaretOffsetIsPreserved("'my template' +\n       'which continues ^'");
+    assertCaretOffsetIsPreserved("'no gap'+'then continue ^'");
+    assertCaretOffsetIsPreserved("'' + 'after empty string ^'");
+    assertCaretOffsetIsPreserved(
+        "'my template' +\n\n       'which continues' + ' and continues ^'");
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_error() {
+    final expression = _parseDartExpression("1 + 'hello'");
+    final value = expression.accept(new OffsettingConstantEvaluator());
+    expect(value, equals(utils.ConstantEvaluator.NOT_A_CONSTANT));
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_notOffsettableGetter() {
+    assertNotOffsettable(r"'hello' + world ", at: 'world');
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_notOffsettableInterp() {
+    assertNotOffsettable(r"'hello $world'", at: 'world');
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_notOffsettableInterpExpr() {
+    assertNotOffsettable(r"'hello ${world}'", at: 'world');
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_notOffsettableMethod() {
+    assertNotOffsettable(r"'hello' + method() ", at: 'method()');
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_notOffsettablePrefixedIdent() {
+    assertNotOffsettable(r"'hello' + prefixed.identifier ",
+        at: 'prefixed.identifier');
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_notStringComputation() {
+    final expression = _parseDartExpression("1 + 2");
+    final value = expression.accept(new OffsettingConstantEvaluator());
+    expect(value, equals(3));
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_parenthesizedString() {
+    assertCaretOffsetIsPreserved("('my template^')");
+    assertCaretOffsetIsPreserved("( 'my template^')");
+    assertCaretOffsetIsPreserved("(  'my template^')");
+    assertCaretOffsetIsPreserved("(\n'my template^')");
+  }
+
+  // ignore: non_constant_identifier_names
+  void test_simpleString() {
+    assertCaretOffsetIsPreserved("'my template^'");
+    assertCaretOffsetIsPreserved("r'my template^'");
+    assertCaretOffsetIsPreserved('"my template^"');
+    assertCaretOffsetIsPreserved('r"my template^"');
+    assertCaretOffsetIsPreserved("'''my template^'''");
+    assertCaretOffsetIsPreserved("r'''my template^'''");
+  }
+
+  Expression _parseDartExpression(String code) {
+    final token = _scanDartCode(code);
+    final parser = new Parser(new _MockSource(), new BooleanErrorListener());
+    return parser.parseExpression(token);
+  }
+
+  Token _scanDartCode(String code) {
+    final reader = new CharSequenceReader(code);
+    final scanner = new Scanner(null, reader, null);
+    return scanner.tokenize();
+  }
+}
+
+class _MockSource extends Mock implements FileSource {}
