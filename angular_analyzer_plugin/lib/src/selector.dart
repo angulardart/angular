9d1a4f62daef544ae02abcd3500f27cac637cb19
Mike Fairhurst <mfairhurst@google.com>
2019-04-09 14:56:05 -0700

    Roll selector parser & tests
    Closes #1777
    
    PiperOrigin-RevId: 242745835

---
+++ angular_analyzer_plugin/lib/src/selector.dart
@@ -0,0 +1,8 @@
+/// Most commonly used behavior for CSS selectors in the angular plugin.
+export 'package:angular_analyzer_plugin/src/selector/element_view.dart';
+export 'package:angular_analyzer_plugin/src/selector/parse_error.dart'
+    show SelectorParseError;
+export 'package:angular_analyzer_plugin/src/selector/parser.dart';
+export 'package:angular_analyzer_plugin/src/selector/selector.dart';
+export 'package:angular_analyzer_plugin/src/selector/match.dart';
+export 'package:angular_analyzer_plugin/src/selector/name.dart';
