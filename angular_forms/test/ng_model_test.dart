19be73c75623367c7c52ef38f54954c083d51848
matanl <matanl@google.com>
2019-04-10 13:38:41 -0700

    test(Forms): Add an example test of a badly behaving ControlValueAccessor.
    **NOTE: This test passes at HEAD due to the bug described.**
    
    Today, NgModel uses ComponentState, which silently does not observe that values "have not changed since last check". This allowed users to implement their own ControlValueAccessor that intercepted "writeValue", and either rejected or transformed the value, writing it back via "registerOnChange".
    
    Ironically, this is what "Expression has changed ..." was supposed to fix, unidirectional synchronous data flow, but it is violated due to a couple of reasons:
    
    1. ComponentState hid the few errors we do throw now (omitted checkBinding).
    
    2. NgModel uses StreamController(sync: true). This was meant to be used for performance only for "bridging APIs" (i.e. change functions that are already asynchronous, like a native `<input>`). In many cases, users do not have asynchronous actions though.
    
    3. ControlValueAccessor does not document or enforce any behavior of its own around proper implementation of writeValue (or whether it is valid to reject or transform on writeValue, for example).
    
    For now, I'd like to check this test in, knowing it will fail if I change NgModel without somehow accounting for this use case. It's possible we just want to go in and update client code.
    
    PiperOrigin-RevId: 242931435

--- angular_forms/test/ng_model_test.dart
+++ angular_forms/test/ng_model_test.dart
@@ -7,15 +7,13 @@ import 'package:angular_test/angular_test.dart';
 import 'ng_model_test.template.dart' as ng;
 
 void main() {
-  ng.initReflector();
+  tearDown(disposeAnyRunningTest);
 
   group('NgModelTest', () {
     NgTestFixture<NgModelTest> fixture;
 
-    tearDown(() => disposeAnyRunningTest());
-
     setUp(() async {
-      var testBed = NgTestBed.forComponent(ng.NgModelTestNgFactory);
+      final testBed = NgTestBed.forComponent(ng.NgModelTestNgFactory);
       fixture = await testBed.create();
     });
 
@@ -31,6 +29,7 @@ void main() {
         expect(cmp.ngModel.untouched, control.untouched);
       });
     });
+
     test('should set up validator', () async {
       await fixture.update((cmp) {
         expect(cmp.ngModel.errors, {'required': true});
@@ -41,6 +40,24 @@ void main() {
       });
     });
   });
+
+  // The significance of this test is that String is a checkBinding violation.
+  //
+  // See b/128999811.
+  test('should not violate the checkBinding contract', () async {
+    final testBed = NgTestBed.forComponent(
+      ng.NgModelWithCheckBindingTestNgFactory,
+    );
+
+    final fixture = await testBed.create();
+    expect(fixture.assertOnlyInstance.value, isNull);
+
+    expect(
+      fixture.update((comp) => comp.value = 'Hello'),
+      completes,
+      reason: 'Should not throw due to checkBinding',
+    );
+  });
 }
 
 @Component(
@@ -49,14 +66,64 @@ void main() {
     formDirectives,
   ],
   template: '''
-<div ngForm>
-  <input [(ngModel)]="loginValue" #login="ngForm" required />
-</div>
-''',
+    <div ngForm>
+      <input [(ngModel)]="loginValue" #login="ngForm" required />
+    </div>
+  ''',
 )
 class NgModelTest {
   @ViewChild('login')
   NgModel ngModel;
 
   String loginValue;
+}
+
+@Component(
+  selector: 'test',
+  directives: [
+    CustomEditorWithNgModelSupport,
+    NgModel,
+  ],
+  template: '''
+    <custom-editor-with-ng-model [(ngModel)]="value">
+    </custom-editor-with-ng-model>
+  ''',
+)
+class NgModelWithCheckBindingTest {
+  String value;
+}
+
+@Component(
+  selector: 'custom-editor-with-ng-model',
+  template: '',
+)
+class CustomEditorWithNgModelSupport implements ControlValueAccessor<String> {
+  final NgControl _ngControl;
+  ChangeFunction<String> _onChange;
+
+  CustomEditorWithNgModelSupport(this._ngControl) {
+    _ngControl.valueAccessor = this;
+  }
+
+  @override
+  void writeValue(String value) {
+    // Example of bad behavior. We are, synchronously, receiving a write coming
+    // from ngModel.model=, and synchronously, responding back to ngModel, which
+    // in turn, synchronously, talks back via StreamController(sync: true).
+    if (value == 'Hello') {
+      value = 'Goodbye';
+      _onChange(value);
+    }
+  }
+
+  @override
+  void onDisabledChanged(_) {}
+
+  @override
+  void registerOnChange(ChangeFunction<String> function) {
+    _onChange = function;
+  }
+
+  @override
+  void registerOnTouched(_) {}
 }
