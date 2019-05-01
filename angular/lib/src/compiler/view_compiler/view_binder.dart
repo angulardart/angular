f5d83121dcf8b7408b3b5c2efe8932408153320a
alorenzen <alorenzen@google.com>
2019-05-01 11:14:40 -0700

    refactor(bound_value_converter): remove constructor param for implicitReceiver.
    All of the usages were supplying the exact same value, so we can just hard code it here.
    
    PiperOrigin-RevId: 246163987

--- angular/lib/src/compiler/view_compiler/view_binder.dart
+++ angular/lib/src/compiler/view_compiler/view_binder.dart
@@ -7,7 +7,6 @@ import 'package:angular/src/compiler/schema/element_schema_registry.dart';
 import 'package:angular/src/compiler/semantic_analysis/binding_converter.dart';
 import 'package:angular/src/compiler/template_ast.dart';
 import 'package:angular/src/compiler/template_parser.dart';
-import 'package:angular/src/compiler/view_compiler/constants.dart';
 import 'package:angular/src/core/linker/view_type.dart';
 import 'package:angular_compiler/cli.dart' show logWarning, throwFailure;
 
@@ -242,8 +241,7 @@ void _bindViewHostProperties(
   final method = CompileMethod();
   final compileElement = view.componentView.declarationElement;
   final renderNode = view.componentView.declarationElement.renderNode;
-  final implicitReceiver = DetectChangesVars.cachedCtx;
-  final converter = BoundValueConverter.forView(view, implicitReceiver);
+  final converter = BoundValueConverter.forView(view);
   bindAndWriteToRenderer(
     convertAllToBinding(hostProperties,
         analyzedClass: view.component.analyzedClass),
