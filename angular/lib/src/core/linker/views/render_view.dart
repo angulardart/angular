e4ecd9322e33934e47e239a754202940cb9c5a3b
leonsenft <leonsenft@google.com>
2019-05-10 13:53:58 -0700

    refactor(Runtime): split `AppView` into distinct base view types
    In addition to refining the existing base view types `View`, `DynamicView` and
    `RenderView`, this introduces the three view types from which all generated
    views will directly extend:
    
      * ComponentView
    
        Implements a component's template. This may consists of multiple nested
        component and embedded views.
    
      * EmbeddedView
    
        Implements a <template> element. Can be created, moved, and destroyed
        dynamically. Inherits much of its state from its parent view (which is
        always a component or embedded view).
    
      * HostView
    
        Hosts a component view so that a component can be created, moved, and
        destroyed dynamically. Unlike the other two view types, this type of view
        never actually creates or directly owns any of the DOM.
    
    All of the view types take part in dependency injection and change detection to
    varying degrees.
    
    Note this change doesn't yet replace `AppView`, but merely introduces its
    replacements (unused) for review. Once landed, a subsequent commit will
    introduce the necessary changes to wire these types into the generated code and
    have the compiler produce them.
    
    Note there's still a decent amount of duplication between views (particularly in
    the _*Data classes), but the hope is to largely reduce this with subsequent
    refactors mentioned in the inlined TODOs. That said, this refactor still
    *reduces* overall code size for medium to large apps, at the expense of a small
    amount of overhead which may increase code size for smaller apps (tested on the
    following change which integrates these new types within the framework). Much of
    the duplication in the hand-written code will allow for reduced duplication in
    the generated code, which should ultimately yield a net code size reduction.
    
    PiperOrigin-RevId: 247670142

--- angular/lib/src/core/linker/views/render_view.dart
+++ angular/lib/src/core/linker/views/render_view.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:html';
 
 import 'package:meta/dart2js.dart' as dart2js;
@@ -5,12 +6,39 @@ import 'package:angular/src/core/linker/app_view_utils.dart';
 import 'package:angular/src/core/linker/style_encapsulation.dart';
 import 'package:angular/src/core/linker/view_container.dart';
 import 'package:angular/src/core/linker/view_fragment.dart';
+import 'package:angular/src/di/injector/injector.dart';
 import 'package:angular/src/runtime.dart';
 import 'package:angular/src/runtime/dom_helpers.dart';
 
 import 'view.dart';
 
-/// A view that renders DOM content.
+/// A view that renders a portion of a component template.
+///
+/// The responsibilities of this view include:
+///
+///   * Rendering the DOM defined in the template. This includes applying
+///   encapsulation classes if the component uses style encapsulation.
+///
+///   * Initializing each directive used in the template.
+///
+///   * Initializing each component used in the template, and its associated
+///   component view.
+///
+///   * Making any services provided by the directives and components in this
+///   view available for injection to their respective host and descendants.
+///
+///   * Updating any expression bindings that changed in the template during
+///   change detection, and invoking change detection on nested views.
+///
+///   * Invoking any life cycle interfaces implemented by components in this
+///   view at the appropriate times.
+///
+///   * Attaching any projected content that matches an `<ng-content>` slot
+///   declared the template at its corresponding location (the `<ng-content>`
+///   tag itself is never rendered).
+///
+/// Note that generated views should never extend this class directly, but
+/// rather one of its specializations.
 abstract class RenderView extends View {
   /// The context in which expressions bound in this view are evaluated.
   ///
@@ -20,19 +48,8 @@ abstract class RenderView extends View {
   /// dramatically reduces code size).
   Object get ctx;
 
-  /// This view's CSS styles.
+  /// This view's compiled CSS styles and style encapsulation information.
   ComponentStyles get componentStyles;
-
-  /// Returns whether this is the first change detection pass.
-  // TODO(b/129479956): remove after directive change detectors are removed.
-  bool get firstCheck;
-
-  /// The index of this view within its [parentView].
-  int get parentIndex;
-
-  /// This view's parent view.
-  // TODO(b/130375490): a component view's parent shouldn't be a `RenderView`.
-  RenderView get parentView;
 
   /// Nodes that are given to this view by a parent view via content projection.
   ///
@@ -41,56 +58,7 @@ abstract class RenderView extends View {
   /// itself but rather by the view's parent.
   List<Object> get projectedNodes;
 
-  /// Equivalent to [addShimE], but optimized for [HtmlElement].
-  @dart2js.noInline
-  void addShimC(HtmlElement element) {
-    final styles = componentStyles;
-    if (styles.usesStyleEncapsulation) {
-      updateClassBinding(element, styles.contentPrefix, true);
-    }
-  }
-
-  /// Adds a content shim class to [element].
-  ///
-  /// This is only used if [componentStyles] are encapsulated, as the content
-  /// shim class is needed for any styles to match [element].
-  ///
-  /// This should only be used for SVG or custom elements. For a plain
-  /// [HtmlElement], use [addShimC] instead.
-  @dart2js.noInline
-  void addShimE(Element element) {
-    final styles = componentStyles;
-    if (styles.usesStyleEncapsulation) {
-      updateClassBindingNonHtml(element, styles.contentPrefix, true);
-    }
-  }
-
-  void Function(E) eventHandler0<E>(void Function() handler) {
-    return (E event) {
-      markForCheck();
-      appViewUtils.eventManager.zone.runGuarded(handler);
-    };
-  }
-
-  // When registering an event listener for a native DOM event, the return value
-  // of this method is passed to EventTarget.addEventListener() which expects a
-  // function that accepts an Event parameter. This means you can't directly
-  // register an event listener for a specific subclass of Event, such as a
-  // MouseEvent for the 'click' event. A workaround is possible by ensuring the
-  // parameter of the event listener is a subclass of Event. The Event passed in
-  // from EventTarget.addEventListener() can then be safely coerced back to its
-  // known type.
-  void Function(E) eventHandler1<E, F extends E>(void Function(F) handler) {
-    assert(
-        E == Null || F != Null,
-        "Event handler '$handler' isn't assignable to expected type "
-        "'($E) => void'");
-    return (E event) {
-      markForCheck();
-      appViewUtils.eventManager.zone
-          .runGuarded(() => handler(unsafeCast<F>(event)));
-    };
-  }
+  // Initialization ------------------------------------------------------------
 
   /// Moves (appends) appropriate DOM [Node]s of [ViewData.projectedNodes].
   ///
@@ -99,19 +67,19 @@ abstract class RenderView extends View {
   /// what parts of the DOM.
   @dart2js.noInline
   void project(Element target, int index) {
-    // TODO: Determine in what case this is `null`.
+    // TODO(b/132111830): Determine in what case this is `null`.
     if (target == null) {
       return;
     }
 
-    // TODO: Determine why this would be `null` or out of bounds.
+    // TODO(b/132111830): Determine why this would be `null` or out of bounds.
     final projectedNodesByContentIndex = projectedNodes;
     if (projectedNodesByContentIndex == null ||
         index >= projectedNodesByContentIndex.length) {
       return;
     }
 
-    // TODO: Also determine why this might be `null`.
+    // TODO(b/132111830): Also determine why this might be `null`.
     final nodesToProjectIntoTarget = unsafeCast<List<Object>>(
       projectedNodesByContentIndex[index],
     );
@@ -144,14 +112,137 @@ abstract class RenderView extends View {
     domRootRendererIsDirty = true;
   }
 
+  // Dependency injection ------------------------------------------------------
+
+  @override
+  Object injectorGetViewInternal(
+    Object token,
+    int nodeIndex, [
+    Object notFoundValue = throwIfNotFound,
+  ]) {
+    // Inject from view.
+    final result = injectorGetInternal(token, nodeIndex, View.providerNotFound);
+    // Return result if found.
+    if (!identical(result, View.providerNotFound)) {
+      return result;
+    }
+    // Otherwise inject from ancestry.
+    return parentView.injectorGetViewInternal(
+        token, parentIndex, notFoundValue);
+  }
+
+  // Change detection ----------------------------------------------------------
+
+  /// Wraps [handler] so that when invoked, this view gets change detected.
+  ///
+  /// The wrapped handler does two things:
+  ///
+  ///   * Ensures the original [handler] is executed inside the Angular zone so
+  ///   that a change detection cycle is triggered. This also ensures that any
+  ///   asynchronous work scheduled by [handler] is also run inside the Angular
+  ///   zone, triggering subsequent change detection cycles.
+  ///
+  ///   * Calls [markForCheck] on this view to ensure it gets change detected
+  ///   during the next change detection cycle, in case it uses a non-default
+  ///   change detection strategy.
+  void Function(E) eventHandler0<E>(void Function() handler) {
+    return (E event) {
+      markForCheck();
+      appViewUtils.eventManager.zone.runGuarded(handler);
+    };
+  }
+
+  /// The same as [eventHandler0], but [handler] is passed an event parameter.
+  ///
+  /// This is used anytime the user binds an event listener with an [Event]
+  /// parameter, or references the `$event` variable inside an event handler in
+  /// a template.
+  ///
+  /// When registering an event listener for a native DOM event, the return
+  /// value of this method is passed to [EventTarget.addEventListener] which
+  /// expects an [EventListener]. This means you can't directly register an
+  /// event listener for a specific subclass of [Event], such as a [MouseEvent]
+  /// for the 'click' event. A workaround is possible by ensuring the parameter
+  /// of the event listener is a subclass of [Event]. The [Event] passed in from
+  /// [EventTarget.addEventListener] can then be safely coerced back to its
+  /// known type.
+  void Function(E) eventHandler1<E, F extends E>(void Function(F) handler) {
+    assert(
+        E == Null || F != Null,
+        "Event handler '$handler' isn't assignable to expected type "
+        "'($E) => void'");
+    return (E event) {
+      markForCheck();
+      appViewUtils.eventManager.zone
+          .runGuarded(() => handler(unsafeCast<F>(event)));
+    };
+  }
+
+  // Styling -------------------------------------------------------------------
+
+  /// Equivalent to [addShimE], but optimized for [HtmlElement].
+  @dart2js.noInline
+  void addShimC(HtmlElement element) {
+    final styles = componentStyles;
+    if (styles.usesStyleEncapsulation) {
+      updateClassBinding(element, styles.contentPrefix, true);
+    }
+  }
+
+  /// Adds a content shim class to [element].
+  ///
+  /// This is only used if [componentStyles] are encapsulated, as the content
+  /// shim class is needed for any styles to match [element].
+  ///
+  /// This should only be used for SVG or custom elements. For a plain
+  /// [HtmlElement], use [addShimC] instead.
+  @dart2js.noInline
+  void addShimE(Element element) {
+    final styles = componentStyles;
+    if (styles.usesStyleEncapsulation) {
+      updateClassBindingNonHtml(element, styles.contentPrefix, true);
+    }
+  }
+
   /// Called by change detector to apply correct host and content shimming
   /// after node's className is changed.
   ///
   /// Used by [detectChanges] when changing [element.className] directly.
   ///
   /// For example, through the `[class]="..."` or `[attr.class]="..."` syntax.
-  void updateChildClass(HtmlElement element, String newClass);
+  @dart2js.noInline
+  void updateChildClass(HtmlElement element, String newClass) {
+    final styles = componentStyles;
+    final shim = styles.usesStyleEncapsulation;
+    element.className = shim ? '$newClass ${styles.contentPrefix}' : newClass;
+  }
 
   /// Similar to [updateChildClass], for an [element] not guaranteed to be HTML.
-  void updateChildClassNonHtml(Element element, String newClass);
+  @dart2js.noInline
+  void updateChildClassNonHtml(Element element, String newClass) {
+    final styles = componentStyles;
+    final shim = styles.usesStyleEncapsulation;
+    updateAttribute(element, 'class',
+        shim ? '$newClass ${styles.contentPrefix}' : newClass);
+  }
+}
+
+/// The interface for [RenderView] data bundled together as an optimization.
+///
+/// Similar to [ViewData], this interface exists solely as a common point for
+/// documentation.
+abstract class RenderViewData implements ViewData {
+  /// Storage for [View.parentView].
+  View get parentView;
+
+  /// Storage for [View.parentIndex].
+  int get parentIndex;
+
+  /// Storage for [RenderView.projectedNodes].
+  List<Object> get projectedNodes;
+
+  /// Storage for subscriptions to any outputs in this view.
+  ///
+  /// These are cancelled when this is [destroyed].
+  List<StreamSubscription<void>> get subscriptions;
 }
