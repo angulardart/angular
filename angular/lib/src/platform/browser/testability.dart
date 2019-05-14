40a1b790d051e9200ca5f051ad738fcfccf7a77a
srawlins <srawlins@google.com>
2019-05-13 22:11:52 -0700

    Fix a few more strict raw types in Angular.
    PiperOrigin-RevId: 248073832

--- angular/lib/src/platform/browser/testability.dart
+++ angular/lib/src/platform/browser/testability.dart
@@ -21,7 +21,7 @@ class BrowserGetTestability implements GetTestability {
       js_util.setProperty(_self, 'ngTestabilityRegistries', jsRegistry = []);
       js_util.setProperty(_self, 'getAngularTestability',
           allowInterop((Element elem, [bool findInAncestors = true]) {
-        List registry =
+        List<dynamic> registry =
             unsafeCast(js_util.getProperty(_self, 'ngTestabilityRegistries'));
         for (int i = 0; i < registry.length; i++) {
           var result =
@@ -31,7 +31,7 @@ class BrowserGetTestability implements GetTestability {
         throw StateError('Could not find testability for element.');
       }));
       var getAllAngularTestabilities = () {
-        List registry =
+        List<dynamic> registry =
             unsafeCast(js_util.getProperty(_self, 'ngTestabilityRegistries'));
         var result = [];
         for (int i = 0; i < registry.length; i++) {
