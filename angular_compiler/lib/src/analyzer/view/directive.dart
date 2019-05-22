1b333f7261daf9d0e4279576f61fea37806df6d7
matanl <matanl@google.com>
2019-05-22 11:21:46 -0700

    fix(Compiler): Emit eager error messages on invalid @HostListener[s].
    Prior to this, we relied on DDC/Dart2JS/Analyzer for determining the code we emitted was invalid (for example, if more arguments were specified than parameters for a method). This change makes it an eager compile-time error from Angular.
    
    All of the tests for `invalid_host_annotations_test.dart` are now enabled!
    
    PiperOrigin-RevId: 249485153

--- angular_compiler/lib/src/analyzer/view/directive.dart
+++ angular_compiler/lib/src/analyzer/view/directive.dart
@@ -1,6 +1,7 @@
 import 'package:analyzer/dart/constant/value.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:meta/meta.dart';
+import 'package:source_gen/source_gen.dart';
 
 import '../../../cli.dart';
 import '../types.dart';
@@ -66,6 +67,25 @@ class DirectiveVisitor {
     BuildError.throwForElement(element, message);
   }
 
+  static bool _isRequired(ParameterElement e) => e.isRequiredPositional;
+
+  static void _assertMaxArgs(Element element, String message, int maxArgs) {
+    // TODO(b/133248314): Re-enable or delete this case.
+    /*
+    if (element is MethodElement &&
+        element.parameters.where(_isRequired).length > maxArgs) {
+      BuildError.throwForElement(element, message);
+    }
+    */
+  }
+
+  static void _assertExactArgs(Element element, String message, int exactArgs) {
+    if (element is MethodElement &&
+        element.parameters.where(_isRequired).length != exactArgs) {
+      BuildError.throwForElement(element, message);
+    }
+  }
+
   /// Visits an `@Directive`-annotated class [element].
   ///
   /// For class members that are annotated, calls, in kind:
@@ -113,6 +133,22 @@ class DirectiveVisitor {
     _assertPublic(member, '@HostListener must be on a public member');
     _assertMethod(member, '@HostListener must be on a method');
     _assertInstance(member, '@HostListener must be on a non-static member');
+    _assertMaxArgs(
+      member,
+      '@HostListener is only valid on methods with 0 or 1 parameters',
+      1,
+    );
+
+    final hostListenerArgs = ConstantReader(annotation).read('args');
+    if (hostListenerArgs.isList) {
+      final inferredParamCount = hostListenerArgs.listValue.length;
+      _assertExactArgs(
+        member,
+        '@HostListener expected a method with $inferredParamCount parameter(s)',
+        inferredParamCount,
+      );
+    }
+
     onHostListener(member, annotation);
   }
 }
