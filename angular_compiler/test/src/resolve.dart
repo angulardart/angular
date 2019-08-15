dd9fc4118d4246301a437f88189babc4684579e5
Googler <noreply@google.com>
2019-08-15 16:56:37 -0700

    Pass the list of archives to archive_helper.sh to avoid problems with exoblaze local execution.
    PiperOrigin-RevId: 263671923

--- angular_compiler/test/src/resolve.dart
+++ angular_compiler/test/src/resolve.dart
@@ -1,19 +1,29 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:build_test/build_test.dart';
+import 'package:package_resolver/package_resolver.dart';
 
 const angular = 'package:angular/angular.dart';
+
+// Use custom package config for angular sources if specified
+final _packageResolverFuture =
+    Platform.environment['ANGULAR_PACKAGE_CONFIG_PATH'] != null
+        ? PackageResolver.loadConfig(
+            Uri.file(Platform.environment['ANGULAR_PACKAGE_CONFIG_PATH']))
+        : PackageResolver.current;
 
 /// Resolves [source] code as-if it is implemented with an AngularDart import.
 ///
 /// Returns the resolved library as `package:test_lib/test_lib.dart`.
-Future<LibraryElement> resolveLibrary(String source) => resolveSource('''
+Future<LibraryElement> resolveLibrary(String source) async => resolveSource('''
       library _test;
       import '$angular';\n\n$source''',
     (resolver) => resolver.findLibraryByName('_test'),
-    inputId: AssetId('test_lib', 'lib/test_lib.dart'));
+    inputId: AssetId('test_lib', 'lib/test_lib.dart'),
+    resolver: await _packageResolverFuture);
 
 /// Resolves [source] code as-if it is implemented with an AngularDart import.
 ///
