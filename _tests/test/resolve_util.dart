6a089fccb9bbd313b4dc12aef43dd31168795a42
Googler <noreply@google.com>
2019-08-15 16:56:37 -0700

    Pass the list of archives to archive_helper.sh to avoid problems with exoblaze local execution.
    PiperOrigin-RevId: 263671923

--- _tests/test/resolve_util.dart
+++ _tests/test/resolve_util.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:angular/src/compiler/offline_compiler.dart';
@@ -7,19 +8,30 @@ import 'package:angular/src/source_gen/template_compiler/component_visitor_excep
 import 'package:build/build.dart';
 import 'package:build_test/build_test.dart';
 import 'package:source_gen/source_gen.dart';
+import 'package:package_resolver/package_resolver.dart';
 
-Future<LibraryElement> resolve(String source) async {
+// Use custom package config for angular sources if specified
+final _packageResolverFuture =
+    Platform.environment['ANGULAR_PACKAGE_CONFIG_PATH'] != null
+        ? PackageResolver.loadConfig(
+            Uri.file(Platform.environment['ANGULAR_PACKAGE_CONFIG_PATH']))
+        : PackageResolver.current;
+
+Future<LibraryElement> resolve(String source,
+    [PackageResolver resolver]) async {
   final testAssetId = AssetId('_tests', 'lib/resolve.dart');
   return await resolveSource(
       source, (resolver) => resolver.libraryFor(testAssetId),
-      inputId: testAssetId);
+      inputId: testAssetId, resolver: resolver);
 }
 
 Future<NormalizedComponentWithViewDirectives> resolveAndFindComponent(
   String source,
 ) async {
-  final library = await resolve("import 'package:angular/angular.dart';"
-      "$source");
+  final library = await resolve(
+      "import 'package:angular/angular.dart';"
+      "$source",
+      await _packageResolverFuture);
   final artifacts = findComponentsAndDirectives(
       LibraryReader(library), ComponentVisitorExceptionHandler());
   return artifacts.components.first;
