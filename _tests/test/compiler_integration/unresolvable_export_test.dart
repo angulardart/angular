5d335030aad8927118b66d066268adb25a8c689a
maopang <maopang@google.com>
2019-09-04 15:31:33 -0700

    Added source information to errors thrown for invalid "exports".
    PiperOrigin-RevId: 267245437

--- _tests/test/compiler_integration/unresolvable_export_test.dart
+++ _tests/test/compiler_integration/unresolvable_export_test.dart
@@ -24,4 +24,48 @@ void main() {
       ]),
     ]);
   });
+
+  test('should fail build for non identifier exports', () async {
+    await compilesExpecting('''
+      import '$ngImport';
+
+      @Component(
+        selector: 'bad',
+        template: '',
+        exports: const [1],
+      )
+      class BadComponent {}
+    ''', errors: [
+      allOf([
+        contains('Item 1 in the "exports" field must be an identifier'),
+        containsSourceLocation(3, 7)
+      ]),
+    ]);
+  });
+
+  test(
+      'should fail build if exports field having a class prefix, it only allows'
+      'library prefix instead', () async {
+    await compilesExpecting('''
+      import '$ngImport';
+
+      class Foo {
+        static String bar() => 'Error';
+      }
+
+      @Component(
+        selector: 'bad',
+        template: '',
+        exports: const [Foo.bar],
+      )
+      class BadComponent {}
+    ''', errors: [
+      allOf([
+        contains('must be either a simple identifier or an identifier with a '
+            'library prefix'),
+        contains('Foo.bar'),
+        containsSourceLocation(7, 7)
+      ])
+    ]);
+  });
 }
