7e1acae4ab4f63ee33ac024f45bbfb2a5a7ecddc
Lukasz Kolodziejczyk <koodziejczyk@google.com>
2020-07-15 14:33:12 +0000

    _tests: replace package_resolver with package_config

--- _tests/test/resolve_util.dart
+++ _tests/test/resolve_util.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:angular_compiler/v1/src/compiler/template_compiler.dart';
@@ -8,21 +9,22 @@ import 'package:angular_compiler/v1/src/source_gen/template_compiler/component_v
 import 'package:build/build.dart';
 import 'package:build_test/build_test.dart';
 import 'package:source_gen/source_gen.dart';
-import 'package:package_resolver/package_resolver.dart';
+import 'package:package_config/package_config.dart';
 
 // Use custom package config for angular sources if specified
-final _packageResolverFuture =
-    Platform.environment['ANGULAR_PACKAGE_CONFIG_PATH'] != null
-        ? PackageResolver.loadConfig(
-            Uri.file(Platform.environment['ANGULAR_PACKAGE_CONFIG_PATH']))
-        : Future.value(PackageResolver.current);
+final _packageConfigFuture = Platform
+            .environment['ANGULAR_PACKAGE_CONFIG_PATH'] !=
+        null
+    ? loadPackageConfigUri(
+            Uri.base.resolve(Platform.environment['ANGULAR_PACKAGE_CONFIG_PATH']))
+    : Isolate.packageConfig.then(loadPackageConfigUri);
 
 Future<LibraryElement> resolve(String source,
-    [PackageResolver resolver]) async {
+    [PackageConfig packageConfig]) async {
   final testAssetId = AssetId('_tests', 'lib/resolve.dart');
   return await resolveSource(
       source, (resolver) => resolver.libraryFor(testAssetId),
-      inputId: testAssetId, resolver: resolver);
+      inputId: testAssetId, packageConfig: packageConfig);
 }
 
 Future<NormalizedComponentWithViewDirectives> resolveAndFindComponent(
@@ -31,7 +33,7 @@ Future<NormalizedComponentWithViewDirectives> resolveAndFindComponent(
   final library = await resolve(
       "import 'package:angular/angular.dart';"
       '$source',
-      await _packageResolverFuture);
+      await _packageConfigFuture);
   final artifacts = findComponentsAndDirectives(
       LibraryReader(library), ComponentVisitorExceptionHandler());
   return artifacts.components.first;
