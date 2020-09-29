fd7a350ab7f85f2a512d457ab78d5b136d942d9c
lukaszkolodziejczyk <koodziejczyk@google.com>
2020-09-29 19:24:17 +0200

    Remove angular analyzer plugin (#1916)

--- angular/tools/analyzer_plugin/bin/plugin.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:isolate';
-
-// ignore: uri_does_not_exist
-import 'package:angular_analyzer_plugin/starter.dart' as plugin;
-
-void main(List<String> args, SendPort sendPort) {
-  plugin.start(args, sendPort);
-}
--- angular_analyzer_plugin/lib/src/model/resolved/top_level.dart
+++
@@ -1,2 +0,0 @@
-// Currently, resolved [TopLevel]'s do not differ at all from syntactic ones.
-export 'package:angular_analyzer_plugin/src/model/syntactic/top_level.dart';
