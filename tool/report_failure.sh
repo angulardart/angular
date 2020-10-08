3a4a1718d54e8abf2f0b81f785b327b7cde59fde
Kevin Moore <kevmoo@google.com>
2020-10-07 20:35:50 -0700

    better

--- tool/report_failure.sh
+++ tool/report_failure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#if [ "$TRAVIS_EVENT_TYPE" != "pull_request" ] && [ "$TRAVIS_ALLOW_FAILURE" != "true" ]; then
-curl -H "Content-Type: application/json" -X POST -d \
-  "{'text':'Build status ${TRAVIS_TEST_RESULT} for ${TRAVIS_EVENT_TYPE} ${TRAVIS_BUILD_WEB_URL}'}" \
-  "${CHAT_HOOK_URI}"
-#fi
+if [ "$TRAVIS_EVENT_TYPE" != "pull_request" ] && [ "$TRAVIS_ALLOW_FAILURE" != "true" ]; then
+  curl -H "Content-Type: application/json" -X POST -d \
+    "{'text':'Build failed! ${TRAVIS_BUILD_WEB_URL}'}" \
+    "${CHAT_HOOK_URI}"
+fi
