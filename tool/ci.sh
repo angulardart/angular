6f61a372c0f800382327d72ce273877d51e37ddc
Kevin Moore <kevmoo@google.com>
2021-05-21 15:29:22 -0700

    Internal change
    PiperOrigin-RevId: 375176220

--- tool/ci.sh
+++ tool/ci.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-# Created with package:mono_repo v3.4.7
+# Created with package:mono_repo v4.0.0
 
 # Support built in commands on windows out of the box.
+# When it is a flutter repo (check the pubspec.yaml for "sdk: flutter")
+# then "flutter" is called instead of "pub".
+# This assumes that the Flutter SDK has been installed in a previous step.
 function pub() {
-  if [[ $TRAVIS_OS_NAME == "windows" ]]; then
-    command pub.bat "$@"
+  if grep -Fq "sdk: flutter" "${PWD}/pubspec.yaml"; then
+    if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+      command flutter.bat pub "$@"
+    else
+      command flutter pub "$@"
+    fi
   else
-    command pub "$@"
+    if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+      command pub.bat "$@"
+    else
+      command pub "$@"
+    fi
   fi
 }
 function dartfmt() {
