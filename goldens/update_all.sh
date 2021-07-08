7998a1c324da2b573df19aaf353619ceb160aff8
Ivan Inozemtsev <iinozemtsev@google.com>
2021-07-08 04:42:52 -0700

    Internal change
    PiperOrigin-RevId: 383607154

--- goldens/update_all.sh
+++ goldens/update_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-blaze test third_party/dart_src/angular/goldens:all --test_arg=--update --test_strategy=local
+blaze test third_party/dart_src/angular/goldens:all --test_arg=--update --test_strategy=local --nocache_test_results
